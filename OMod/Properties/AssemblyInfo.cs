19431925ea7b204a1304928763f274f8ef32493d
kaburke <kaburke@localhost>
2011-11-09 21:01:19 +0000

    Incremented programme version to 0.11.9

--- OMod/Properties/AssemblyInfo.cs
+++ OMod/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4330.35931")]
+[assembly: AssemblyFileVersion("1.1.4330.37809")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
--- Setup/setup.iss
+++ Setup/setup.iss
@@ -15,7 +15,7 @@
 
 #define MyAppSetupName 'Nexus Mod Manager'
 #define MyExeName 'NexusClient.exe'
-#define MyAppVersion '0.11.8'
+#define MyAppVersion '0.11.9'
 #define SetupScriptVersion '0.6.1.0'
 [Setup]
 AppName={#MyAppSetupName}
