81269754aaf9bc85f9d816bf1224ed169365409e
kaburke <kaburke@localhost>
2011-11-17 04:26:15 +0000

    Incremented programme version to 0.12.5

--- Fallout3.XmlScript/Properties/AssemblyInfo.cs
+++ Fallout3.XmlScript/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4338.94")]
+[assembly: AssemblyFileVersion("1.1.4338.7962")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
