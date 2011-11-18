c702034be17ff0ebf53430901cdaa52a2e688eb4
kaburke <kaburke@localhost>
2011-11-18 00:49:36 +0000

    Incremented programme version to 0.12.6

--- CSharpScript/Properties/AssemblyInfo.cs
+++ CSharpScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4338.7959")]
+[assembly: AssemblyFileVersion("1.1.4339.1443")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
--- Fallout3.CSharpScript/Properties/AssemblyInfo.cs
+++ Fallout3.CSharpScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4338.7961")]
+[assembly: AssemblyFileVersion("1.1.4339.1445")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
