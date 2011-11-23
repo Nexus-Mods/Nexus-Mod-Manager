11a1f825a9f5cc9a68610e9411ab79c643f1656e
kaburke <kaburke@localhost>
2011-11-23 19:25:46 +0000

    Incremented programme version to 0.12.8

--- Controls/Properties/AssemblyInfo.cs
+++ Controls/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4344.33764")]
+[assembly: AssemblyFileVersion("1.1.4344.34683")]
--- Oblivion.ModScript/Properties/AssemblyInfo.cs
+++ Oblivion.ModScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4344.33769")]
+[assembly: AssemblyFileVersion("1.1.4344.34688")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
