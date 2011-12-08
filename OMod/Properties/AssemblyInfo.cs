9c16b18d057827145214a6f4dc20255446fb1505
kaburke <kaburke@localhost>
2011-12-08 01:07:09 +0000

    Incremented programme version to 0.12.13

--- OMod/Properties/AssemblyInfo.cs
+++ OMod/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4358.41066")]
+[assembly: AssemblyFileVersion("1.1.4359.1484")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
--- XmlScript/Properties/AssemblyInfo.cs
+++ XmlScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.4358.41063")]
+[assembly: AssemblyFileVersion("1.1.4359.1481")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
