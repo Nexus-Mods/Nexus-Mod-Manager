d94d83038e4551ae772423f1294436cde87632e6
kaburke <kaburke@localhost>
2011-12-09 05:32:57 +0000

    Incremented programme version to 0.12.14

--- Fallout3.XmlScript/Properties/AssemblyInfo.cs
+++ Fallout3.XmlScript/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4359.1483")]
+[assembly: AssemblyFileVersion("1.1.4360.9939")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
--- Oblivion/Properties/AssemblyInfo.cs
+++ Oblivion/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4359.1482")]
+[assembly: AssemblyFileVersion("1.1.4360.9938")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
