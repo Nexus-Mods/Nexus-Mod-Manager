6b549ad9a04b661d3ee2ca84667ad0ded4248dca
kaburke <kaburke@localhost>
2012-01-10 07:21:56 +0000

    Incremented programme version to 0.13.1

--- XmlScript/Properties/AssemblyInfo.cs
+++ XmlScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.4392.10241")]
+[assembly: AssemblyFileVersion("1.1.4392.13223")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
