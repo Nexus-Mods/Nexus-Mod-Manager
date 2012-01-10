e5b84f4dd4f490a699ea0716a6f52f7b15287ca5
kaburke <kaburke@localhost>
2012-01-10 05:52:53 +0000

    Changed the implementation to improve behaviour.

--- Controls/AutosizeLabel.cs
+++ Controls/AutosizeLabel.cs
@@ -11,12 +11,173 @@ namespace Nexus.Client.Controls
 	/// <remarks>
 	/// Currently, the label only resizes its height.
 	/// </remarks>
-	public class AutosizeLabel : RichTextBox
+	public class AutosizeLabel : ScrollableControl
 	{
-		private ScrollableControl m_ctlNonExistant = new ScrollableControl();
+		/// <summary>
+		/// A <see cref="RichTextBox"/> that is styled to behave like a label.
+		/// </summary>
+		private class RichTextLabel : RichTextBox
+		{
+			private bool m_booAllowSelection = false;
+
+			#region Properties
+
+			/// <summary>
+			/// Gets or sets whether text selection is allowed.
+			/// </summary>
+			/// <value>Whether text selection is allowed.</value>
+			[Browsable(true)]
+			[Category("Behavior")]
+			[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+			[DefaultValue(false)]
+			public bool AllowSelection
+			{
+				get
+				{
+					return m_booAllowSelection;
+				}
+				set
+				{
+					if (m_booAllowSelection != value)
+					{
+						m_booAllowSelection = value;
+						if (m_booAllowSelection)
+							this.Enter -= new EventHandler(AutosizeLabel_Enter);
+						else
+							this.Enter += new EventHandler(AutosizeLabel_Enter);
+					}
+				}
+			}
+
+			/// <summary>
+			/// Gets or sets whether the label is multiline.
+			/// </summary>
+			/// <remarks>
+			/// Since the label autosizes, this is always true.
+			/// </remarks>
+			/// <value>Always <c>true</c>.</value>
+			[DefaultValue(true)]
+			public new bool Multiline
+			{
+				get
+				{
+					return base.Multiline;
+				}
+				set
+				{
+					base.Multiline = true;
+				}
+			}
+
+			/// <summary>
+			/// Gets or sets whether the label is readonly.
+			/// </summary>
+			/// <remarks>
+			/// Since this is a label, this is always true.
+			/// </remarks>
+			/// <value>Always <c>true</c>.</value>
+			[DefaultValue(true)]
+			public new bool ReadOnly
+			{
+				get
+				{
+					return base.ReadOnly;
+				}
+				set
+				{
+					base.ReadOnly = true;
+				}
+			}
+
+			/// <summary>
+			/// Gets or sets whether the label has a tab stop.
+			/// </summary>
+			/// <remarks>
+			/// Since this is a label, this is always false.
+			/// </remarks>
+			/// <value>Always <c>false</c>.</value>
+			[DefaultValue(false)]
+			public new bool TabStop
+			{
+				get
+				{
+					return base.TabStop;
+				}
+				set
+				{
+					base.TabStop = false;
+				}
+			}
+
+			#endregion
+
+			#region Constructors
+
+			/// <summary>
+			/// The default constructor.
+			/// </summary>
+			public RichTextLabel()
+			{
+				//set this to true so that when we set AllowSelection to false,
+				// the setter doesn't short circut, and actually performs the
+				// required set up.
+				m_booAllowSelection = true;
+				SetTextColor();
+			}
+
+			#endregion
+
+			/// <summary>
+			/// Makes sure all text doesn't look disabled.
+			/// </summary>
+			/// <param name="e">A <see cref="EventArgs"/> describing the event arguments.</param>
+			protected override void OnTextChanged(EventArgs e)
+			{
+				base.OnTextChanged(e);
+				SetTextColor();
+			}
+
+			/// <summary>
+			/// Forces the text color not to look disabled.
+			/// </summary>
+			protected void SetTextColor()
+			{
+				SelectAll();
+				SelectionColor = ForeColor;
+				Select(0, 0);
+			}
+
+			/// <summary>
+			/// Raises the <see cref="RichTextBox.LinkClicked"/> event.
+			/// </summary>
+			/// <remarks>
+			/// This ask the OS to launch the clicked link.
+			/// </remarks>
+			/// <param name="e">A <see cref="LinkClickedEventArgs"/> describing the event arguments.</param>
+			protected override void OnLinkClicked(LinkClickedEventArgs e)
+			{
+				Uri uriUrl = new Uri(e.LinkText);
+				System.Diagnostics.Process.Start(uriUrl.ToString());
+				base.OnLinkClicked(e);
+			}
+
+			/// <summary>
+			/// Handles the <see cref="Control.Enter"/> event of the autosize label.
+			/// </summary>
+			/// <remarks>
+			/// This gives the hidden scrollable panel focus instead of the label,
+			/// so that the user can't select any text.
+			/// </remarks>
+			/// <param name="sender">The object that raised the event.</param>
+			/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+			public void AutosizeLabel_Enter(object sender, EventArgs e)
+			{
+				Parent.Focus();
+			}
+		}
+
+		RichTextLabel m_rtlLabel = new RichTextLabel();
 		private RichTextBoxScrollBars m_sbrScrollBars = RichTextBoxScrollBars.None;
-		private DockStyle m_dksDockStyle = DockStyle.None;
-		private bool m_booAllowSelection = false;
 
 		#region Properties
 
@@ -32,17 +193,15 @@ namespace Nexus.Client.Controls
 		{
 			get
 			{
-				return m_booAllowSelection;
+				return m_rtlLabel.AllowSelection;
 			}
 			set
 			{
-				if (m_booAllowSelection != value)
+				if (m_rtlLabel.AllowSelection != value)
 				{
-					m_booAllowSelection = value;
-					if (m_booAllowSelection)
-						this.Enter -= new EventHandler(AutosizeLabel_Enter);
-					else
-						this.Enter += new EventHandler(AutosizeLabel_Enter);
+					m_rtlLabel.AllowSelection = value;
+					base.Cursor = value ? Cursors.IBeam : Cursors.Arrow;
+					m_rtlLabel.Cursor = value ? Cursors.IBeam : Cursors.Arrow;
 					SetUpScrollHandling();
 				}
 			}
@@ -55,16 +214,19 @@ namespace Nexus.Client.Controls
 		/// Since the label autosizes, this is always true.
 		/// </remarks>
 		/// <value>Always <c>true</c>.</value>
+		[Browsable(true)]
+		[Category("Behavior")]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
 		[DefaultValue(true)]
-		public new bool Multiline
+		public bool Multiline
 		{
 			get
 			{
-				return base.Multiline;
+				return m_rtlLabel.Multiline;
 			}
 			set
 			{
-				base.Multiline = true;
+				m_rtlLabel.Multiline = true;
 			}
 		}
 
@@ -72,8 +234,11 @@ namespace Nexus.Client.Controls
 		/// Gets or sets whether the label has scrollbars.
 		/// </summary>
 		/// <value>Whether the label has scrollbars.</value>
+		[Browsable(true)]
+		[Category("Appearance")]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
 		[DefaultValue(typeof(RichTextBoxScrollBars), "None")]
-		public new RichTextBoxScrollBars ScrollBars
+		public RichTextBoxScrollBars ScrollBars
 		{
 			get
 			{
@@ -90,40 +255,22 @@ namespace Nexus.Client.Controls
 		}
 
 		/// <summary>
-		/// Gets or sets the control's dock style.
-		/// </summary>
-		/// <value>The control's dock style.</value>
-		[DefaultValue(typeof(DockStyle), "None")]
-		public new DockStyle Dock
-		{
-			get
-			{
-				return m_dksDockStyle;
-			}
-			set
-			{
-				if (m_dksDockStyle != value)
-				{
-					m_dksDockStyle = value;
-					SetUpScrollHandling();
-				}
-			}
-		}
-
-		/// <summary>
 		/// Gets or sets the border style.
 		/// </summary>
 		/// <value>The border style.</value>
+		[Browsable(true)]
+		[Category("Appearance")]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
 		[DefaultValue(typeof(BorderStyle), "None")]
-		public new BorderStyle BorderStyle
+		public BorderStyle BorderStyle
 		{
 			get
 			{
-				return base.BorderStyle;
+				return m_rtlLabel.BorderStyle;
 			}
 			set
 			{
-				base.BorderStyle = value;
+				m_rtlLabel.BorderStyle = value;
 			}
 		}
 
@@ -132,7 +279,7 @@ namespace Nexus.Client.Controls
 		/// </summary>
 		/// <value>The back colour.</value>
 		[DefaultValue(typeof(SystemColors), "Control")]
-		public new Color BackColor
+		public override Color BackColor
 		{
 			get
 			{
@@ -141,6 +288,60 @@ namespace Nexus.Client.Controls
 			set
 			{
 				base.BackColor = value;
+				m_rtlLabel.BackColor = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the text colour.
+		/// </summary>
+		/// <value>The text colour.</value>
+		[Browsable(true)]
+		[Category("Appearance")]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+		[DefaultValue(typeof(SystemColors), "ControlText")]
+		public override Color ForeColor
+		{
+			get
+			{
+				return m_rtlLabel.ForeColor;
+			}
+			set
+			{
+				m_rtlLabel.ForeColor = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the text font.
+		/// </summary>
+		/// <value>The text font.</value>
+		public override Font Font
+		{
+			get
+			{
+				return base.Font;
+			}
+			set
+			{
+				m_rtlLabel.Font = value;
+				base.Font = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the label's text.
+		/// </summary>
+		/// <value>The label's text.</value>
+		public override string Text
+		{
+			get
+			{
+				return m_rtlLabel.Text;
+			}
+			set
+			{
+				m_rtlLabel.Text = value;
 			}
 		}
 
@@ -151,16 +352,39 @@ namespace Nexus.Client.Controls
 		/// Since this is a label, this is always true.
 		/// </remarks>
 		/// <value>Always <c>true</c>.</value>
+		[Browsable(true)]
+		[Category("Behavior")]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
 		[DefaultValue(true)]
-		public new bool ReadOnly
+		public bool ReadOnly
 		{
 			get
 			{
-				return base.ReadOnly;
+				return m_rtlLabel.ReadOnly;
 			}
 			set
 			{
-				base.ReadOnly = true;
+				m_rtlLabel.ReadOnly = true;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets whether the label will automatically detect, and create links for, URLs.
+		/// </summary>
+		/// <value>Whether the label will automatically detect, and create links for, URLs.</value>
+		[Browsable(true)]
+		[Category("Behavior")]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+		[DefaultValue(true)]
+		public bool DetectUrls
+		{
+			get
+			{
+				return m_rtlLabel.DetectUrls;
+			}
+			set
+			{
+				m_rtlLabel.DetectUrls = true;
 			}
 		}
 
@@ -171,16 +395,19 @@ namespace Nexus.Client.Controls
 		/// Since this is a label, this is always false.
 		/// </remarks>
 		/// <value>Always <c>false</c>.</value>
+		[Browsable(true)]
+		[Category("Behavior")]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
 		[DefaultValue(false)]
 		public new bool TabStop
 		{
 			get
 			{
-				return base.TabStop;
+				return m_rtlLabel.TabStop;
 			}
 			set
 			{
-				base.TabStop = false;
+				m_rtlLabel.TabStop = false;
 			}
 		}
 
@@ -192,7 +419,7 @@ namespace Nexus.Client.Controls
 		/// </remarks>
 		/// <value>Always <see cref="Cursors.Arrow"/>.</value>
 		[DefaultValue(typeof(Cursors), "Arrow")]
-		public new Cursor Cursor
+		public override Cursor Cursor
 		{
 			get
 			{
@@ -200,45 +427,59 @@ namespace Nexus.Client.Controls
 			}
 			set
 			{
-				base.Cursor = Cursors.Arrow;
 			}
 		}
 
 		#endregion
+
+		#region Constructors
 
 		/// <summary>
 		/// The default constructor.
 		/// </summary>
 		public AutosizeLabel()
 		{
-			m_ctlNonExistant.Name = "NonExistant";
-			m_ctlNonExistant.AutoScroll = true;
-			this.Multiline = true;
-			this.ScrollBars = RichTextBoxScrollBars.None;
-			this.BorderStyle = BorderStyle.None;
-			this.BackColor = SystemColors.Control;
-			this.ReadOnly = true;
-			this.TabStop = false;
-			this.Cursor = Cursors.Arrow;
-			//set this to true so that when we set AllowSelection to false,
-			// the setter doesn't short circut, and actually performs the
-			// required set up.
-			m_booAllowSelection = true;
+			AutoScroll = true;
+			m_rtlLabel.ContentsResized += new ContentsResizedEventHandler(Label_ContentsResized);
+			m_rtlLabel.TextChanged += new EventHandler(Label_TextChanged);
+			Controls.Add(m_rtlLabel);
+			Multiline = true;
+			ScrollBars = RichTextBoxScrollBars.None;
+			BorderStyle = BorderStyle.None;
+			BackColor = SystemColors.Control;
+			ReadOnly = true;
+			TabStop = false;
+			base.Cursor = Cursors.Arrow;
 			AllowSelection = false;
-			SetTextColor();
+			SetUpScrollHandling();
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Handles the <see cref="RichTextBox.ContentsResized"/> event of the rich text label.
+		/// </summary>
+		/// <remarks>
+		/// Resizes the label as the content size changes.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">A <see cref="ContentsResizedEventArgs"/> describing the event arguments.</param>
+		private void Label_ContentsResized(object sender, ContentsResizedEventArgs e)
+		{
+			Height = e.NewRectangle.Height + 5;
 		}
 
 		/// <summary>
-		/// Raises the <see cref="Control.ParentChanged"/> event.
+		/// Handles the <see cref="Control.TextChanged"/> event of the rich text label.
 		/// </summary>
+		/// <remarks>
+		/// Raises the <see cref="Control.TextChanged"/> event for the control.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
 		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
-		protected override void OnParentChanged(EventArgs e)
+		private void Label_TextChanged(object sender, EventArgs e)
 		{
-			base.OnParentChanged(e);
-			if (Parent == m_ctlNonExistant)
-				return;
-			if (Parent != null)
-				SetUpScrollHandling();
+			OnTextChanged(e);
 		}
 
 		/// <summary>
@@ -248,151 +489,30 @@ namespace Nexus.Client.Controls
 		{
 			if (AllowSelection)
 			{
-				base.ScrollBars = m_sbrScrollBars;
-				base.Dock = m_dksDockStyle;
-				if ((m_ctlNonExistant.Parent != null) && (m_ctlNonExistant.Parent != Parent))
-				{
-					Control ctlParent = m_ctlNonExistant.Parent;
-					m_ctlNonExistant.Parent = null;
-					ctlParent.Controls.Add(this);
-				}
+				m_rtlLabel.ScrollBars = m_sbrScrollBars;
+				m_rtlLabel.Dock = DockStyle.Fill;
 			}
 			else
 			{
-				base.ScrollBars = RichTextBoxScrollBars.None;
+				m_rtlLabel.ScrollBars = RichTextBoxScrollBars.None;
 				switch (m_sbrScrollBars)
 				{
 					case RichTextBoxScrollBars.Both:
 					case RichTextBoxScrollBars.ForcedBoth:
 					case RichTextBoxScrollBars.ForcedVertical:
 					case RichTextBoxScrollBars.Vertical:
-						base.Dock = DockStyle.Top;
+						m_rtlLabel.Dock = DockStyle.Top;
 						break;
 					case RichTextBoxScrollBars.ForcedHorizontal:
 					case RichTextBoxScrollBars.Horizontal:
-						base.Dock = DockStyle.Left;
+						m_rtlLabel.Dock = DockStyle.Left;
 						break;
 					case RichTextBoxScrollBars.None:
 					default:
-						base.Dock = DockStyle.Fill;
-						break;
-				}
-				m_ctlNonExistant.Dock = m_dksDockStyle;
-				if ((Parent != null) && (Parent != m_ctlNonExistant))
-					Parent.Controls.Add(m_ctlNonExistant);
-				if (Parent != m_ctlNonExistant)
-					m_ctlNonExistant.Controls.Add(this);
-				m_ctlNonExistant.Size = Size;
-			}
-		}
-
-		/// <summary>
-		/// Resizes the label as the content size changes.
-		/// </summary>
-		/// <param name="e">A <see cref="ContentsResizedEventArgs"/> describing the event arguments.</param>
-		protected override void OnContentsResized(ContentsResizedEventArgs e)
-		{
-			Height = e.NewRectangle.Height + 5;
-			base.OnContentsResized(e);
-		}
-
-		/// <summary>
-		/// Raises the <see cref="Control.SizeChanged"/> event.
-		/// </summary>
-		/// <remarks>
-		/// This resizes the non-existant control that privodes scrolling for the label.
-		/// </remarks>
-		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
-		protected override void OnSizeChanged(EventArgs e)
-		{
-			if (m_ctlNonExistant.Parent != null)
-			{
-				switch (m_ctlNonExistant.Dock)
-				{
-					case DockStyle.Bottom:
-					case DockStyle.Top:
-						m_ctlNonExistant.Size = new Size(m_ctlNonExistant.Width, Size.Height);
-						break;
-					case DockStyle.Left:
-					case DockStyle.Right:
-						m_ctlNonExistant.Size = new Size(Size.Width, m_ctlNonExistant.Height);
-						break;
-					case DockStyle.None:
-						m_ctlNonExistant.Size = Size;
+						m_rtlLabel.Dock = DockStyle.Fill;
 						break;
 				}
 			}
-			base.OnSizeChanged(e);
-		}
-
-		/// <summary>
-		/// Makes sure all text doesn't look disabled.
-		/// </summary>
-		/// <param name="e">A <see cref="EventArgs"/> describing the event arguments.</param>
-		protected override void OnTextChanged(EventArgs e)
-		{
-			base.OnTextChanged(e);
-			SetTextColor();
-		}
-
-		/// <summary>
-		/// Forces the text color not to look disabled.
-		/// </summary>
-		protected void SetTextColor()
-		{
-			SelectAll();
-			SelectionColor = SystemColors.ControlText;
-			Select(0, 0);
-		}
-
-		/// <summary>
-		/// Raises the <see cref="RichTextBox.LinkClicked"/> event.
-		/// </summary>
-		/// <remarks>
-		/// This ask the OS to launch the clicked link.
-		/// </remarks>
-		/// <param name="e">A <see cref="LinkClickedEventArgs"/> describing the event arguments.</param>
-		protected override void OnLinkClicked(LinkClickedEventArgs e)
-		{
-			Uri uriUrl = new Uri(e.LinkText);
-			System.Diagnostics.Process.Start(uriUrl.ToString());
-			base.OnLinkClicked(e);
-		}
-
-		/// <summary>
-		/// Handles the <see cref="Control.Enter"/> event of the autosize label.
-		/// </summary>
-		/// <remarks>
-		/// This gives the hidden scrollable panel focus instead of the label,
-		/// so that the user can't select any text.
-		/// </remarks>
-		/// <param name="sender">The object that raised the event.</param>
-		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
-		public void AutosizeLabel_Enter(object sender, EventArgs e)
-		{
-			m_ctlNonExistant.Focus();
-		}
-
-		/// <summary>
-		/// Brings the label to the foreground.
-		/// </summary>
-		public new void BringToFront()
-		{
-			if (AllowSelection)
-				base.BringToFront();
-			else
-				m_ctlNonExistant.BringToFront();
-		}
-
-		/// <summary>
-		/// Sends the label to the background.
-		/// </summary>
-		public new void SendToBack()
-		{
-			if (AllowSelection)
-				base.BringToFront();
-			else
-				m_ctlNonExistant.BringToFront();
 		}
 	}
 }
