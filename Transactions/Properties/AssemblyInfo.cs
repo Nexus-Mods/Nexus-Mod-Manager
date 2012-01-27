47d293fe502c60cdf749f8ba47487bc08d8a241c
kaburke <kaburke@localhost>
2012-01-27 00:07:54 +0000

    Incremented version to 0.14.1

--- Transactions/Properties/AssemblyInfo.cs
+++ Transactions/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4408.41800")]
+[assembly: AssemblyFileVersion("1.1.4409.54")]
