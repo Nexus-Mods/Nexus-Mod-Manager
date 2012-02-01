55a9e88937b848a8aa13f2dd6bde1a97891bbf50
kaburke <kaburke@localhost>
2012-01-31 21:02:05 -0700

    Changing how game mode selection works. Games will be discovered, and only installed games will display in the selector.

--- NexusClient/GameModeSelector.cs
+++ NexusClient/GameModeSelector.cs
@@ -19,8 +19,6 @@ namespace Nexus.Client
 	/// </remarks>
 	public class GameModeSelector
 	{
-		private Dictionary<string, IGameModeFactory> m_dicGameModeFactories = new Dictionary<string, IGameModeFactory>(StringComparer.OrdinalIgnoreCase);
-
 		#region Properties
 
 		/// <summary>
@@ -40,74 +38,9 @@ namespace Nexus.Client
 		public GameModeSelector(IEnvironmentInfo p_eifEnvironmentInfo)
 		{
 			EnvironmentInfo = p_eifEnvironmentInfo;
-			DiscoverGameModes();
 		}
 
 		#endregion
-
-		/// <summary>
-		/// Finds the game mode assemblies available for selection.
-		/// </summary>
-		protected void DiscoverGameModes()
-		{
-			Trace.TraceInformation("Discovering Game Mode Factories...");
-			Trace.Indent();
-
-			string strGameModesPath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "GameModes");
-
-			Trace.TraceInformation("Looking in: {0}", strGameModesPath);
-
-			string[] strAssemblies = Directory.GetFiles(strGameModesPath, "*.dll");
-			foreach (string strAssembly in strAssemblies)
-			{
-				Trace.TraceInformation("Checking: {0}", Path.GetFileName(strAssembly));
-				Trace.Indent();
-
-				Assembly asmGameMode = Assembly.LoadFrom(strAssembly);
-				try
-				{
-					Type[] tpeTypes = asmGameMode.GetExportedTypes();
-					foreach (Type tpeType in tpeTypes)
-					{
-						if (typeof(IGameModeFactory).IsAssignableFrom(tpeType) && !tpeType.IsAbstract)
-						{
-							Trace.TraceInformation("Initializing: {0}", tpeType.FullName);
-							Trace.Indent();
-
-							ConstructorInfo cifConstructor = tpeType.GetConstructor(new Type[] { typeof(IEnvironmentInfo) });
-							if (cifConstructor == null)
-							{
-								Trace.TraceInformation("No constructor accepting one argument of type IEnvironmentInfo found.");
-								Trace.Unindent();
-								continue;
-							}
-							IGameModeFactory gmfGameModeFactory = (IGameModeFactory)cifConstructor.Invoke(new object[] { EnvironmentInfo });
-							if (m_dicGameModeFactories.ContainsKey(gmfGameModeFactory.GameModeDescriptor.ModeId))
-							{
-								string strError = String.Format("{0} has the same Game Mode Id as {1}. {1} will not be available.", m_dicGameModeFactories[gmfGameModeFactory.GameModeDescriptor.ModeId].GameModeDescriptor.Name, gmfGameModeFactory.GameModeDescriptor.Name);
-								Trace.TraceWarning(strError);
-								MessageBox.Show(strError, "Duplicate Game Mode Ids", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-							}
-							else
-								m_dicGameModeFactories[gmfGameModeFactory.GameModeDescriptor.ModeId] = gmfGameModeFactory;
-
-							Trace.Unindent();
-						}
-					}
-				}
-				catch (FileNotFoundException e)
-				{
-					Trace.TraceError(String.Format("Cannot load {0}: cannot find dependency {1}", strAssembly, e.FileName));
-					//some dependencies were missing, so we couldn't load the assembly
-					// given that these are plugins we don't have control over the dependecies:
-					// we may not even know what they (we can get their name, but if it's a custom
-					// dll not part of the client code base, we can't provide it even if we wanted to)
-					// there's nothing we can do, so simply skip the assembly
-				}
-				Trace.Unindent();
-			}
-			Trace.Unindent();
-		}
 
 		/// <summary>
 		/// Selectes the current game mode.
