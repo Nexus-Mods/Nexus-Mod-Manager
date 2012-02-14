e2a1cae818984f34cfcfd98d047f541f0973a7ba
kaburke <kaburke@localhost>
2012-02-13 20:17:19 -0700

    Added a label control that uses the path ellipses truncation instead of the end ellipses format.

---
+++ Controls/PathLabel.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+
+namespace Nexus.Client.Controls
+{
+	/// <summary>
+	/// A label that runcates the displayed string as if it were a file system path.
+	/// </summary>
+	public class PathLabel : Label
+	{
+		/// <summary>
+		/// Paints the label's string.
+		/// </summary>
+		/// <param name="e">A <see cref="PaintEventArgs"/> describing the event arguments.</param>
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			if (AutoEllipsis)
+			{
+				TextFormatFlags tffFormatting = TextFormatFlags.Left | TextFormatFlags.PathEllipsis;
+				TextRenderer.DrawText(e.Graphics, Text, Font, ClientRectangle, ForeColor, tffFormatting);
+			}
+			else
+				base.OnPaint(e);
+		}
+
+	}
+}
