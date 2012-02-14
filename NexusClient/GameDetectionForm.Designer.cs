d987ae6c0bd0adade5f063a41c5bace58f313630
kaburke <kaburke@localhost>
2012-02-13 22:30:36 -0700

    Added persistence of found game installation paths.

--- NexusClient/GameDetectionForm.Designer.cs
+++ NexusClient/GameDetectionForm.Designer.cs
@@ -30,14 +30,19 @@
 		{
 			this.panel1 = new System.Windows.Forms.Panel();
 			this.gameModeListView1 = new Nexus.Client.GameModeListView();
+			this.butOK = new System.Windows.Forms.Button();
+			this.butCancel = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// panel1
 			// 
+			this.panel1.Controls.Add(this.butCancel);
+			this.panel1.Controls.Add(this.butOK);
 			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.panel1.Location = new System.Drawing.Point(0, 212);
+			this.panel1.Location = new System.Drawing.Point(0, 223);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(284, 52);
+			this.panel1.Size = new System.Drawing.Size(284, 41);
 			this.panel1.TabIndex = 0;
 			// 
 			// gameModeListView1
@@ -46,19 +51,49 @@
 			this.gameModeListView1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.gameModeListView1.Location = new System.Drawing.Point(0, 0);
 			this.gameModeListView1.Name = "gameModeListView1";
-			this.gameModeListView1.Size = new System.Drawing.Size(284, 212);
+			this.gameModeListView1.Size = new System.Drawing.Size(284, 223);
 			this.gameModeListView1.TabIndex = 1;
+			// 
+			// butOK
+			// 
+			this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butOK.Enabled = false;
+			this.butOK.Location = new System.Drawing.Point(116, 6);
+			this.butOK.Name = "butOK";
+			this.butOK.Size = new System.Drawing.Size(75, 23);
+			this.butOK.TabIndex = 0;
+			this.butOK.Text = "OK";
+			this.butOK.UseVisualStyleBackColor = true;
+			this.butOK.Click += new System.EventHandler(this.butOK_Click);
+			// 
+			// butCancel
+			// 
+			this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.butCancel.Location = new System.Drawing.Point(197, 6);
+			this.butCancel.Name = "butCancel";
+			this.butCancel.Size = new System.Drawing.Size(75, 23);
+			this.butCancel.TabIndex = 1;
+			this.butCancel.Text = "Cancel";
+			this.butCancel.UseVisualStyleBackColor = true;
+			this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
 			// 
 			// GameDetectionForm
 			// 
+			this.AcceptButton = this.butOK;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.butCancel;
 			this.ClientSize = new System.Drawing.Size(284, 264);
+			this.ControlBox = false;
 			this.Controls.Add(this.gameModeListView1);
 			this.Controls.Add(this.panel1);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "GameDetectionForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "GameDetectionForm";
+			this.panel1.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -67,5 +102,7 @@
 
 		private System.Windows.Forms.Panel panel1;
 		private GameModeListView gameModeListView1;
+		private System.Windows.Forms.Button butCancel;
+		private System.Windows.Forms.Button butOK;
 	}
 }
