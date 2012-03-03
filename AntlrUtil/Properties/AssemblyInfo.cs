699cc59b503a1fb4c29e970a6a713aaf727f4c81
kaburke <kaburke@localhost>
2012-03-03 10:57:07 -0700

    Incremented version to 0.15.5

--- AntlrUtil/Properties/AssemblyInfo.cs
+++ AntlrUtil/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4445.32087")]
+[assembly: AssemblyFileVersion("1.1.4445.32290")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
