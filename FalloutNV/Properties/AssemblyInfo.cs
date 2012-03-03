87150539d32119a1c71ddaf4b197e5d6f7b7ece4
kaburke <kaburke@localhost>
2012-03-03 13:26:52 -0700

    Incremented version to 0.15.6

--- FalloutNV/Properties/AssemblyInfo.cs
+++ FalloutNV/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4445.32291")]
+[assembly: AssemblyFileVersion("1.1.4445.36780")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
--- XmlScript/Properties/AssemblyInfo.cs
+++ XmlScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.4445.32290")]
+[assembly: AssemblyFileVersion("1.1.4445.36779")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
