6e5a12898c714ccafe319ff5cafacdd627741d19
kaburke <kaburke@localhost>
2012-03-05 09:39:22 -0700

    Incremented version to 0.15.9

--- FOMod/Properties/AssemblyInfo.cs
+++ FOMod/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4445.40012")]
+[assembly: AssemblyFileVersion("1.1.4447.29961")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
