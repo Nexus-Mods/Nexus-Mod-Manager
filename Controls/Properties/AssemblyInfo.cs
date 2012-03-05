874a470cdeeb7f587750c2ca78f3309d94ca5ec3
kaburke <kaburke@localhost>
2012-03-05 12:24:08 -0700

    Incremented version to 0.15.11

--- Controls/Properties/AssemblyInfo.cs
+++ Controls/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4447.30370")]
+[assembly: AssemblyFileVersion("1.1.4447.34907")]
--- Setup/setup.iss
+++ Setup/setup.iss
@@ -15,7 +15,7 @@
 
 #define MyAppSetupName 'Nexus Mod Manager'
 #define MyExeName 'NexusClient.exe'
-#define MyAppVersion '0.15.10'
+#define MyAppVersion '0.15.11'
 #define SetupScriptVersion '0.7.1.0'
 #define MyPublisher 'Black Tree Gaming'
 [Setup]
