0292fbe017bb49891b31824fe33da8b6cf860e4d
kaburke <kaburke@localhost>
2012-03-07 11:09:42 -0700

    Incremented version to 0.15.15

--- FalloutNV.XmlScript/Properties/AssemblyInfo.cs
+++ FalloutNV.XmlScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4448.31997")]
+[assembly: AssemblyFileVersion("1.1.4449.32576")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
