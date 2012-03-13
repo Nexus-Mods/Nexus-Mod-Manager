9c769c4906b6052a34ec9bf046f284a481f2cdcd
kaburke <kaburke@localhost>
2012-03-13 17:06:59 -0600

    Added comments and cleaned up code.

--- NexusClient/IGameModeListViewItem.cs
+++ NexusClient/IGameModeListViewItem.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace Nexus.Client
 {
+	/// <summary>
+	/// An item that is displayed in a <see cref="GameModeListView"/>.
+	/// </summary>
 	public interface IGameModeListViewItem
 	{
-		void SetSelected(bool p_booIsSelected);
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets whether the item is selected in the list view.
+		/// </summary>
+		/// <value>Whether the item is selected in the list view.</value>
+		bool Selected { get; set; }
+
+		#endregion
 	}
 }
