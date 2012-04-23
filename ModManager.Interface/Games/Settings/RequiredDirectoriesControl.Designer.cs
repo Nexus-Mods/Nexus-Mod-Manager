b20b10957ec9bc5b5a1a0b2afc6af4c860922fae
duskdweller <duskdweller@localhost>
2012-04-23 21:40:21 +0200

    Small changes to base files.

---
+++ ModManager.Interface/Games/Settings/RequiredDirectoriesControl.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Nexus.Client.Games.Settings
+{
+	partial class RequiredDirectoriesControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.butSelectInfoDirectory = new System.Windows.Forms.Button();
+			this.lblInstallInfoPrompt = new System.Windows.Forms.Label();
+			this.tbxInstallInfo = new System.Windows.Forms.TextBox();
+			this.lblInstallInfoLabel = new System.Windows.Forms.Label();
+			this.butSelectModDirectory = new System.Windows.Forms.Button();
+			this.lblModPrompt = new System.Windows.Forms.Label();
+			this.tbxModDirectory = new System.Windows.Forms.TextBox();
+			this.lblModDirectoryLabel = new System.Windows.Forms.Label();
+			this.erpErrors = new System.Windows.Forms.ErrorProvider(this.components);
+			this.fbdDirectory = new System.Windows.Forms.FolderBrowserDialog();
+			((System.ComponentModel.ISupportInitialize)(this.erpErrors)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// butSelectInfoDirectory
+			// 
+			this.butSelectInfoDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.butSelectInfoDirectory.AutoSize = true;
+			this.butSelectInfoDirectory.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.butSelectInfoDirectory.Location = new System.Drawing.Point(394, 57);
+			this.butSelectInfoDirectory.Name = "butSelectInfoDirectory";
+			this.butSelectInfoDirectory.Size = new System.Drawing.Size(26, 23);
+			this.butSelectInfoDirectory.TabIndex = 12;
+			this.butSelectInfoDirectory.Text = "...";
+			this.butSelectInfoDirectory.UseVisualStyleBackColor = true;
+			this.butSelectInfoDirectory.Click += new System.EventHandler(this.butSelectInfoDirectory_Click);
+			// 
+			// lblInstallInfoPrompt
+			// 
+			this.lblInstallInfoPrompt.AutoSize = true;
+			this.lblInstallInfoPrompt.Location = new System.Drawing.Point(3, 43);
+			this.lblInstallInfoPrompt.Name = "lblInstallInfoPrompt";
+			this.lblInstallInfoPrompt.Size = new System.Drawing.Size(336, 13);
+			this.lblInstallInfoPrompt.TabIndex = 14;
+			this.lblInstallInfoPrompt.Text = "Choose the directory where you would like to store your {0} install info.";
+			// 
+			// tbxInstallInfo
+			// 
+			this.tbxInstallInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tbxInstallInfo.Location = new System.Drawing.Point(102, 59);
+			this.tbxInstallInfo.Name = "tbxInstallInfo";
+			this.tbxInstallInfo.Size = new System.Drawing.Size(286, 20);
+			this.tbxInstallInfo.TabIndex = 10;
+			// 
+			// lblInstallInfoLabel
+			// 
+			this.lblInstallInfoLabel.AutoSize = true;
+			this.lblInstallInfoLabel.Location = new System.Drawing.Point(38, 62);
+			this.lblInstallInfoLabel.Name = "lblInstallInfoLabel";
+			this.lblInstallInfoLabel.Size = new System.Drawing.Size(58, 13);
+			this.lblInstallInfoLabel.TabIndex = 13;
+			this.lblInstallInfoLabel.Text = "Install Info:";
+			// 
+			// butSelectModDirectory
+			// 
+			this.butSelectModDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.butSelectModDirectory.AutoSize = true;
+			this.butSelectModDirectory.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.butSelectModDirectory.Location = new System.Drawing.Point(394, 14);
+			this.butSelectModDirectory.Name = "butSelectModDirectory";
+			this.butSelectModDirectory.Size = new System.Drawing.Size(26, 23);
+			this.butSelectModDirectory.TabIndex = 9;
+			this.butSelectModDirectory.Text = "...";
+			this.butSelectModDirectory.UseVisualStyleBackColor = true;
+			this.butSelectModDirectory.Click += new System.EventHandler(this.butSelectModDirectory_Click);
+			// 
+			// lblModPrompt
+			// 
+			this.lblModPrompt.AutoSize = true;
+			this.lblModPrompt.Location = new System.Drawing.Point(3, 0);
+			this.lblModPrompt.Name = "lblModPrompt";
+			this.lblModPrompt.Size = new System.Drawing.Size(316, 13);
+			this.lblModPrompt.TabIndex = 11;
+			this.lblModPrompt.Text = "Choose the directory where you would like to store your {0} Mods.";
+			// 
+			// tbxModDirectory
+			// 
+			this.tbxModDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tbxModDirectory.Location = new System.Drawing.Point(102, 16);
+			this.tbxModDirectory.Name = "tbxModDirectory";
+			this.tbxModDirectory.Size = new System.Drawing.Size(286, 20);
+			this.tbxModDirectory.TabIndex = 7;
+			// 
+			// lblModDirectoryLabel
+			// 
+			this.lblModDirectoryLabel.AutoSize = true;
+			this.lblModDirectoryLabel.Location = new System.Drawing.Point(20, 19);
+			this.lblModDirectoryLabel.Name = "lblModDirectoryLabel";
+			this.lblModDirectoryLabel.Size = new System.Drawing.Size(76, 13);
+			this.lblModDirectoryLabel.TabIndex = 8;
+			this.lblModDirectoryLabel.Text = "Mod Directory:";
+			// 
+			// erpErrors
+			// 
+			this.erpErrors.ContainerControl = this;
+			// 
+			// RequiredDirectoriesControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.butSelectInfoDirectory);
+			this.Controls.Add(this.lblInstallInfoPrompt);
+			this.Controls.Add(this.tbxInstallInfo);
+			this.Controls.Add(this.lblInstallInfoLabel);
+			this.Controls.Add(this.butSelectModDirectory);
+			this.Controls.Add(this.lblModPrompt);
+			this.Controls.Add(this.tbxModDirectory);
+			this.Controls.Add(this.lblModDirectoryLabel);
+			this.Name = "RequiredDirectoriesControl";
+			this.Size = new System.Drawing.Size(443, 405);
+			((System.ComponentModel.ISupportInitialize)(this.erpErrors)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button butSelectInfoDirectory;
+		private System.Windows.Forms.Label lblInstallInfoPrompt;
+		private System.Windows.Forms.TextBox tbxInstallInfo;
+		private System.Windows.Forms.Label lblInstallInfoLabel;
+		private System.Windows.Forms.Button butSelectModDirectory;
+		private System.Windows.Forms.Label lblModPrompt;
+		private System.Windows.Forms.TextBox tbxModDirectory;
+		private System.Windows.Forms.Label lblModDirectoryLabel;
+		private System.Windows.Forms.ErrorProvider erpErrors;
+		private System.Windows.Forms.FolderBrowserDialog fbdDirectory;
+	}
+}
---
+++ ModManager.Interface/ModManagement/BasicInstallTask.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Nexus.Client.Mods;
+using Nexus.Client.PluginManagement;
+using Nexus.Client.Games;
+using Nexus.Client.BackgroundTasks;
+
+namespace Nexus.Client.ModManagement
+{
+	/// <summary>
+	/// Performs a standard mod installation.
+	/// </summary>
+	/// <remarks>
+	/// A basic install installs all of the files in the mod to the installation directory,
+	/// and activates all plugin files.
+	/// </remarks>
+	public class BasicInstallTask : ThreadedBackgroundTask
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the mod being installed.
+		/// </summary>
+		/// <value>The mod being installed.</value>
+		protected IMod Mod { get; set; }
+
+		/// <summary>
+		/// Gets the current game mode.
+		/// </summary>
+		/// <value>The the current game mode.</value>
+		protected IGameMode GameMode { get; private set; }
+
+		/// <summary>
+		/// Gets or sets the installer to use to install files.
+		/// </summary>
+		/// <value>The installer to use to install files.</value>
+		protected IModFileInstaller FileInstaller { get; set; }
+
+		/// <summary>
+		/// Gets the manager to use to manage plugins.
+		/// </summary>
+		/// <value>The manager to use to manage plugins.</value>
+		protected IPluginManager PluginManager { get; private set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_modMod">The mod being installed.</param>
+		/// <param name="p_gmdGameMode">The the current game mode.</param>
+		/// <param name="p_mfiFileInstaller">The file installer to use.</param>
+		/// <param name="p_pmgPluginManager">The plugin manager.</param>
+		public BasicInstallTask(IMod p_modMod, IGameMode p_gmdGameMode, IModFileInstaller p_mfiFileInstaller, IPluginManager p_pmgPluginManager)
+		{
+			Mod = p_modMod;
+			GameMode = p_gmdGameMode;
+			FileInstaller = p_mfiFileInstaller;
+			PluginManager = p_pmgPluginManager;
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Runs the basic install task.
+		/// </summary>
+		/// <returns><c>true</c> if the installation succeed;
+		/// <c>false</c> otherwise.</returns>
+		public bool Execute()
+		{
+			OverallMessage = "Installing Mod...";
+			ShowItemProgress = false;
+			OverallProgressStepSize = 1;
+			return (bool)StartWait();
+		}
+
+		/// <summary>
+		/// The method that is called to start the backgound task.
+		/// </summary>
+		/// <remarks>
+		/// This method installs all of the files in the <see cref="IMod"/> being installed.
+		/// </remarks>
+		/// <param name="p_objArgs">Arguments to for the task execution.</param>
+		/// <returns>A return value.</returns>
+		protected override object DoWork(object[] p_objArgs)
+		{
+			char[] chrDirectorySeperators = new char[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar };
+			List<string> lstFiles = Mod.GetFileList();
+			OverallProgressMaximum = lstFiles.Count;
+			foreach (string strFile in lstFiles)
+			{
+				if (Status == TaskStatus.Cancelling)
+					return false;
+				string strFixedPath = GameMode.GetModFormatAdjustedPath(Mod.Format, strFile);
+				FileInstaller.InstallFileFromMod(strFile, strFixedPath);
+				if (PluginManager.IsActivatiblePluginFile(strFixedPath))
+					PluginManager.ActivatePlugin(strFixedPath);
+				StepOverallProgress();
+			}
+			return true;
+		}
+	}
+}
