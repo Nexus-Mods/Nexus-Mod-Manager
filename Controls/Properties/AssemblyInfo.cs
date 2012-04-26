6a2eedb70921b4fa3767062514ac9831a3ba9421
kaburke <kaburke@localhost>
2012-04-26 11:45:47 -0600

    Changed error message that is displayed when a supported game is not installed.

---
+++ Controls/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Controls")]
+[assembly: AssemblyDescription("A control library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Black Tree Gaming")]
+[assembly: AssemblyProduct("Controls")]
+[assembly: AssemblyCopyright("Copyright © Black Tree Gaming 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("27580c05-bf72-43cc-a9b7-46b6d66944ef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1")]
+// AssemblyFileVersionFormat("1.1.*")
+[assembly: AssemblyFileVersion("1.1.4499.30445")]
