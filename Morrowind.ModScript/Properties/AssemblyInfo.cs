ac6ee032b04db9d7a142bc040bb8145c010b246e
kaburke <kaburke@localhost>
2012-04-26 14:14:44 -0600

    Incremented version to 0.17.1

--- Morrowind.ModScript/Properties/AssemblyInfo.cs
+++ Morrowind.ModScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4499.30450")]
+[assembly: AssemblyFileVersion("1.1.4499.36423")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
