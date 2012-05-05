258ee4ce69a6a4c7ada1113341e2b5b58ab57fec
kaburke <kaburke@localhost>
2012-05-04 19:06:38 -0600

    Closing ApplicationInitializer bugfix branch.

---
+++ ModManager.Interface/Mods/IModInfo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Xml;
+using Nexus.Client.Util;
+
+namespace Nexus.Client.Mods
+{
+	/// <summary>
+	/// A contract specifying an object that contains information about a mod.
+	/// </summary>
+	public interface IModInfo
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the Id of the mod.
+		/// </summary>
+		/// <remarks>The id of the mod</remarks>
+		string Id { get; }
+
+		/// <summary>
+		/// Gets or sets the name of the mod.
+		/// </summary>
+		/// <value>The name of the mod.</value>
+		string ModName { get; }
+
+		/// <summary>
+		/// Gets or sets the human readable form of the mod's version.
+		/// </summary>
+		/// <value>The human readable form of the mod's version.</value>
+		string HumanReadableVersion { get; }
+
+		/// <summary>
+		/// Gets or sets the version of the mod.
+		/// </summary>
+		/// <value>The version of the mod.</value>
+		Version MachineVersion { get; }
+
+		/// <summary>
+		/// Gets or sets the author of the mod.
+		/// </summary>
+		/// <value>The author of the mod.</value>
+		string Author { get; }
+
+		/// <summary>
+		/// Gets or sets the description of the mod.
+		/// </summary>
+		/// <value>The description of the mod.</value>
+		string Description { get; }
+
+		/// <summary>
+		/// Gets or sets the website of the mod.
+		/// </summary>
+		/// <value>The website of the mod.</value>
+		Uri Website { get; }
+
+		/// <summary>
+		/// Gets or sets the mod's screenshot.
+		/// </summary>
+		/// <value>The mod's screenshot.</value>
+		ExtendedImage Screenshot { get; }
+
+		#endregion
+
+		/// <summary>
+		/// Updates the object's proerties to the values of the
+		/// given <see cref="IModInfo"/>.
+		/// </summary>
+		/// <param name="p_mifInfo">The <see cref="IModInfo"/> whose values
+		/// are to be used to update this object's properties.</param>
+		/// <param name="p_booOverwriteAllValues">Whether to overwrite the current info values,
+		/// or just the empty ones.</param>
+		void UpdateInfo(IModInfo p_mifInfo, bool p_booOverwriteAllValues);
+	}
+}
---
+++ ModScript/ModScriptSelectOption.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Nexus.Client.ModManagement.Scripting.ModScript
+{
+	/// <summary>
+	/// Describes an option in a Mod Script script's select statement.
+	/// </summary>
+	[Serializable]
+	public class ModScriptSelectOption
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets the name of the option.
+		/// </summary>
+		/// <value>The name of the option.</value>
+		public string Name { get; private set; }
+
+		/// <summary>
+		/// Gets the description of the option.
+		/// </summary>
+		/// <value>The description of the option.</value>
+		public string Description { get; private set; }
+
+		/// <summary>
+		/// Gets the path in the mod of the option's image.
+		/// </summary>
+		/// <value>The path in the mod of the option's image.</value>
+		public string ImagePath { get; private set; }
+
+		/// <summary>
+		/// Gets or sets whether the option is a default.
+		/// </summary>
+		/// <value>Whether the option is a default.</value>
+		public bool IsDefault { get; set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_strName">The name of the option.</param>
+		/// <param name="p_booIsDefault">Whether the option is a default.</param>
+		/// <param name="p_strDescription">The description of the option.</param>
+		/// <param name="p_strImagePath">The path in the mod of the option's image.</param>
+		public ModScriptSelectOption(string p_strName, bool p_booIsDefault, string p_strDescription, string p_strImagePath)
+		{
+			Name = p_strName;
+			IsDefault = p_booIsDefault;
+			Description = p_strDescription;
+			ImagePath = p_strImagePath;
+		}
+
+		#endregion
+	}
+}
