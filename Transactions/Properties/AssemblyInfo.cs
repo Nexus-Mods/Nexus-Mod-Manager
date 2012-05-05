9f7f5387734ed571645173d19996ff5838b83db1
kaburke <kaburke@localhost>
2012-05-05 15:52:58 -0600

    Closing IPC bug fix branch.

--- Transactions/Properties/AssemblyInfo.cs
+++ Transactions/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4499.36417")]
+[assembly: AssemblyFileVersion("1.1.4508.37167")]
