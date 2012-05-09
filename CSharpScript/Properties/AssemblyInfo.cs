9f5430c1dc62fff5bd0fd6485ed7b381ef2b52ac
duskdweller <duskdweller@localhost>
2012-05-09 16:09:38 +0200

    This solves the bug with some corrupt nxm links (dobule / before files resulting in a null FileId)

--- CSharpScript/Properties/AssemblyInfo.cs
+++ CSharpScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4508.37169")]
+[assembly: AssemblyFileVersion("1.1.4512.25369")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
