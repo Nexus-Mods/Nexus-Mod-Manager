50ffc81852df0022116e0977537a16e7c03aefbc
kaburke <kaburke@localhost>
2012-05-17 09:40:47 -0600

    Closing branch in preparation for merge.

--- ModScript/Properties/AssemblyInfo.cs
+++ ModScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.4518.35895")]
+[assembly: AssemblyFileVersion("1.1.4520.27669")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
