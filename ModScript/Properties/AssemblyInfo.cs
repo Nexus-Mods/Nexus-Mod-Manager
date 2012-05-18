86132a58c6b6ddfdf0ab2d8bb4b4089cdcb61c71
kaburke <kaburke@localhost>
2012-05-18 14:43:33 -0600

    Incremented version to 0.18.2

--- ModScript/Properties/AssemblyInfo.cs
+++ ModScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.4520.28388")]
+[assembly: AssemblyFileVersion("1.1.4521.37288")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
