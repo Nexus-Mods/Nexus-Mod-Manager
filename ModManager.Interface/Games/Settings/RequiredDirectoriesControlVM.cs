7521186da09bdf9626e369343d6339e73c94b836
duskdweller <duskdweller@localhost>
2012-05-24 15:17:48 +0200

    Fix for the renaming bug (if renaming a mod changed his position in the list).

---
+++ ModManager.Interface/Games/Settings/RequiredDirectoriesControlVM.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.IO;
+using Nexus.Client.Util;
+using Nexus.Client.Controls;
+
+namespace Nexus.Client.Games.Settings
+{
+	/// <summary>
+	/// This class encapsulates the data and the operations presented by UI
+	/// elements that allow the selection of required directories.
+	/// </summary>
+	public class RequiredDirectoriesControlVM : ObservableObject
+	{
+		private string m_strInstallInfoDirectory = null;
+		private string m_strModDirectory = null;
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the descriptor of the current game mode.
+		/// </summary>
+		/// <value>The descriptor of the current game mode.</value>
+		public IGameModeDescriptor GameModeDescriptor { get; private set; }
+
+		/// <summary>
+		/// Gets the application's envrionment info.
+		/// </summary>
+		/// <value>The application's envrionment info.</value>
+		public IEnvironmentInfo EnvironmentInfo { get; private set; }
+
+		/// <summary>
+		/// Gets the name of the games mode currently being managed.
+		/// </summary>
+		/// <value>The name of the games mode currently being managed.</value>
+		public string GameModeName
+		{
+			get
+			{
+				return GameModeDescriptor.Name;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the directory where installation information is stored for this game mode.
+		/// </summary>
+		/// <remarks>
+		/// This is where install logs, overwrites, and the like are stored.
+		/// </remarks>
+		/// <value>The directory where installation information is stored for this game mode.</value>
+		public string InstallInfoDirectory
+		{
+			get
+			{
+				return m_strInstallInfoDirectory;
+			}
+			set
+			{
+				SetPropertyIfChanged(ref m_strInstallInfoDirectory, value, () => InstallInfoDirectory);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the path of the directory where this Game Mode's mods are stored.
+		/// </summary>
+		/// <value>The path of the directory where this Game Mode's mods are stored.</value>
+		public string ModDirectory
+		{
+			get
+			{
+				return m_strModDirectory;
+			}
+			set
+			{
+				SetPropertyIfChanged(ref m_strModDirectory, value, () => ModDirectory);
+			}
+		}
+
+		/// <summary>
+		/// Gets the validation errors for the current values.
+		/// </summary>
+		/// <value>The validation errors for the current values.</value>
+		public ErrorContainer Errors { get; private set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given dependencies.
+		/// </summary>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		/// <param name="p_gmdGameModeInfo">The descriptor of the current game mode.</param>
+		public RequiredDirectoriesControlVM(IEnvironmentInfo p_eifEnvironmentInfo, IGameModeDescriptor p_gmdGameModeInfo)
+		{
+			EnvironmentInfo = p_eifEnvironmentInfo;
+			GameModeDescriptor = p_gmdGameModeInfo;
+			Errors = new ErrorContainer();
+		}
+
+		#endregion
+
+		#region Validation
+
+		/// <summary>
+		/// Validates the specified directory.
+		/// </summary>
+		/// <returns><c>true</c> if the specified directory is valid;
+		/// <c>false</c> otherwise.</returns>
+		protected bool ValidateDirectory(string p_strPath, string p_strPathName, string p_strProperty)
+		{
+			Errors.Clear(p_strProperty);
+			if (String.IsNullOrEmpty(p_strPath))
+			{
+				Errors.SetError(p_strProperty, String.Format("You must select a {0}.", p_strPathName));
+				return false;
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// Validates the selected mod directory.
+		/// </summary>
+		/// <returns><c>true</c> if the selected mod directory is valid;
+		/// <c>false</c> otherwise.</returns>
+		protected bool ValidateModDirectory()
+		{
+			return ValidateDirectory(ModDirectory, "Mod Directory", ObjectHelper.GetPropertyName(() => ModDirectory));
+		}
+
+		/// <summary>
+		/// Validates the selected install info directory.
+		/// </summary>
+		/// <returns><c>true</c> if the selected install info directory is valid;
+		/// <c>false</c> otherwise.</returns>
+		protected bool ValidateInstallInfoDirectory()
+		{
+			return ValidateDirectory(InstallInfoDirectory, "Install Info Directory", ObjectHelper.GetPropertyName(() => InstallInfoDirectory));
+		}
+
+		/// <summary>
+		/// Validates the settings on this control.
+		/// </summary>
+		/// <returns><c>true</c> if the settings are valid;
+		/// <c>false</c> otherwise.</returns>
+		public bool ValidateSettings()
+		{
+			return ValidateModDirectory() && ValidateInstallInfoDirectory();
+		}
+
+
+		#endregion
+
+		/// <summary>
+		/// Loads the user's settings into the control.
+		/// </summary>
+		public void LoadSettings()
+		{
+			string strInstalationPath = EnvironmentInfo.Settings.InstallationPaths[GameModeDescriptor.ModeId];
+
+			string strDirectory = null;
+			bool booRetrieved = false;
+			if (EnvironmentInfo.Settings.DelayedSettings.ContainsKey(GameModeDescriptor.ModeId))
+				booRetrieved = EnvironmentInfo.Settings.DelayedSettings[GameModeDescriptor.ModeId].TryGetValue(String.Format("ModFolder~{0}", GameModeDescriptor.ModeId), out strDirectory);
+			if (!booRetrieved)
+				EnvironmentInfo.Settings.ModFolder.TryGetValue(GameModeDescriptor.ModeId, out strDirectory);
+			if (String.IsNullOrEmpty(strDirectory))
+			{
+				string strDefault = Path.Combine(Path.Combine(Path.Combine(Path.Combine(Path.GetPathRoot(strInstalationPath), "Games"), EnvironmentInfo.Settings.ModManagerName), GameModeDescriptor.ModeId), "Mods");
+				strDirectory = strDefault;
+			}
+			ModDirectory = strDirectory;
+
+			strDirectory = null;
+			booRetrieved = false;
+			if (EnvironmentInfo.Settings.DelayedSettings.ContainsKey(GameModeDescriptor.ModeId))
+				booRetrieved = EnvironmentInfo.Settings.DelayedSettings[GameModeDescriptor.ModeId].TryGetValue(String.Format("InstallInfoFolder~{0}", GameModeDescriptor.ModeId), out strDirectory);
+			if (!booRetrieved)
+				EnvironmentInfo.Settings.InstallInfoFolder.TryGetValue(GameModeDescriptor.ModeId, out strDirectory);
+			if (String.IsNullOrEmpty(strDirectory))
+			{
+				string strDefault = Path.Combine(Path.Combine(Path.Combine(Path.Combine(Path.GetPathRoot(strInstalationPath), "Games"), EnvironmentInfo.Settings.ModManagerName), GameModeDescriptor.ModeId), "Install Info");
+				strDirectory = strDefault;
+			}
+			InstallInfoDirectory = strDirectory;
+
+			ValidateSettings();
+		}
+
+		/// <summary>
+		/// Persists the settings on this control.
+		/// </summary>
+		/// <param name="p_booDelaySettings">Whether the settings should be delayed until the next application restart.</param>
+		public void SaveSettings(bool p_booDelaySettings)
+		{
+			if (!String.Equals(EnvironmentInfo.Settings.InstallInfoFolder[GameModeDescriptor.ModeId], InstallInfoDirectory))
+			{
+				if (p_booDelaySettings)
+					EnvironmentInfo.Settings.DelayedSettings[GameModeDescriptor.ModeId].Add(String.Format("InstallInfoFolder~{0}", GameModeDescriptor.ModeId), InstallInfoDirectory);
+				else
+					EnvironmentInfo.Settings.InstallInfoFolder[GameModeDescriptor.ModeId] = InstallInfoDirectory;
+			}
+			if (!String.Equals(EnvironmentInfo.Settings.ModFolder[GameModeDescriptor.ModeId], ModDirectory))
+			{
+				if (p_booDelaySettings)
+					EnvironmentInfo.Settings.DelayedSettings[GameModeDescriptor.ModeId].Add(String.Format("ModFolder~{0}", GameModeDescriptor.ModeId), ModDirectory);
+				else
+					EnvironmentInfo.Settings.ModFolder[GameModeDescriptor.ModeId] = ModDirectory;
+			}
+			EnvironmentInfo.Settings.Save();
+		}
+	}
+}
---
+++ NexusClient/ModManagement/InstallationLog/Upgraders/Upgrade0300Task.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Nexus.Client.Mods;
+
+namespace Nexus.Client.ModManagement.InstallationLog.Upgraders
+{
+	/// <summary>
+	/// Upgrades the Install Log to the current version from version 0.3.0.0.
+	/// </summary>
+	public class Upgrade0300Task : UpgradeTask
+	{
+		private static readonly Version SUPPORTED_VERSION = new Version("0.3.0.0");
+
+		/// <summary>
+		/// Upgrades the install log.
+		/// </summary>
+		/// <param name="p_mrgModRegistry">The <see cref="ModRegistry"/> that contains the list
+		/// of managed mods.</param>
+		/// <param name="p_strModInstallDirectory">The path of the directory where all of the mods are installed.</param>
+		/// <param name="p_strLogPath">The path from which to load the install log information.</param>
+		protected override void UpgradeInstallLog(string p_strLogPath, string p_strModInstallDirectory, ModRegistry p_mrgModRegistry)
+		{
+			if (!File.Exists(p_strLogPath))
+				return;
+			string strModInstallDirectory = p_strModInstallDirectory.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar).Trim(Path.DirectorySeparatorChar) + Path.DirectorySeparatorChar;
+			XDocument docLog = XDocument.Load(p_strLogPath);
+
+			string strLogVersion = docLog.Element("installLog").Attribute("fileVersion").Value;
+			if (!SUPPORTED_VERSION.ToString().Equals(strLogVersion))
+				throw new UpgradeException(String.Format("Cannot upgrade Install Log version: {0} Expecting {1}", strLogVersion, SUPPORTED_VERSION));
+
+			XElement xelModList = docLog.Descendants("modList").FirstOrDefault();
+			if (xelModList != null)
+			{
+				foreach (XElement xelMod in xelModList.Elements("mod"))
+				{
+					string strModPath = xelMod.Attribute("path").Value;
+					if (InstallLog.OriginalValueMod.Filename.Equals(strModPath))
+						xelMod.Add(new XElement("name", new XText(InstallLog.OriginalValueMod.ModName)));
+					else if (InstallLog.ModManagerValueMod.Filename.Equals(strModPath))
+						xelMod.Add(new XElement("name", new XText(InstallLog.ModManagerValueMod.ModName)));
+					else
+					{
+						strModPath = Path.GetFileName(xelMod.Attribute("path").Value);
+						IMod modMod = p_mrgModRegistry.RegisteredMods.FirstOrDefault(x => Path.GetFileName(x.Filename).Equals(strModPath, StringComparison.OrdinalIgnoreCase));
+						if (modMod != null)
+						{
+							strModPath = modMod.Filename.Substring(strModInstallDirectory.Length);
+							xelMod.Attribute("path").Value = strModPath;
+							xelMod.Add(new XElement("name",
+													new XText(modMod.ModName)));
+						}
+						else
+							xelMod.Add(new XElement("name", new XText("MISSING MOD")));
+					}
+				}
+			}
+
+			docLog.Element("installLog").Attribute("fileVersion").Value = InstallLog.CurrentVersion.ToString();
+
+			if (!Directory.Exists(Path.GetDirectoryName(p_strLogPath)))
+				Directory.CreateDirectory(Path.GetDirectoryName(p_strLogPath));
+			docLog.Save(p_strLogPath);
+		}
+	}
+}
