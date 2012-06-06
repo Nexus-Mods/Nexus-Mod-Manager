6a6b3f4cdd755b6fb085e4b75cf53217bce98580
kaburke <kaburke@localhost>
2012-06-06 16:50:14 -0600

    Incremented version to 0.18.7

--- Morrowind.ModScript/Properties/AssemblyInfo.cs
+++ Morrowind.ModScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4540.35143")]
+[assembly: AssemblyFileVersion("1.1.4540.41067")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
--- WorldOfTanks.ModScript/Properties/AssemblyInfo.cs
+++ WorldOfTanks.ModScript/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4540.35143")]
+[assembly: AssemblyFileVersion("1.1.4540.41068")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
