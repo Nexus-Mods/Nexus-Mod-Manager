5baaf06b6a29f517dc5060cc2e81dd09f7802f34
kaburke <kaburke@localhost>
2012-06-06 12:49:54 -0600

    Closing branch in preparation for merge.

--- Oblivion/Properties/AssemblyInfo.cs
+++ Oblivion/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4524.1528")]
+[assembly: AssemblyFileVersion("1.1.4540.33645")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
