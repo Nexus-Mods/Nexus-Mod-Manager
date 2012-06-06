a972b6f3c6068b30942a3d1046e221970949d8b3
kaburke <kaburke@localhost>
2012-06-06 16:25:17 -0600

    Changed how messages are passed between threads so that exceptions raised on the extractor thread can be bubbled on the calling thread.

---
+++ ModManager.Interface/Games/Tools/ITool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Nexus.Client.Commands;
+
+namespace Nexus.Client.Games.Tools
+{
+	/// <summary>
+	/// Describes the methods and properties of a game mode tool.
+	/// </summary>
+	/// <remarks>
+	/// A game mode tool provides custom functionality for a specific game mode.
+	/// </remarks>
+	public interface ITool
+	{
+		/// <summary>
+		/// Notifies listeners that the tool wants a view displayed.
+		/// </summary>
+		event EventHandler<DisplayToolViewEventArgs> DisplayToolView;
+
+		/// <summary>
+		/// Notifies listeners that the tool wants a view closed.
+		/// </summary>
+		event EventHandler<DisplayToolViewEventArgs> CloseToolView;
+
+		/// <summary>
+		/// Gets the command to launch the tool.
+		/// </summary>
+		/// <value>The command to launch the tool.</value>
+		Command LaunchCommand { get; }
+	}
+}
