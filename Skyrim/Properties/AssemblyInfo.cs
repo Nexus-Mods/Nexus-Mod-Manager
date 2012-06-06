cfee7a4395b5b384740c25e05ed18576772ada35
kaburke <kaburke@localhost>
2012-06-06 13:04:40 -0600

    Incremented version to 0.18.5

--- Skyrim/Properties/AssemblyInfo.cs
+++ Skyrim/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4526.28709")]
+[assembly: AssemblyFileVersion("1.1.4540.34301")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
