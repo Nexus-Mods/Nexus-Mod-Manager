78bc8c70579af1a821bc86377475da555e9af4ec
kaburke <kaburke@localhost>
2012-06-06 12:49:30 -0600

    Corrected how malformed archives or archive with missing parts are handled. Instead of throwing an exception, the task sets the status message and terminates in an error state.

--- NexusClient/ModAuthoring/ModBuilder.cs
+++ NexusClient/ModAuthoring/ModBuilder.cs
@@ -96,21 +96,14 @@ namespace Nexus.Client.ModAuthoring
 		/// which indicates the type of source from which the mod is being built.
 		/// </remarks>
 		/// <param name="p_objArgs">Arguments to for the task execution.</param>
+		/// <param name="p_strMessage">The message describing the state of the task.</param>
 		/// <returns>A return value.</returns>
-		protected override object DoWork(object[] p_objArgs)
+		protected override object DoWork(object[] p_objArgs, out string p_strMessage)
 		{
 			switch ((Sources)p_objArgs[0])
 			{
 				case Sources.Archive:
-                    try
-                    {
-                        return DoFromArchive((IModFormatRegistry)p_objArgs[1], (string)p_objArgs[2], (ConfirmOverwriteCallback)p_objArgs[3]);
-                    }
-                    catch (NotImplementedException ex)
-                    {
-                        throw ex;
-                    }
-
+					return DoFromArchive((IModFormatRegistry)p_objArgs[1], (string)p_objArgs[2], (ConfirmOverwriteCallback)p_objArgs[3], out p_strMessage);
 			}
 			throw new ArgumentException("Unrecognized activity source.");
 		}
@@ -136,7 +129,7 @@ namespace Nexus.Client.ModAuthoring
 			OverallMessage = "Building Mod...";
 			Sources srcModSource = Sources.Archive;
 			if (String.IsNullOrEmpty(p_strFilePath) || !File.Exists(p_strFilePath))
-				throw new ArgumentException("The given file path does not exist: " +  p_strFilePath);
+				throw new ArgumentException("The given file path does not exist: " + p_strFilePath);
 			if (!Archive.IsArchive(p_strFilePath))
 			{
 				Status = TaskStatus.Error;
@@ -144,7 +137,7 @@ namespace Nexus.Client.ModAuthoring
 				return;
 			}
 
-            Start(srcModSource, p_mfrFormats, p_strFilePath, p_dlgConfirmOverwrite);
+			Start(srcModSource, p_mfrFormats, p_strFilePath, p_dlgConfirmOverwrite);
 		}
 
 		#region From Archive
@@ -160,10 +153,12 @@ namespace Nexus.Client.ModAuthoring
 		/// <param name="p_mfrFormats">The registry of supported mod formats.</param>
 		/// <param name="p_strArchivePath">The archive to build into a mod.</param>
 		/// <param name="p_dlgConfirmOverwrite">The delegate to call to resolve conflicts with existing files.</param>
+		/// <param name="p_strMessage">The message describing the state of the task.</param>
 		/// <returns>The paths to the new mods.</returns>
 		/// <exception cref="ArgumentException">Thrown if the specified path is not an archive.</exception>
-		private IList<string> DoFromArchive(IModFormatRegistry p_mfrFormats, string p_strArchivePath, ConfirmOverwriteCallback p_dlgConfirmOverwrite)
+		private IList<string> DoFromArchive(IModFormatRegistry p_mfrFormats, string p_strArchivePath, ConfirmOverwriteCallback p_dlgConfirmOverwrite, out string p_strMessage)
 		{
+			p_strMessage = null;
 			Trace.TraceInformation(String.Format("[{0}] Adding mod from archive.", p_strArchivePath));
 			if (String.IsNullOrEmpty(p_strArchivePath) || !File.Exists(p_strArchivePath) || !Archive.IsArchive(p_strArchivePath))
 				throw new ArgumentException("The specified path is not an archive file.", "p_strArchivePath");
@@ -211,80 +206,78 @@ namespace Nexus.Client.ModAuthoring
 			StepOverallProgress();
 
 			string strTmpPath = null;
-            try
-            {
-                using (SevenZipExtractor szeExtractor = Archive.GetExtractor(p_strArchivePath))
-                {
-                    if ((mftDestFormat != null) && (szeExtractor.VolumeFileNames.Count > 1) ||
-                        (lstModsInArchive.Count > 0))
-                    {
-                        ItemMessage = "Extracting archive...";
-                        ItemProgress = 0;
-                        ItemProgressMaximum = szeExtractor.ArchiveFileNames.Count;
-                        strTmpPath = FileUtility.CreateTempDirectory();
-                        szeExtractor.FileExtractionStarted += new EventHandler<FileInfoEventArgs>(Extractor_FileExtractionStarted);
-                        szeExtractor.FileExtractionFinished += new EventHandler<FileInfoEventArgs>(Extractor_FileExtractionFinished);
-                        try
-                        {
-                            szeExtractor.ExtractArchive(strTmpPath);
-                        }
-                        catch (FileNotFoundException ex)
-                        {
-                            throw new NotImplementedException(ex.Message);
-                        }
-                        for (Int32 i = 0; i < lstModsInArchive.Count; i++)
-                            lstModsInArchive[i] = Path.Combine(strTmpPath, lstModsInArchive[i]);
-                    }
-                    else
-                        lstModsInArchive.Add(p_strArchivePath);
-                }
-                StepOverallProgress();
+			try
+			{
+				using (SevenZipExtractor szeExtractor = Archive.GetExtractor(p_strArchivePath))
+				{
+					if ((mftDestFormat != null) && (szeExtractor.VolumeFileNames.Count > 1) ||
+						(lstModsInArchive.Count > 0))
+					{
+						ItemMessage = "Extracting archive...";
+						ItemProgress = 0;
+						ItemProgressMaximum = szeExtractor.ArchiveFileNames.Count;
+						strTmpPath = FileUtility.CreateTempDirectory();
+						szeExtractor.FileExtractionStarted += new EventHandler<FileInfoEventArgs>(Extractor_FileExtractionStarted);
+						szeExtractor.FileExtractionFinished += new EventHandler<FileInfoEventArgs>(Extractor_FileExtractionFinished);
+						try
+						{
+							szeExtractor.ExtractArchive(strTmpPath);
+						}
+						catch (FileNotFoundException ex)
+						{
+							Status = TaskStatus.Error;
+							p_strMessage = ex.Message;
+							return lstFoundMods;
+						}
+						for (Int32 i = 0; i < lstModsInArchive.Count; i++)
+							lstModsInArchive[i] = Path.Combine(strTmpPath, lstModsInArchive[i]);
+					}
+					else
+						lstModsInArchive.Add(p_strArchivePath);
+				}
+				StepOverallProgress();
 
-                if (!String.IsNullOrEmpty(strTmpPath) && (mftDestFormat != null))
-                {
-                    //if we have extracted the file to do format shifting
-                    if (!mftDestFormat.SupportsModCompression)
-                        return lstFoundMods;
-                    ItemMessage = "Compressing mod...";
-                    ItemProgress = 0;
-                    ItemProgressMaximum = Directory.GetFiles(strTmpPath, "*", SearchOption.AllDirectories).Length;
-                    IModCompressor mcpCompressor = mftDestFormat.GetModCompressor(EnvironmentInfo);
-                    mcpCompressor.FileCompressionFinished += new CancelEventHandler(Compressor_FileCompressionFinished);
-                    string strDest = Path.Combine(GameModeInfo.ModDirectory, Path.GetFileName(p_strArchivePath));
-                    strDest = Path.ChangeExtension(strDest, mftDestFormat.Extension);
-                    strDest = ConfirmOverwrite(p_dlgConfirmOverwrite, strDest);
-                    if (!String.IsNullOrEmpty(strDest))
-                    {
-                        mcpCompressor.Compress(strTmpPath, strDest);
-                        lstFoundMods.Add(strDest);
-                    }
-                }
-                else
-                {
-                    ItemMessage = "Copying mods...";
-                    ItemProgress = 0;
-                    ItemProgressMaximum = lstModsInArchive.Count;
-                    foreach (string strMod in lstModsInArchive)
-                    {
-                        if (Status == TaskStatus.Cancelling)
-                            return lstFoundMods;
-                        ItemMessage = String.Format("Copying mod {0}...", Path.GetFileName(strMod));
-                        string strDest = Path.Combine(GameModeInfo.ModDirectory, Path.GetFileName(strMod));
-                        strDest = ConfirmOverwrite(p_dlgConfirmOverwrite, strDest);
-                        if (!String.IsNullOrEmpty(strDest))
-                        {
-                            File.Copy(strMod, strDest, true);
-                            lstFoundMods.Add(strDest);
-                        }
-                        StepItemProgress();
-                    }
-                }
-                StepOverallProgress();
-            }
-            catch (NotImplementedException ex)
-            {
-                throw ex;
-            }
+				if (!String.IsNullOrEmpty(strTmpPath) && (mftDestFormat != null))
+				{
+					//if we have extracted the file to do format shifting
+					if (!mftDestFormat.SupportsModCompression)
+						return lstFoundMods;
+					ItemMessage = "Compressing mod...";
+					ItemProgress = 0;
+					ItemProgressMaximum = Directory.GetFiles(strTmpPath, "*", SearchOption.AllDirectories).Length;
+					IModCompressor mcpCompressor = mftDestFormat.GetModCompressor(EnvironmentInfo);
+					mcpCompressor.FileCompressionFinished += new CancelEventHandler(Compressor_FileCompressionFinished);
+					string strDest = Path.Combine(GameModeInfo.ModDirectory, Path.GetFileName(p_strArchivePath));
+					strDest = Path.ChangeExtension(strDest, mftDestFormat.Extension);
+					strDest = ConfirmOverwrite(p_dlgConfirmOverwrite, strDest);
+					if (!String.IsNullOrEmpty(strDest))
+					{
+						mcpCompressor.Compress(strTmpPath, strDest);
+						lstFoundMods.Add(strDest);
+					}
+				}
+				else
+				{
+					ItemMessage = "Copying mods...";
+					ItemProgress = 0;
+					ItemProgressMaximum = lstModsInArchive.Count;
+					foreach (string strMod in lstModsInArchive)
+					{
+						if (Status == TaskStatus.Cancelling)
+							return lstFoundMods;
+						ItemMessage = String.Format("Copying mod {0}...", Path.GetFileName(strMod));
+						string strDest = Path.Combine(GameModeInfo.ModDirectory, Path.GetFileName(strMod));
+						strDest = ConfirmOverwrite(p_dlgConfirmOverwrite, strDest);
+						if (!String.IsNullOrEmpty(strDest))
+						{
+							File.Copy(strMod, strDest, true);
+							lstFoundMods.Add(strDest);
+						}
+						StepItemProgress();
+					}
+				}
+				StepOverallProgress();
+			}
 			finally
 			{
 				if (!String.IsNullOrEmpty(strTmpPath))
@@ -311,7 +304,7 @@ namespace Nexus.Client.ModAuthoring
 			if (p_dlgConfirmOverwrite(strDest, out strDest))
 				return strDest;
 			return null;
-		}	
+		}
 
 		#endregion
 
