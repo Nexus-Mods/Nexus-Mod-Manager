736ac8656504ccda042eee8c0e887e548ebbdabb
kaburke <kaburke@localhost>
2012-06-07 16:22:53 -0600

    Added base classes for forms and user controls that use the FontProvider.

---
+++ NexusClient.Interface/UI/ManagedFontForm.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using Nexus.UI.Controls;
+
+namespace Nexus.Client.UI
+{
+	/// <summary>
+	/// A base form that has a <see cref="FontProvider"/> already declared.
+	/// </summary>
+	/// <remarks>
+	/// This is to make it easier to use the font provider.
+	/// </remarks>
+	public class ManagedFontForm : Form
+	{
+		protected FontProvider m_fpdFontProvider = null;
+
+		#region Contructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public ManagedFontForm()
+		{
+			m_fpdFontProvider = new FontProvider();
+		}
+
+		#endregion
+	}
+}
---
+++ NexusClient.Interface/UI/ManagedFontUserControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using Nexus.UI.Controls;
+
+namespace Nexus.Client.UI
+{
+	/// <summary>
+	/// A base user control that has a <see cref="FontProvider"/> already declared.
+	/// </summary>
+	/// <remarks>
+	/// This is to make it easier to use the font provider.
+	/// </remarks>
+	public class ManagedFontUserControl : UserControl
+	{
+		protected FontProvider m_fpdFontProvider = null;
+
+		#region Contructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public ManagedFontUserControl()
+		{
+			m_fpdFontProvider = new FontProvider();
+		}
+
+		#endregion
+	}
+}
