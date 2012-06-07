2b37487c35d8ede094e9c622e0e1a5ca08e48e1c
kaburke <kaburke@localhost>
2012-06-07 17:47:32 -0600

    Added small text font set.

--- NexusClient/Bootstrapper.cs
+++ NexusClient/Bootstrapper.cs
@@ -281,6 +281,7 @@ namespace Nexus.Client
 			FontSetGroup fsgDefault = new FontSetGroup(fstDefault);
 			fsgDefault.AddFontSet("StandardText", fstDefault);
 			fsgDefault.AddFontSet("HeadingText", fstDefault);
+			fsgDefault.AddFontSet("SmallText", new FontSet(new string[] { "Segoe UI", "Arial" }));
 			fsgDefault.AddFontSet("ToolbarText", new FontSet(new string[] { "Segoe UI", "Arial" }));
 			fsgDefault.AddFontSet("GameSearchText", new FontSet(new string[] { "LinBiolinum" }));
 
