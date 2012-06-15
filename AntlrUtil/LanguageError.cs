baa160cdb376352ada7c5f785520c9f6e8fb313b
duskdweller <duskdweller@localhost>
2012-06-15 12:28:27 +0200

    Properly handles PathTooLong and ContainsInvalidChars.

---
+++ AntlrUtil/LanguageError.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Nexus.Client.Util.Antlr
+{
+	/// <summary>
+	/// Describes an error genered while parsing CPL.
+	/// </summary>
+	public class LanguageError
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the line in the code where the error was encountered.
+		/// </summary>
+		/// <value>The line in the code where the error was encountered.</value>
+		public Int32 Line { get; set; }
+
+		/// <summary>
+		/// Gets or sets the column in the line in the code where
+		/// the error was encountered.
+		/// </summary>
+		/// <value>The column in the line in the code where
+		/// the error was encountered.</value>
+		public Int32 Column { get; set; }
+
+
+		/// <summary>
+		/// Gets or sets the position in the code where the error was encountered.
+		/// </summary>
+		/// <value>The position in the code where the error was encountered.</value>
+		public Int32 Position { get; set; }
+
+		/// <summary>
+		/// Gets or sets the position in the code where the error ends.
+		/// </summary>
+		/// <value>The position in the code where the error ends.</value>
+		public Int32 End { get; set; }
+
+		/// <summary>
+		/// Gets or sets the error message.
+		/// </summary>
+		/// <value>The error message.</value>
+		public string Message { get; set; }
+
+		#endregion
+
+		/// <summary>
+		/// Generates a string representation of the class.
+		/// </summary>
+		/// <remarks>
+		/// This returns an error message summarizing the error's data.
+		/// </remarks>
+		/// <returns>A string representation of the class.</returns>
+		public override string ToString()
+		{
+			return String.Format("{0},{1}: {2}", Line, Column, Message);
+		}
+	}
+}
---
+++ NexusClient/ModRepositories/Nexus/NexusModInfo.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Nexus.Client.Mods;
+
+namespace Nexus.Client.ModRepositories.Nexus
+{
+	/// <summary>
+	/// Describes the metadata of a mod in the Nexus repository.
+	/// </summary>
+	[DataContract]
+	public class NexusModInfo
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets whether the mod contains adult material.
+		/// </summary>
+		/// <value>Whether the mod contains adult material.</value>
+		[DataMember(Name = "adult")]
+		public bool IsAdult { get; set; }
+
+		/// <summary>
+		/// Gets or sets the category of the mod.
+		/// </summary>
+		/// <value>The category of the mod.</value>
+		[DataMember(Name = "category_id")]
+		public Int32 CategoryId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the description of the mod.
+		/// </summary>
+		/// <value>The description of the mod.</value>
+		[DataMember(Name = "description")]
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Gets or sets the Id of the mod.
+		/// </summary>
+		/// <value>The Id of the mod.</value>
+		[DataMember(Name = "id")]
+		public string Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the last updated date of the mod.
+		/// </summary>
+		/// <value>The last updated date of the mod.</value>
+		[DataMember(Name = "lastupdate")]
+		public DateTime? LastUpdated { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of the mod.
+		/// </summary>
+		/// <value>The name of the mod.</value>
+		[DataMember(Name = "name")]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the author of the mod.
+		/// </summary>
+		/// <value>The author of the mod.</value>
+		[DataMember(Name = "author")]
+		public string Author { get; set; }
+
+		/// <summary>
+		/// Gets or sets the owner of the mod.
+		/// </summary>
+		/// <value>The owner of the mod.</value>
+		[DataMember(Name = "owner_id")]
+		public Int32 OwnerId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the summary of the mod.
+		/// </summary>
+		/// <value>The summary of the mod.</value>
+		[DataMember(Name = "summary")]
+		public string Summary { get; set; }
+
+		/// <summary>
+		/// Gets or sets the human readable mod version.
+		/// </summary>
+		/// <value>The human readable mod version.</value>
+		[DataMember(Name = "version")]
+		public string HumanReadableVersion { get; set; }
+
+		#endregion
+	}
+}
