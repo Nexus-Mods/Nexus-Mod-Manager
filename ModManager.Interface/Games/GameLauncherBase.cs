d0c8780675e25819e7c1bbd38548370413fb55a5
duskdweller <duskdweller@localhost>
2012-07-09 15:40:27 +0200

    Adds a button to the interface that opens an explorer window to the game path, mods path, install info path.

---
+++ ModManager.Interface/Games/GameLauncherBase.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using Nexus.Client.Commands;
+
+namespace Nexus.Client.Games
+{
+	/// <summary>
+	/// A base implementation of a game launcher.
+	/// </summary>
+	/// <remarks>
+	/// This implements some common functionality for game launchers.
+	/// </remarks>
+	public abstract class GameLauncherBase : IGameLauncher
+	{
+		private List<Command> m_lstLaunchCommands = new List<Command>();
+		private Command m_cmdDefault = null;
+
+		#region IGameLauncher Members
+
+		/// <summary>
+		/// Raised when an attempt to launch the game is about to be made.
+		/// </summary>
+		public event CancelEventHandler GameLaunching = delegate { };
+
+		/// <summary>
+		/// Raised when a attempt to launch the game has been made.
+		/// </summary>
+		public event EventHandler<GameLaunchEventArgs> GameLaunched = delegate { };
+
+		/// <summary>
+		/// Gets the list of available commands that can launch the game.
+		/// </summary>
+		/// <value>The list of available commands that can launch the game.</value>
+		public IEnumerable<Command> LaunchCommands
+		{
+			get
+			{
+				return m_lstLaunchCommands;
+			}
+		}
+
+		/// <summary>
+		/// Gets the default command to use to launch the game.
+		/// </summary>
+		/// <value>The default command to use to launch the game.</value>
+		public Command DefaultLaunchCommand
+		{
+			get
+			{
+				return m_cmdDefault;
+			}
+			protected set
+			{
+				m_cmdDefault = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets the game mode currently being managed.
+		/// </summary>
+		/// <value>The game mode currently being managed.</value>
+		protected IGameMode GameMode { get; private set; }
+
+		/// <summary>
+		/// Gets the application's envrionment info.
+		/// </summary>
+		/// <value>The application's envrionment info.</value>
+		protected IEnvironmentInfo EnvironmentInfo { get; private set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given dependencies.
+		/// </summary>
+		/// <param name="p_gmdGameMode">>The game mode currently being managed.</param>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		public GameLauncherBase(IGameMode p_gmdGameMode, IEnvironmentInfo p_eifEnvironmentInfo)
+		{
+			GameMode = p_gmdGameMode;
+			EnvironmentInfo = p_eifEnvironmentInfo;
+			SetupCommands();
+		}
+
+		#endregion
+
+		#region Event Raising
+
+		/// <summary>
+		/// Raises the <see cref="GameLaunching"/> event.
+		/// </summary>
+		/// <param name="e">A <see cref="CancelEventArgs"/> describing the event arguments.</param>
+		/// <seealso cref="OnGameLaunching()"/>
+		protected virtual void OnGameLaunching(CancelEventArgs e)
+		{
+			GameLaunching(this, e);
+		}
+
+		/// <summary>
+		/// Raises the <see cref="GameLaunching"/> event.
+		/// </summary>
+		/// <returns><c>true</c> if the game launch should be cancelled;
+		/// <c>false</c> otherwise.</returns>
+		/// <seealso cref="OnGameLaunching(CancelEventArgs)"/>
+		protected bool OnGameLaunching()
+		{
+			CancelEventArgs e = new CancelEventArgs(false);
+			OnGameLaunching(e);
+			return e.Cancel;
+		}
+
+		/// <summary>
+		/// Raises the <see cref="GameLaunched"/> event.
+		/// </summary>
+		/// <param name="e">A <see cref="GameLaunchEventArgs"/> describing the event arguments.</param>
+		/// <seealso cref="OnGameLaunched(bool, string)"/>
+		protected virtual void OnGameLaunched(GameLaunchEventArgs e)
+		{
+			GameLaunched(this, e);
+		}
+
+		/// <summary>
+		/// Raises the <see cref="GameLaunched"/> event.
+		/// </summary>
+		/// <param name="p_booGameLaunched">Whether or not the game launched successfully.</param>
+		/// <param name="p_strMessage">A message to display to the user.</param>
+		/// <seealso cref="OnGameLaunched(GameLaunchEventArgs)"/>
+		protected void OnGameLaunched(bool p_booGameLaunched, string p_strMessage)
+		{
+			OnGameLaunched(new GameLaunchEventArgs(p_booGameLaunched, p_strMessage));
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Initializes the game launch commands.
+		/// </summary>
+		protected abstract void SetupCommands();
+
+		/// <summary>
+		/// Clears all the launch commands.
+		/// </summary>
+		protected void ClearLaunchCommands()
+		{
+			m_lstLaunchCommands.Clear();
+		}
+
+		/// <summary>
+		/// Adds the given launch command.
+		/// </summary>
+		/// <param name="p_cmdLaunch">The launch command to add.</param>
+		protected void AddLaunchCommand(Command p_cmdLaunch)
+		{
+			m_lstLaunchCommands.Add(p_cmdLaunch);
+		}
+
+		#region Launch Commands
+
+		/// <summary>
+		/// Launches the game.
+		/// </summary>
+		/// <remarks>
+		/// This is the root launch method that all the other launch methods call. This method
+		/// actually spawns the new process to launch the game, using the given information.
+		/// </remarks>
+		/// <param name="p_strCommand">The command to execute to launch the game.</param>
+		/// <param name="p_strCommandArgs">The command argumetns to pass to the launch command.</param>
+		protected void Launch(string p_strCommand, string p_strCommandArgs)
+		{
+			if (OnGameLaunching())
+			{
+				Trace.TraceInformation("Cancelled");
+				Trace.Unindent();
+				return;
+			}
+			try
+			{
+				ProcessStartInfo psiGameLaunch = new ProcessStartInfo();
+				if (!String.IsNullOrEmpty(p_strCommandArgs))
+					psiGameLaunch.Arguments = p_strCommandArgs;
+				psiGameLaunch.FileName = p_strCommand;
+				psiGameLaunch.WorkingDirectory = Path.GetDirectoryName(p_strCommand);
+				if (Process.Start(psiGameLaunch) == null)
+				{
+					Trace.TraceError("Failed (unknown error)");
+					Trace.Unindent();
+					OnGameLaunched(false, String.Format("Failed to launch '{0}'.", Path.GetFileName(p_strCommand)));
+					return;
+				}
+			}
+			catch (Exception ex)
+			{
+				Trace.TraceError("Failed:");
+				Trace.Indent();
+				Trace.TraceError(ex.ToString());
+				Trace.Unindent();
+				Trace.Unindent();
+				OnGameLaunched(false, String.Format("Failed to launch '{0}'{1}{2}.", Path.GetFileName(p_strCommand), Environment.NewLine, ex.Message));
+				return;
+			}
+			Trace.TraceInformation("Succeeded");
+			Trace.Unindent();
+			OnGameLaunched(true, null);
+		}
+
+		#endregion
+	}
+}
---
+++ ModManager.Interface/Games/InstallationPathAutoDetector.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using Nexus.Client.BackgroundTasks;
+using Nexus.Client.Util.Collections;
+
+namespace Nexus.Client.Games
+{
+	/// <summary>
+	/// This task searched the user's computer for the specified installation path.
+	/// </summary>
+	/// <remarks>
+	/// The task is given specific file names for which to search that indicate the
+	/// installation path being sought.
+	/// </remarks>
+	public class InstallationPathAutoDetector : ThreadedBackgroundTask
+	{
+		private System.Func<string, bool> ConfirmFoundInstallationPath;
+		private Set<string> m_setSearchedFolders = new Set<string>(StringComparer.OrdinalIgnoreCase);
+		private Set<string> m_setSkipFolders = new Set<string>(StringComparer.OrdinalIgnoreCase);
+		
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_fncConfirmFoundInstallationPath">The delegate to call to confirm if a found path
+		/// is correct.</param>
+		public InstallationPathAutoDetector(System.Func<string, bool> p_fncConfirmFoundInstallationPath)
+		{
+			ConfirmFoundInstallationPath = p_fncConfirmFoundInstallationPath ?? (x => true);
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Finds the installation path.
+		/// </summary>
+		/// <param name="p_strSearchFiles">The files to search for when auto-detecting.</param>
+		public void Detect(string[] p_strSearchFiles)
+		{
+			OverallProgressStepSize = 1;
+			ShowItemProgress = true;
+			ShowItemProgressAsMarquee = true;
+
+			Start(p_strSearchFiles);
+		}
+
+		/// <summary>
+		/// The delegate that is called to start the backgound task.
+		/// </summary>
+		/// <param name="p_objArgs">the files to search for.</param>
+		/// <returns>The auto-detected installation path.</returns>
+		protected override object DoWork(object[] p_objArgs)
+		{
+			string[] strSearchFiles = (string[])p_objArgs;
+			DriveInfo[] difDrives = DriveInfo.GetDrives();
+
+			OverallProgressMaximum = difDrives.Length * 2;
+			string strFound = null;
+			string strSystemRoot = Environment.ExpandEnvironmentVariables("%SYSTEMROOT%");
+			for (Int32 i = 0; i < 2; i++)
+			{
+				if (!"%SYSTEMROOT%".Equals(strSystemRoot))
+				{
+					if (i == 0)
+						m_setSkipFolders.Add(strSystemRoot);
+					else
+						m_setSkipFolders.Remove(strSystemRoot);
+				}
+				foreach (DriveInfo difDrive in difDrives)
+				{
+					if (Status == TaskStatus.Cancelling)
+						return null;
+					OverallMessage = String.Format("Searching {0} ({1})...", difDrive.Name, (i == 0) ? "Quick Scan" : "Deep Search");
+					if ((difDrive.DriveType != DriveType.CDRom) && difDrive.IsReady)
+						strFound = SearchToDepth(difDrive.Name, strSearchFiles, (i == 0) ? 3 : -1, 0);
+					StepOverallProgress();
+					if (!String.IsNullOrEmpty(strFound))
+						return strFound;
+				}
+			}
+			return strFound;
+		}
+
+		/// <summary>
+		/// This recursively searches the specified directory for the search files.
+		/// </summary>
+		/// <param name="p_strPath">The path of the direcotry to recursively search.</param>
+		/// <param name="p_strSearchFiles">The files to search for when auto-detecting.</param>
+		/// <param name="p_intMaxDepth">The depth to hich the search for the search files.</param>
+		/// <param name="p_intDepth">The current search depth.</param>
+		protected string SearchToDepth(string p_strPath, string[] p_strSearchFiles, Int32 p_intMaxDepth, Int32 p_intDepth)
+		{
+			if ((p_intMaxDepth > -1) && (p_intDepth > p_intMaxDepth) || m_setSkipFolders.Contains(p_strPath))
+				return null;
+			ItemMessage = p_strPath;
+			if (!m_setSearchedFolders.Contains(p_strPath))
+			{
+				m_setSearchedFolders.Add(p_strPath);
+				foreach (string strSearchFile in p_strSearchFiles)
+				{
+					if (Status == TaskStatus.Cancelling)
+						return null;
+					try
+					{
+						string[] strFoundFiles = Directory.GetFiles(p_strPath, strSearchFile, SearchOption.TopDirectoryOnly);
+						foreach (string strFoundFile in strFoundFiles)
+							if (ConfirmFoundInstallationPath(Path.GetDirectoryName(strFoundFile)))
+								return Path.GetDirectoryName(strFoundFile);
+					}
+					catch (UnauthorizedAccessException)
+					{
+						//we don't have access to the path we are trying to search, so let's bail
+						return null;
+					}
+				}
+			}
+			try
+			{
+				string[] strDirectories = Directory.GetDirectories(p_strPath);
+				foreach (string strDirectory in strDirectories)
+				{
+					if (Status == TaskStatus.Cancelling)
+						return null;
+					if (Path.GetFileName(strDirectory).StartsWith("$"))
+						continue;
+					string strFound = SearchToDepth(strDirectory, p_strSearchFiles, p_intMaxDepth, p_intDepth + 1);
+					if (!String.IsNullOrEmpty(strFound))
+						return strFound;
+				}
+			}
+			catch (UnauthorizedAccessException)
+			{
+				//we don't have access to the path we are trying to search, so let's bail
+				return null;
+			}
+			return null;
+		}
+	}
+}
