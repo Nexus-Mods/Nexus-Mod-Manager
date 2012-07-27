fab55aa18e01dea255d1beaeb2e92acf3ba5a7fe
kaburke <kaburke@localhost>
2012-07-27 09:23:41 -0600

    Incremented version to 0.19.0

--- WorldOfTanks.ModScript/Properties/AssemblyInfo.cs
+++ WorldOfTanks.ModScript/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4569.32562")]
+[assembly: AssemblyFileVersion("1.1.4591.27667")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
