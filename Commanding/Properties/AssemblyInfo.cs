1284b08a57a988e43f77f4e6325fc12a41d4fe57
kaburke <kaburke@localhost>
2012-07-27 09:23:41 -0600

    Incremented version to 0.19.0

--- Commanding/Properties/AssemblyInfo.cs
+++ Commanding/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4569.32552")]
+[assembly: AssemblyFileVersion("1.1.4591.27662")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
