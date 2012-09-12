c4c6b2b9abe12e6e0b55b530cf66950a54c3cb11
duskdweller <duskdweller@localhost>
2012-09-12 14:13:03 +0200

    BOSS dlls updated to version 2.1.0

---
+++ NexusClient/Bootstrapper.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using Nexus.Client.BackgroundTasks;
+using Nexus.Client.Games;
+using Nexus.Client.Properties;
+using Nexus.Client.Settings;
+using Nexus.Client.Util;
+using Nexus.UI;
+using Nexus.UI.Controls;
+using SevenZip;
+using Nexus.Client.UI;
+
+namespace Nexus.Client
+{
+	/// <summary>
+	/// This class is responsible for creating all the services the application needs, and making sure that the
+	/// environment is sane.
+	/// </summary>
+	public class Bootstrapper
+	{
+		private EnvironmentInfo m_eifEnvironmentInfo = null;
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		public Bootstrapper(EnvironmentInfo p_eifEnvironmentInfo)
+		{
+			m_eifEnvironmentInfo = p_eifEnvironmentInfo;
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Runs the applications
+		/// </summary>
+		/// <remarks>
+		/// This method makes sure the environment is sane. If so, it creates the required services
+		/// and launches the main form.
+		/// </remarks>
+		/// <param name="p_strArgs">The command line arguments passed to the application.</param>
+		/// <returns><c>true</c> if the application started as expected;
+		/// <c>false</c> otherwise.</returns>
+		public bool RunMainForm(string[] p_strArgs)
+		{
+			if (!SandboxCheck(m_eifEnvironmentInfo))
+				return false;
+			SetCompressorPath(m_eifEnvironmentInfo);
+
+			string strRequestedGameMode = null;
+			string[] strArgs = p_strArgs;
+			Uri uriModToAdd = null;
+			if ((p_strArgs.Length > 0) && !p_strArgs[0].StartsWith("-"))
+			{
+				if (Uri.TryCreate(p_strArgs[0], UriKind.Absolute, out uriModToAdd) && uriModToAdd.Scheme.Equals("nxm", StringComparison.OrdinalIgnoreCase))
+					strRequestedGameMode = uriModToAdd.Host;
+			}
+			else
+				for (Int32 i = 0; i < p_strArgs.Length; i++)
+				{
+					string strArg = p_strArgs[i];
+					if (strArg.StartsWith("-"))
+					{
+						switch (strArg)
+						{
+							case "-game":
+								strRequestedGameMode = p_strArgs[i + 1];
+								Trace.Write("Game Specified On Command line: " + strRequestedGameMode + ") ");
+								break;
+						}
+					}
+				}
+
+			bool booChangeDefaultGameMode = false;
+			GameModeRegistry gmrSupportedGames = GetSupportedGameModes();
+			do
+			{
+				NexusFontSetResolver nfrResolver = SetUpFonts();
+
+				GameModeRegistry gmrInstalledGames = GetInstalledGameModes(gmrSupportedGames);
+				if (gmrInstalledGames == null)
+				{
+					Trace.TraceInformation("No installed games.");
+					MessageBox.Show(String.Format("No games were detected! {0} will now close.", m_eifEnvironmentInfo.Settings.ModManagerName), "No Games", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					return false;
+				}
+
+				GameModeSelector gmsSelector = new GameModeSelector(gmrSupportedGames, gmrInstalledGames, m_eifEnvironmentInfo);
+				IGameModeFactory gmfGameModeFactory = gmsSelector.SelectGameMode(strRequestedGameMode, booChangeDefaultGameMode);
+				if (gmsSelector.RescanRequested)
+				{
+					m_eifEnvironmentInfo.Settings.InstalledGamesDetected = false;
+					m_eifEnvironmentInfo.Settings.Save();
+					booChangeDefaultGameMode = true;
+					continue;
+				}
+				if (gmfGameModeFactory == null)
+					return false;
+
+				Trace.TraceInformation(String.Format("Game Mode Factory Selected: {0} ({1})", gmfGameModeFactory.GameModeDescriptor.Name, gmfGameModeFactory.GameModeDescriptor.ModeId));
+
+				Mutex mtxGameModeMutex = null;
+				bool booOwnsMutex = false;
+				try
+				{
+					for (Int32 intAttemptCount = 0; intAttemptCount < 3; intAttemptCount++)
+					{
+						Trace.TraceInformation("Creating Game Mode mutex (Attempt: {0})", intAttemptCount);
+						mtxGameModeMutex = new Mutex(true, String.Format("{0}-{1}-GameModeMutex", m_eifEnvironmentInfo.Settings.ModManagerName, gmfGameModeFactory.GameModeDescriptor.ModeId), out booOwnsMutex);
+
+						//If the mutex is owned, you are the first instance of the mod manager for game mode, so break out of loop.
+						if (booOwnsMutex)
+							break;
+
+						//If the mutex isn't owned, attempt to talk across the messager.
+						using (IMessager msgMessager = MessagerClient.GetMessager(m_eifEnvironmentInfo, gmfGameModeFactory.GameModeDescriptor))
+						{
+							if (msgMessager != null)
+							{
+								//Messenger was created OK, send download request, or bring to front.
+								if (uriModToAdd != null)
+								{
+									Trace.TraceInformation(String.Format("Messaging to add: {0}", uriModToAdd));
+									msgMessager.AddMod(uriModToAdd.ToString());
+								}
+								else
+								{
+									Trace.TraceInformation(String.Format("Messaging to bring to front."));
+									msgMessager.BringToFront();
+								}
+								return true;
+							}
+						}
+						mtxGameModeMutex.Close();
+						mtxGameModeMutex = null;
+
+						//Messenger couldn't be created, so sleep for a few seconds to give time for opening
+						// the running copy of the mod manager to start up/shut down
+						Thread.Sleep(TimeSpan.FromSeconds(5.0d));
+					}
+					if (!booOwnsMutex)
+					{
+						HeaderlessTextWriterTraceListener htlListener = (HeaderlessTextWriterTraceListener)Trace.Listeners["DefaultListener"];
+						htlListener.ChangeFilePath(Path.Combine(Path.GetDirectoryName(htlListener.FilePath), "Messager" + Path.GetFileName(htlListener.FilePath)));
+						Trace.TraceInformation("THIS IS A MESSAGER TRACE LOG.");
+						if (!htlListener.TraceIsForced)
+							htlListener.SaveToFile();
+
+						StringBuilder stbPromptMessage = new StringBuilder();
+						stbPromptMessage.AppendFormat("{0} was unable to start. It appears another instance of {0} is already running.", m_eifEnvironmentInfo.Settings.ModManagerName).AppendLine();
+						stbPromptMessage.AppendLine("A Trace Log was created at:");
+						stbPromptMessage.AppendLine(htlListener.FilePath);
+						stbPromptMessage.AppendLine("Before reporting the issue, don't close this window and check for a fix here (you can close it afterwards):");
+						stbPromptMessage.AppendLine("http://forums.nexusmods.com/index.php?/topic/721054-read-here-first-nexus-mod-manager-frequent-issues/");
+						stbPromptMessage.AppendLine("If you can't find a solution, please include the contents of that file if you want to make a bug report:");
+						stbPromptMessage.AppendLine("http://forums.nexusmods.com/index.php?/tracker/project-3-mod-manager-open-beta/");
+						MessageBox.Show(stbPromptMessage.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						return false;
+					}
+
+					ApplicationInitializer ainInitializer = new ApplicationInitializer(m_eifEnvironmentInfo, nfrResolver);
+					ApplicationInitializationForm frmAppInitilizer = new ApplicationInitializationForm(ainInitializer);
+					ainInitializer.Initialize(gmfGameModeFactory, SynchronizationContext.Current);
+					frmAppInitilizer.ShowDialog();
+					if (ainInitializer.Status != TaskStatus.Complete)
+					{
+						if (ainInitializer.Status == TaskStatus.Error)
+							return false;
+						booChangeDefaultGameMode = true;
+						DisposeServices(ainInitializer.Services);
+						continue;
+					}
+
+					IGameMode gmdGameMode = ainInitializer.GameMode;
+					ServiceManager svmServices = ainInitializer.Services;
+
+					MainFormVM vmlMainForm = new MainFormVM(m_eifEnvironmentInfo, gmrInstalledGames, gmdGameMode, svmServices.ModRepository, svmServices.ActivityMonitor, svmServices.UpdateManager, svmServices.ModManager, svmServices.PluginManager);
+					MainForm frmMain = new MainForm(vmlMainForm);
+
+					using (IMessager msgMessager = MessagerServer.InitializeListener(m_eifEnvironmentInfo, gmdGameMode, svmServices.ModManager, frmMain))
+					{
+						if (uriModToAdd != null)
+						{
+							Trace.TraceInformation("Adding mod: " + uriModToAdd.ToString());
+							msgMessager.AddMod(uriModToAdd.ToString());
+						}
+
+						Trace.TraceInformation("Running Application.");
+						try
+						{
+							Application.Run(frmMain);
+							svmServices.ModInstallLog.Backup();
+							strRequestedGameMode = vmlMainForm.RequestedGameMode;
+							booChangeDefaultGameMode = vmlMainForm.DefaultGameModeChangeRequested;
+						}
+						finally
+						{
+							DisposeServices(svmServices);
+							gmdGameMode.Dispose();
+						}
+					}
+				}
+				finally
+				{
+					if (mtxGameModeMutex != null)
+					{
+						if (booOwnsMutex)
+							mtxGameModeMutex.ReleaseMutex();
+						mtxGameModeMutex.Close();
+					}
+                    FileUtil.ForceDelete(m_eifEnvironmentInfo.TemporaryPath);
+
+                    //Clean up created font's.
+                    FontManager.Dispose();
+				}
+			} while (!String.IsNullOrEmpty(strRequestedGameMode) || booChangeDefaultGameMode);
+			return true;
+		}
+
+		#region Pre Game Mode Selection
+
+		/// <summary>
+		/// Checks to see if a sandbox is interfering with dynamic code generation.
+		/// </summary>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		/// <returns><c>true</c> if the check passed;
+		/// <c>false</c> otherwise.</returns>
+		protected bool SandboxCheck(EnvironmentInfo p_eifEnvironmentInfo)
+		{
+			try
+			{
+				new XmlSerializer(typeof(WindowPositions));
+			}
+			catch (InvalidOperationException)
+			{
+				string strMessage = "{0} has detected that it is running in a sandbox." + Environment.NewLine +
+									"The sandbox is preventing {0} from performing" + Environment.NewLine +
+									"important operations. Please run {0} again," + Environment.NewLine +
+									"without the sandbox.";
+				string strDetails = "This error commonly occurs on computers running Comodo Antivirus.<br/>" +
+									"If you are running Comodo, please add {0} to the exception list.<br/><br/>" +
+									"If you are not running in a sandbox, press Cancel, and make a bug report " +
+									"with the generated TraceLog.";
+				if (ExtendedMessageBox.Show(null, String.Format(strMessage, p_eifEnvironmentInfo.Settings.ModManagerName), "Sandbox Detected", String.Format(strDetails, p_eifEnvironmentInfo.Settings.ModManagerName), MessageBoxButtons.OKCancel, MessageBoxIcon.Error) == DialogResult.Cancel)
+					throw;
+				return false;
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// Sets the path to the external compression library.
+		/// </summary>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		protected void SetCompressorPath(EnvironmentInfo p_eifEnvironmentInfo)
+		{
+			string str7zPath = Path.Combine(p_eifEnvironmentInfo.ProgrammeInfoDirectory, p_eifEnvironmentInfo.Is64BitProcess ? "7z-64bit.dll" : "7z-32bit.dll");
+			SevenZipCompressor.SetLibraryPath(str7zPath);
+		}
+
+		#endregion
+
+		#region Font Management
+
+		/// <summary>
+		/// Sets up the fonts.
+		/// </summary>
+		/// <returns>The <see cref="NexusFontSetResolver"/> to be used.</returns>
+		private NexusFontSetResolver SetUpFonts()
+		{
+			FontManager.Add("LinBiolinum", Resources.LinBiolinum_RB);
+			FontManager.Add("LinBiolinum", Resources.LinBiolinum_RI);
+
+			FontSet fstDefault = new FontSet(new string[] { "Microsoft Sans Serif", "Arial" });
+			FontSetGroup fsgDefault = new FontSetGroup(fstDefault);
+			fsgDefault.AddFontSet("StandardText", fstDefault);
+			fsgDefault.AddFontSet("HeadingText", fstDefault);
+			fsgDefault.AddFontSet("SmallText", new FontSet(new string[] { "Segoe UI", "Arial" }));
+			fsgDefault.AddFontSet("MenuText", new FontSet(new string[] { "Segoe UI", "Arial" }));
+			fsgDefault.AddFontSet("GameSearchText", new FontSet(new string[] { "LinBiolinum" }));
+			fsgDefault.AddFontSet("TestText", new FontSet(new string[] { "Wingdings" }));
+
+			NexusFontSetResolver fsrResolver = new NexusFontSetResolver();
+			fsrResolver.AddFontSets(fsgDefault);
+
+			FontProvider.SetFontSetResolver(fsrResolver);
+			return fsrResolver;
+		}
+
+		#endregion
+
+		#region Support Methods
+
+		/// <summary>
+		/// This disposes of the services we created.
+		/// </summary>
+		/// <param name="p_smgServices">The services to dispose.</param>
+		protected void DisposeServices(ServiceManager p_smgServices)
+		{
+			if (p_smgServices == null)
+				return;
+			p_smgServices.ModInstallLog.Release();
+			if (p_smgServices.ActivePluginLog != null)
+				p_smgServices.ActivePluginLog.Release();
+			if (p_smgServices.PluginOrderLog != null)
+				p_smgServices.PluginOrderLog.Release();
+			if (p_smgServices.PluginManager != null)
+				p_smgServices.PluginManager.Release();
+			p_smgServices.ModManager.Release();
+		}
+
+		#endregion
+
+		#region Game Detection/Selection
+
+		/// <summary>
+		/// Gets a registry of supported game modes.
+		/// </summary>
+		/// <returns>A registry of supported game modes.</returns>
+		protected GameModeRegistry GetSupportedGameModes()
+		{
+			return GameModeRegistry.DiscoverSupportedGameModes(m_eifEnvironmentInfo);
+		}
+
+		/// <summary>
+		/// Gets a registry of installed game modes.
+		/// </summary>
+		/// <param name="p_gmrSupportedGameModes">The games modes supported by the mod manager.</param>
+		/// <returns>A registry of installed game modes.</returns>
+		protected GameModeRegistry GetInstalledGameModes(GameModeRegistry p_gmrSupportedGameModes)
+		{
+			if (!m_eifEnvironmentInfo.Settings.InstalledGamesDetected)
+			{
+				GameDiscoverer gdrGameDetector = new GameDiscoverer();
+				GameDetectionVM vmlGameDetection = new GameDetectionVM(m_eifEnvironmentInfo, gdrGameDetector, p_gmrSupportedGameModes);
+				GameDetectionForm frmGameDetector = new GameDetectionForm(vmlGameDetection);
+				gdrGameDetector.Find(p_gmrSupportedGameModes.RegisteredGameModeFactories);
+				frmGameDetector.ShowDialog();
+				if (gdrGameDetector.Status != TaskStatus.Complete)
+					return null;
+				if (gdrGameDetector.DiscoveredGameModes.Count == 0)
+					return null;
+				m_eifEnvironmentInfo.Settings.InstalledGames.Clear();
+				foreach (GameDiscoverer.GameInstallData gidGameMode in gdrGameDetector.DiscoveredGameModes)
+				{
+					IGameModeFactory gmfGameModeFactory = p_gmrSupportedGameModes.GetGameMode(gidGameMode.GameMode.ModeId);
+					m_eifEnvironmentInfo.Settings.InstallationPaths[gidGameMode.GameMode.ModeId] = gmfGameModeFactory.GetInstallationPath(gidGameMode.GameInstallPath);
+					m_eifEnvironmentInfo.Settings.InstalledGames.Add(gidGameMode.GameMode.ModeId);
+				}
+				m_eifEnvironmentInfo.Settings.InstalledGamesDetected = true;
+				m_eifEnvironmentInfo.Settings.Save();
+			}
+			GameModeRegistry gmrInstalledGameModes = GameModeRegistry.LoadInstalledGameModes(p_gmrSupportedGameModes, m_eifEnvironmentInfo);
+			return gmrInstalledGameModes;
+		}
+
+		#endregion
+	}
+}
---
+++ XmlScript/UI/OptionFormStep.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Nexus.Client.ModManagement.Scripting.XmlScript.UI
+{
+	partial class OptionFormStep
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.sptPlugins = new System.Windows.Forms.SplitContainer();
+			this.lvwPlugins = new System.Windows.Forms.ListView();
+			this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+			this.sptImage = new System.Windows.Forms.SplitContainer();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.tbxDescription = new System.Windows.Forms.TextBox();
+			this.pbxImage = new System.Windows.Forms.PictureBox();
+			this.sptPlugins.Panel1.SuspendLayout();
+			this.sptPlugins.Panel2.SuspendLayout();
+			this.sptPlugins.SuspendLayout();
+			this.sptImage.Panel1.SuspendLayout();
+			this.sptImage.Panel2.SuspendLayout();
+			this.sptImage.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pbxImage)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// sptPlugins
+			// 
+			this.sptPlugins.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.sptPlugins.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+			this.sptPlugins.Location = new System.Drawing.Point(0, 47);
+			this.sptPlugins.Name = "sptPlugins";
+			// 
+			// sptPlugins.Panel1
+			// 
+			this.sptPlugins.Panel1.Controls.Add(this.lvwPlugins);
+			// 
+			// sptPlugins.Panel2
+			// 
+			this.sptPlugins.Panel2.Controls.Add(this.sptImage);
+			this.sptPlugins.Size = new System.Drawing.Size(587, 396);
+			this.sptPlugins.SplitterDistance = 195;
+			this.sptPlugins.TabIndex = 0;
+			this.sptPlugins.TabStop = false;
+			// 
+			// lvwPlugins
+			// 
+			this.lvwPlugins.CheckBoxes = true;
+			this.lvwPlugins.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+			this.lvwPlugins.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lvwPlugins.FullRowSelect = true;
+			this.lvwPlugins.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+			this.lvwPlugins.HideSelection = false;
+			this.lvwPlugins.Location = new System.Drawing.Point(0, 0);
+			this.lvwPlugins.MultiSelect = false;
+			this.lvwPlugins.Name = "lvwPlugins";
+			this.lvwPlugins.Size = new System.Drawing.Size(195, 396);
+			this.lvwPlugins.TabIndex = 0;
+			this.lvwPlugins.UseCompatibleStateImageBehavior = false;
+			this.lvwPlugins.View = System.Windows.Forms.View.Details;
+			this.lvwPlugins.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.lvwPlugins_ItemChecked);
+			this.lvwPlugins.SelectedIndexChanged += new System.EventHandler(this.lvwPlugins_SelectedIndexChanged);
+			this.lvwPlugins.SizeChanged += new System.EventHandler(this.lvwPlugins_SizeChanged);
+			this.lvwPlugins.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.lvwPlugins_ItemCheck);
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Name";
+			// 
+			// sptImage
+			// 
+			this.sptImage.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.sptImage.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+			this.sptImage.Location = new System.Drawing.Point(0, 0);
+			this.sptImage.Name = "sptImage";
+			this.sptImage.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// sptImage.Panel1
+			// 
+			this.sptImage.Panel1.Controls.Add(this.groupBox1);
+			// 
+			// sptImage.Panel2
+			// 
+			this.sptImage.Panel2.Controls.Add(this.pbxImage);
+			this.sptImage.Size = new System.Drawing.Size(388, 396);
+			this.sptImage.SplitterDistance = 129;
+			this.sptImage.TabIndex = 101;
+			this.sptImage.TabStop = false;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.tbxDescription);
+			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.groupBox1.Location = new System.Drawing.Point(0, 0);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(388, 129);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Description";
+			// 
+			// tbxDescription
+			// 
+			this.tbxDescription.BackColor = System.Drawing.SystemColors.Window;
+			this.tbxDescription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.tbxDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tbxDescription.Location = new System.Drawing.Point(3, 16);
+			this.tbxDescription.Multiline = true;
+			this.tbxDescription.Name = "tbxDescription";
+			this.tbxDescription.ReadOnly = true;
+			this.tbxDescription.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.tbxDescription.Size = new System.Drawing.Size(382, 110);
+			this.tbxDescription.TabIndex = 0;
+			// 
+			// pbxImage
+			// 
+			this.pbxImage.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pbxImage.Location = new System.Drawing.Point(0, 0);
+			this.pbxImage.Name = "pbxImage";
+			this.pbxImage.Size = new System.Drawing.Size(388, 263);
+			this.pbxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pbxImage.TabIndex = 0;
+			this.pbxImage.TabStop = false;
+			// 
+			// OptionFormStep
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.sptPlugins);
+			this.Name = "OptionFormStep";
+			this.Size = new System.Drawing.Size(612, 588);
+			this.sptPlugins.Panel1.ResumeLayout(false);
+			this.sptPlugins.Panel2.ResumeLayout(false);
+			this.sptPlugins.ResumeLayout(false);
+			this.sptImage.Panel1.ResumeLayout(false);
+			this.sptImage.Panel2.ResumeLayout(false);
+			this.sptImage.ResumeLayout(false);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pbxImage)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer sptPlugins;
+		private System.Windows.Forms.ListView lvwPlugins;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.SplitContainer sptImage;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox tbxDescription;
+		private System.Windows.Forms.PictureBox pbxImage;
+	}
+}
