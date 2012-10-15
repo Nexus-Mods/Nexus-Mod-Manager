425c27b64dfdf9a59879f229b436cd108540b5d0
duskdweller <duskdweller@localhost>
2012-10-15 14:43:15 +0200

    Closing branch in preparation for merge.

--- AntlrUtil/Properties/AssemblyInfo.cs
+++ AntlrUtil/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4591.27662")]
+[assembly: AssemblyFileVersion("1.1.4671.22743")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
