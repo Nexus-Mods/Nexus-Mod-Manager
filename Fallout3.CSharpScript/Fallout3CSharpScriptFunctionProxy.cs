846103fa63ef32dee883d3cee75c245372411aff
duskdweller <duskdweller@localhost>
2012-10-15 16:18:05 +0200

    Fixed a bug while retrieving DS critical plugins.

---
+++ Fallout3.CSharpScript/Fallout3CSharpScriptFunctionProxy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Nexus.Client.Games.Gamebryo.ModManagement.Scripting;
+using Nexus.Client.ModManagement;
+using Nexus.Client.ModManagement.Scripting;
+using Nexus.Client.Mods;
+
+namespace Nexus.Client.Games.Fallout3.Scripting.CSharpScript
+{
+	/// <summary>
+	/// Implements the functions availabe to Fallout 3 C# scripts.
+	/// </summary>
+	public class Fallout3CSharpScriptFunctionProxy : FalloutCSharpScriptFunctionProxy
+	{
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initialies the object with the given values.
+		/// </summary>
+		/// <param name="p_modMod">The mod for which the script is running.</param>
+		/// <param name="p_gmdGameMode">The game mode currently being managed.</param>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		/// <param name="p_igpInstallers">The utility class to use to install the mod items.</param>
+		/// <param name="p_bamBsaManager">The manager to use to work with BSA files.</param>
+		/// <param name="p_uipUIProxy">The UI manager to use to interact with UI elements.</param>
+		public Fallout3CSharpScriptFunctionProxy(IMod p_modMod, IGameMode p_gmdGameMode, IEnvironmentInfo p_eifEnvironmentInfo, InstallerGroup p_igpInstallers, BsaManager p_bamBsaManager, UIUtil p_uipUIProxy)
+			: base(p_modMod, p_gmdGameMode, p_eifEnvironmentInfo, p_igpInstallers, p_bamBsaManager, p_uipUIProxy)
+		{
+		}
+
+		#endregion
+
+		#region Version Checking
+
+		/// <summary>
+		/// Gets the version of FOSE that is installed.
+		/// </summary>
+		/// <returns>The version of FOSE, or <c>null</c> if FOSE
+		/// is not installed.</returns>
+		public Version GetFoseVersion()
+		{
+			return ((Fallout3GameMode)GameMode).ScriptExtenderVersion;
+		}
+
+		#endregion
+	}
+}
---
+++ NexusClient/GameDetectionVM.cs
@@ -0,0 +1,58 @@
+﻿using Nexus.Client.Games;
+
+namespace Nexus.Client
+{
+	/// <summary>
+	/// This class encapsulates the data and the operations presented by UI
+	/// elements that display the progress of game detection.
+	/// </summary>
+	public class GameDetectionVM
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets the application's envrionment info.
+		/// </summary>
+		/// <value>The application's envrionment info.</value>
+		public IEnvironmentInfo EnvironmentInfo { get; private set; }
+
+		/// <summary>
+		/// Gets the discoverer to use to find the game installation path.
+		/// </summary>
+		/// <value>The discoverer to use to find the game installation path.</value>
+		public GameDiscoverer GameDetector { get; private set; }
+
+		/// <summary>
+		/// Gets the list of supported game modes.
+		/// </summary>
+		/// <value>The list of supported game modes.</value>
+		public GameModeRegistry SupportedGameModes { get; private set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given dependencies.
+		/// </summary>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		/// <param name="p_gdvGameDetector">The discoverer to use to find the game installation path.</param>
+		/// <param name="p_gmrSupportedGameModes">The list of supported game modes.</param>
+		public GameDetectionVM(IEnvironmentInfo p_eifEnvironmentInfo, GameDiscoverer p_gdvGameDetector, GameModeRegistry p_gmrSupportedGameModes)
+		{
+			EnvironmentInfo = p_eifEnvironmentInfo;
+			GameDetector = p_gdvGameDetector;
+			SupportedGameModes = p_gmrSupportedGameModes;
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Cancels the search for installed games.
+		/// </summary>
+		public void Cancel()
+		{
+			GameDetector.Cancel();
+		}
+	}
+}
