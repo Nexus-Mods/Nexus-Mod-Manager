525c4c37ac3d0be5b2f69ac741466d6769ec48c5
duskdweller <duskdweller@localhost>
2012-10-19 14:40:31 +0200

    Cancelling a download after pausing it wasn't removing the temp files.

---
+++ NexusClient/ModAuthoring/UI/Controls/InstallScriptEditorVM.cs
@@ -0,0 +1,44 @@
+﻿using Nexus.Client.ModManagement.Scripting;
+
+namespace Nexus.Client.ModAuthoring.UI.Controls
+{
+	/// <summary>
+	/// This class encapsulates the data and the operations presented by UI
+	/// elements that display a mod install script editor.
+	/// </summary>
+	public class InstallScriptEditorVM : ModScriptEditorVM
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the install script being edited.
+		/// </summary>
+		/// <value>The install script being edited.</value>
+		protected override IScript EditedScript
+		{
+			get
+			{
+				return EditedMod.InstallScript;
+			}
+			set
+			{
+				EditedMod.InstallScript = value;
+			}
+		}
+
+		#endregion
+		
+		#region Constructors
+
+		/// <summary>
+		/// A siple constructor that initializes the object with the given dependencies.
+		/// </summary>
+		/// <param name="p_stgScriptTypeRegistry">The <see cref="IScriptTypeRegistry"/> of available <see cref="IScriptType"/>s.</param>
+		public InstallScriptEditorVM(IScriptTypeRegistry p_stgScriptTypeRegistry)
+			: base(p_stgScriptTypeRegistry)
+		{
+		}
+
+		#endregion
+	}
+}
---
+++ Util/Threading/TrackedThread.cs
@@ -0,0 +1,112 @@
+﻿using System.Threading;
+
+namespace Nexus.Client.Util.Threading
+{
+	/// <summary>
+	/// A thread that is tracked by the <see cref="TrackedThreadManager"/>.
+	/// </summary>
+	public class TrackedThread
+	{
+		private readonly Thread m_thdThread = null;
+		private readonly ParameterizedThreadStart m_ptsThreadMethod = null;
+		private readonly ThreadStart m_tdsThreadMethod = null;
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the managed thread being tracked.
+		/// </summary>
+		/// <value>The managed thread being tracked.</value>
+		public Thread Thread
+		{
+			get
+			{
+				return this.m_thdThread;
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_ptsThreadMethod">The method to call when the thread starts.</param>
+		public TrackedThread(ParameterizedThreadStart p_ptsThreadMethod)
+		{
+			m_ptsThreadMethod = p_ptsThreadMethod;
+			m_thdThread = new Thread(RunParameterizedThread);
+			TrackedThreadManager.AddThread(this);
+		}
+
+		/// <summary>
+		/// Initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_tdsThreadMethod">The method to call when the thread starts.</param>
+		public TrackedThread(ThreadStart p_tdsThreadMethod)
+		{
+			m_tdsThreadMethod = p_tdsThreadMethod;
+			m_thdThread = new Thread(RunThread);
+			TrackedThreadManager.AddThread(this);
+		}
+
+		#endregion
+		
+		/// <summary>
+		/// Executes the paramterized method from within the thread.
+		/// </summary>
+		/// <remarks>
+		/// Once the method returns, the thread is removed from the <see cref="TrackedThreadManager"/>
+		/// and exits.
+		/// </remarks>
+		/// <param name="p_objParam">The parameter to pass to the thread.</param>
+		private void RunParameterizedThread(object p_objParam)
+		{
+			try
+			{
+				this.m_ptsThreadMethod(p_objParam);
+			}
+			finally
+			{
+				TrackedThreadManager.RemoveThread(this);
+			}
+		}
+
+		/// <summary>
+		/// Executes the non-parameterized method from within the thread.
+		/// </summary>
+		/// <remarks>
+		/// Once the method returns, the thread is removed from the <see cref="TrackedThreadManager"/>
+		/// and exits.
+		/// </remarks>
+		private void RunThread()
+		{
+			try
+			{
+				this.m_tdsThreadMethod();
+			}
+			finally
+			{
+				TrackedThreadManager.RemoveThread(this);
+			}
+		}
+
+		/// <summary>
+		/// Starts the thread.
+		/// </summary>
+		public void Start()
+		{
+			m_thdThread.Start();
+		}
+
+		/// <summary>
+		/// Starts the thread.
+		/// </summary>
+		/// <param name="p_objParam">The parameter to pass to the thread.</param>
+		public void Start(object p_objParam)
+		{
+			m_thdThread.Start(p_objParam);
+		}
+	}
+}
