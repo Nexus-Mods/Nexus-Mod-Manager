7d962a4ea90e9f3b106b5c6c3ce23e9d44778a02
duskdweller <duskdweller@localhost>
2012-10-19 14:41:45 +0200

    Closing branch in preparation for merge.

--- CSharpScript/Properties/AssemblyInfo.cs
+++ CSharpScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4671.40202")]
+[assembly: AssemblyFileVersion("1.1.4675.22566")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
