e2555c289f045d029983f618c481e2260f623a74
duskdweller <duskdweller@localhost>
2012-10-23 17:37:05 +0200

    Closing branch in preparation for merge.

--- DarkSouls/Properties/AssemblyInfo.cs
+++ DarkSouls/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4675.42344")]
+[assembly: AssemblyFileVersion("1.1.4679.27082")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
