8c893e49dfcdd0ca229b09bbb68668765b3129e7
duskdweller <duskdweller@localhost>
2012-10-23 17:36:43 +0200

    Added overwrite dialog options: Yes To Mod, No To Mod.

---
+++ Morrowind/MorrowindGameModeFactory.cs
@@ -0,0 +1,66 @@
+﻿using Nexus.Client.Games.Gamebryo;
+using Nexus.Client.UI;
+using Nexus.Client.Util;
+
+namespace Nexus.Client.Games.Morrowind
+{
+    /// <summary>
+    /// The game mode factory that builds <see cref="MorrowindGameMode"/>s.
+    /// </summary>
+    public class MorrowindGameModeFactory : GamebryoGameModeFactory
+    {
+        private readonly IGameModeDescriptor m_gmdGameModeDescriptor = null;
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the descriptor of the game mode that this factory builds.
+        /// </summary>
+        /// <value>The descriptor of the game mode that this factory builds.</value>
+        public override IGameModeDescriptor GameModeDescriptor
+        {
+            get
+            {
+                return m_gmdGameModeDescriptor;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// A simple consturctor that initializes the object with the given values.
+        /// </summary>
+        /// <param name="p_eifEnvironmentInfo">The application's environement info.</param>
+        public MorrowindGameModeFactory(IEnvironmentInfo p_eifEnvironmentInfo)
+            : base(p_eifEnvironmentInfo)
+        {
+            m_gmdGameModeDescriptor = new MorrowindGameModeDescriptor(p_eifEnvironmentInfo);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Instantiates the game mode.
+        /// </summary>
+        /// <param name="p_futFileUtility">The file utility class to be used by the game mode.</param>
+        /// <returns>The game mode for which this is a factory.</returns>
+        protected override GamebryoGameModeBase InstantiateGameMode(FileUtil p_futFileUtility)
+        {
+            return new MorrowindGameMode(EnvironmentInfo, p_futFileUtility);
+        }
+
+        /// <summary>
+        /// Performs the initializtion for the game mode being created.
+        /// </summary>
+        /// <param name="p_dlgShowView">The delegate to use to display a view.</param>
+        /// <param name="p_dlgShowMessage">The delegate to use to display a message.</param>
+        /// <returns><c>true</c> if the setup completed successfully;
+        /// <c>false</c> otherwise.</returns>
+        public override bool PerformInitialization(ShowViewDelegate p_dlgShowView, ShowMessageDelegate p_dlgShowMessage)
+        {
+            return true;
+        }
+    }
+}
