c153256ce5698b4eef1f2a015d58710f4da07192
duskdweller <duskdweller@localhost>
2012-10-26 13:49:56 +0200

    Incremented version to 0.32.0

--- Fallout3.XmlScript/Properties/AssemblyInfo.cs
+++ Fallout3.XmlScript/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4682.20886")]
+[assembly: AssemblyFileVersion("1.1.4682.21267")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
--- Setup/setup.iss
+++ Setup/setup.iss
@@ -15,7 +15,7 @@
 
 #define MyAppSetupName 'Nexus Mod Manager'
 #define MyExeName 'NexusClient.exe'
-#define MyAppVersion '0.31.2'
+#define MyAppVersion '0.32.0'
 #define SetupScriptVersion '0.7.1.0'
 #define MyPublisher 'Black Tree Gaming'
 [Setup]
