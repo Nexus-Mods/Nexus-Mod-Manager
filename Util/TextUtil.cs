2ce59894249b3c85977601e93f9997366667534c
duskdweller <duskdweller@localhost>
2012-11-26 14:34:24 +0100

    Fixed a bug with the Premium Enabled setting.

---
+++ Util/TextUtil.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Nexus.Client.Util
+{
+	/// <summary>
+	/// Utility functions to work with text.
+	/// </summary>
+	public static class TextUtil
+	{
+		/// <summary>
+		/// Converts the given byte array to a string.
+		/// </summary>
+		/// <remarks>
+		/// This method attempts to detect the text encoding.
+		/// </remarks>
+		/// <param name="p_bteText">The bytes to convert to a string.</param>
+		/// <returns>A string respresented by the given bytes.</returns>
+		public static string ByteToString(byte[] p_bteText)
+		{
+			string strText;
+			using (MemoryStream msmFile = new MemoryStream(p_bteText))
+			{
+				using (StreamReader strReader = new StreamReader(msmFile, true))
+				{
+					strText = strReader.ReadToEnd();
+					strReader.Close();
+				}
+				msmFile.Close();
+			}
+			return strText;
+		}
+
+		/// <summary>
+		/// Converts the given byte array to an array of string lines.
+		/// </summary>
+		/// <remarks>
+		/// This method attempts to detect the text encoding.
+		/// </remarks>
+		/// <param name="p_bteText">The bytes to convert to an array of string lines.</param>
+		/// <returns>An array of string lines respresented by the given bytes.</returns>
+		public static string[] ByteToStringLines(byte[] p_bteText)
+		{
+			List<string> lstLines = new List<string>();
+			using (MemoryStream msmFile = new MemoryStream(p_bteText))
+			{
+				using (StreamReader strReader = new StreamReader(msmFile, true))
+				{
+					string strLine = null;
+					while ((strLine = strReader.ReadLine()) != null)
+						lstLines.Add(strLine);
+					strReader.Close();
+				}
+				msmFile.Close();
+			}
+			return lstLines.ToArray();
+		}
+	}
+}
