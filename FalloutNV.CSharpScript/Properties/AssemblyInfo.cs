484343c92df86f2049a368af2cbb31aba4ea6ad0
duskdweller <duskdweller@localhost>
2012-11-28 12:53:46 +0100

    Incremented version to 0.34.0

--- FalloutNV.CSharpScript/Properties/AssemblyInfo.cs
+++ FalloutNV.CSharpScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4715.21260")]
+[assembly: AssemblyFileVersion("1.1.4715.21384")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
