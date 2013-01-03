46a47874820b1035fc521a1c3338bac24ec51d48
duskdweller <duskdweller@localhost>
2013-01-03 17:01:47 +0100

    Disables Yes/No To Mod when there's no mod to overwrite.

---
+++ FalloutNV.CSharpScript/FalloutNVCSharpBaseScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Nexus.Client.Games.Fallout3.Scripting.CSharpScript;
+
+namespace Nexus.Client.Games.FalloutNV.Scripting.CSharpScript
+{
+	/// <summary>
+	/// The base class for the Fallout: New Vegas variant of C# scripts.
+	/// </summary>
+	public class FalloutNVCSharpBaseScript : FalloutCSharpBaseScript
+	{
+		#region Version Checking
+
+		/// <summary>
+		/// Gets the version of NVSE that is installed.
+		/// </summary>
+		/// <returns>The version of NVSE, or <c>null</c> if NVSE
+		/// is not installed.</returns>
+		public static Version GetNvseVersion()
+		{
+			return ExecuteMethod(() => ((FalloutNVCSharpScriptFunctionProxy)Functions).GetNvseVersion());
+		}
+
+		#endregion
+	}
+}
