072f51a3ba3cc1121c459141ad8dbcb56a26deed
duskdweller <duskdweller@localhost>
2013-01-03 17:20:54 +0100

    Fixes NMM not saving latest version info when it was already present in the info.xml file.

---
+++ NexusClient/ModManagement/UI/OverwriteForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+using Nexus.Client.UI;
+
+namespace Nexus.Client.ModManagement.UI
+{
+	/// <summary>
+	/// A form that prompt the users to overwrite an item.
+	/// </summary>
+	public partial class OverwriteForm : ManagedFontForm
+	{
+		#region Properties
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		/// <param name="p_strMessage">The message to display.</param>
+		/// <param name="p_booAllowGroup">Whether to display the
+		/// "Yes to all in Group" and "No to all in Group" buttons.</param>
+		private OverwriteForm(string p_strMessage, bool p_booAllowGroup)
+		{
+			InitializeComponent();
+			lblMessage.Text = p_strMessage;
+			if (!p_booAllowGroup)
+			{
+				butYesToGroup.Enabled = false;
+				butNoToGroup.Enabled = false;
+				butYesToMod.Enabled = false;
+				butNoToMod.Enabled = false;
+			}
+
+			butNo.Tag = OverwriteResult.No;
+			butNoToAll.Tag = OverwriteResult.NoToAll;
+			butNoToGroup.Tag = OverwriteResult.NoToGroup;
+			butNoToMod.Tag = OverwriteResult.NoToMod;
+			butYes.Tag = OverwriteResult.Yes;
+			butYesToAll.Tag = OverwriteResult.YesToAll;
+			butYesToGroup.Tag = OverwriteResult.YesToGroup;
+			butYesToMod.Tag = OverwriteResult.YesToMod;
+
+		}
+
+		#endregion
+
+		private OverwriteResult m_owrResult;
+
+		/// <summary>
+		/// Displays the overwrite form.
+		/// </summary>
+		/// <param name="p_winOwner">The window to use as the owner of the form.</param>
+		/// <param name="p_strMessage">The message to display.</param>
+		/// <param name="p_booAllowGroup">Whether to display the
+		/// "Yes to all in Group" and "No to all in Group" buttons.</param>
+		/// <returns>The selected result.</returns>
+		public static OverwriteResult ShowDialog(IWin32Window p_winOwner, string p_strMessage, bool p_booAllowGroup)
+		{
+			OverwriteForm of = new OverwriteForm(p_strMessage, p_booAllowGroup);
+			of.ShowDialog(p_winOwner);
+			return of.m_owrResult;
+		}
+
+		/// <summary>
+		/// Displays the overwrite form.
+		/// </summary>
+		/// <param name="p_strMessage">The message to display.</param>
+		/// <param name="p_booAllowGroup">Whether to display the
+		/// "Yes to all in Group" and "No to all in Group" buttons.</param>
+		/// <returns>The selected result.</returns>
+		public static OverwriteResult ShowDialog(string p_strMessage, bool p_booAllowGroup)
+		{
+			OverwriteForm of = new OverwriteForm(p_strMessage, p_booAllowGroup);
+			of.ShowDialog();
+			return of.m_owrResult;
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.Click"/> events of the buttons.
+		/// </summary>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void Button_Click(object sender, EventArgs e)
+		{
+			m_owrResult = (OverwriteResult)((Button)sender).Tag;
+			Close();
+		}
+	}
+}
