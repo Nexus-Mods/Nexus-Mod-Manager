684d9fdf51d5b6dfb0b4e56cfc7e087073740b2f
duskdweller <duskdweller@localhost>
2013-01-03 17:48:42 +0100

    NMM now always uses the info.xml file in the cache folder instead of the one in the mod archive.

---
+++ NexusClient.Interface/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(Nexus.Client.ProgrammeMetadata.ModManagerName + " Interface")]
+[assembly: AssemblyDescription("The interface classes for the official Mod Manager the Nexus network of game mod websites.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Black Tree Gaming")]
+[assembly: AssemblyProduct("NexusClient.Interface")]
+[assembly: AssemblyCopyright("Copyright © Black Tree Gaming 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f317fdfb-ddf1-4ba2-8368-74ff345d2688")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1")]
+// AssemblyFileVersionFormat("1.1.*")
+[assembly: AssemblyFileVersion("1.1.4715.21382")]
+[assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
