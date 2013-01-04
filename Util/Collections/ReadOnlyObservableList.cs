109d5f76fe5224181aec924b78d2cb938e271078
duskdweller <duskdweller@localhost>
2013-01-04 16:59:31 +0100

    Adds the LoadOrder import/export functionality.

---
+++ Util/Collections/ReadOnlyObservableList.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace Nexus.Client.Util.Collections
+{
+	/// <summary>
+	/// A wrapper for a list that implements <see cref="INotifyCollectionChanged"/> and
+	/// <see cref="INotifyPropertyChanged"/>, exposing the wrapped list as read-only.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class ReadOnlyObservableList<T> : IList<T>, INotifyCollectionChanged, INotifyPropertyChanged
+	{
+		private IList<T> m_lstItems = null;
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes the read-only list with the list that is to be wrapped.
+		/// </summary>
+		/// <param name="p_lstList">The list to wrap in a read-only list. The given list must implement
+		/// <see cref="INotifyCollectionChanged"/> and <see cref="INotifyPropertyChanged"/>.</param>
+		/// <exception cref="ArgumentException">Thrown if the given list does not implement
+		/// <see cref="INotifyCollectionChanged"/> and <see cref="INotifyPropertyChanged"/>.</exception>
+		public ReadOnlyObservableList(IList<T> p_lstList)
+		{
+			if (!(p_lstList is INotifyCollectionChanged))
+				throw new ArgumentException("The given list must implement INotifyCollectionChanged.", "p_lstList");
+			if (!(p_lstList is INotifyPropertyChanged))
+				throw new ArgumentException("The given list must implement INotifyPropertyChanged.", "p_lstList");
+			m_lstItems = p_lstList;
+			((INotifyCollectionChanged)p_lstList).CollectionChanged += new NotifyCollectionChangedEventHandler(ReadOnlyObservableList_CollectionChanged);
+			((INotifyPropertyChanged)p_lstList).PropertyChanged += new PropertyChangedEventHandler(ReadOnlyObservableList_PropertyChanged);
+		}
+
+		#endregion
+
+		#region IList<T> Members
+
+		/// <summary>
+		/// Determines the first index of the specified item.
+		/// </summary>
+		/// <param name="p_tItem">The item whose index in the set is to be found.</param>
+		/// <returns>The first index of the specified item, or -1 if the item is not in the set.</returns>
+		public Int32 IndexOf(T item)
+		{
+			return m_lstItems.IndexOf(item);
+		}
+
+		/// <summary>
+		/// Inserts the given item at the specifed index. Cannot be used with a set.
+		/// </summary>
+		/// <remarks>
+		/// This operation cannot be performed on a read-only list.
+		/// </remarks>
+		/// <param name="index">The index at which to insert the item.</param>
+		/// <param name="item">The item to insert.</param>
+		/// <exception cref="NotSupportedException">Thrown always.</exception>
+		void IList<T>.Insert(int index, T item)
+		{
+			throw new NotSupportedException("The method or operation cannot be performed on a Read Only List.");
+		}
+
+		/// <summary>
+		/// Removes the item form the set at the given index.
+		/// </summary>
+		/// <remarks>
+		/// This operation cannot be performed on a read-only list.
+		/// </remarks>
+		/// <param name="index">The index of the item to remove from the set.</param>
+		/// <exception cref="NotSupportedException">Thrown always.</exception>
+		void IList<T>.RemoveAt(int index)
+		{
+			throw new NotSupportedException("The method or operation cannot be performed on a Read Only List.");
+		}
+
+		/// <summary>
+		/// Gets the item ate the specified index.
+		/// </summary>
+		/// <param name="index">The index of the item to get.</param>
+		/// <returns>The requrested item.</returns>
+		public T this[int index]
+		{
+			get
+			{
+				return m_lstItems[index];
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the item ate the specified index. Indexed setting cannot be used with a read-only list.
+		/// </summary>
+		/// <remarks>
+		/// The setting of an indexed item doesn't make sense in a read-only list.
+		/// </remarks>
+		/// <param name="index">The index of the item to get or set.</param>
+		/// <returns>The requrested item.</returns>
+		/// <exception cref="NotSupportedException">Thrown if the setting of an index item is attempted.</exception>
+		T IList<T>.this[int index]
+		{
+			get
+			{
+				return m_lstItems[index];
+			}
+			set
+			{
+				throw new NotSupportedException("The method or operation cannot be performed on a Read Only List.");
+			}
+		}
+
+		#endregion
+
+		#region ICollection<T> Members
+
+		/// <summary>
+		/// Adds the given item to the set.
+		/// </summary>
+		/// <remarks>
+		/// This operation cannot be performed on a read-only list.
+		/// </remarks>
+		/// <param name="p_tItem">The item to add.</param>
+		/// <exception cref="NotSupportedException">Thrown always.</exception>
+		void ICollection<T>.Add(T item)
+		{
+			throw new NotSupportedException("The method or operation cannot be performed on a Read Only List.");
+		}
+
+		/// <summary>
+		/// Empties the set.
+		/// </summary>
+		/// <remarks>
+		/// This operation cannot be performed on a read-only list.
+		/// </remarks>
+		/// <exception cref="NotSupportedException">Thrown always.</exception>
+		void ICollection<T>.Clear()
+		{
+			throw new NotSupportedException("The method or operation cannot be performed on a Read Only List.");
+		}
+
+		/// <summary>
+		/// Determines if the given item is in the set.
+		/// </summary>
+		/// <param name="p_tItem">The item to look for in the set.</param>
+		/// <returns><c>true</c> if the item is in the set;
+		/// <c>false</c> otherwise.</returns>
+		public bool Contains(T item)
+		{
+			return m_lstItems.Contains(item);
+		}
+
+		/// <summary>
+		/// Copies the contents of the set to the given array, starting at the given index.
+		/// </summary>
+		/// <param name="array">The array into which to copy the items.</param>
+		/// <param name="arrayIndex">The index in the array at which to start copying the items.</param>
+		public void CopyTo(T[] array, int arrayIndex)
+		{
+			m_lstItems.CopyTo(array, arrayIndex);
+		}
+
+		/// <summary>
+		/// Gets the number of items in the set.
+		/// </summary>
+		/// <value>The number of items in the set.</value>
+		public int Count
+		{
+			get
+			{
+				return m_lstItems.Count;
+			}
+		}
+
+		/// <summary>
+		/// Gets whether the set is read-only. Always <c>true</c>.
+		/// </summary>
+		/// <value>Whether the set is read-only. Always <c>true</c>.</value>
+		public bool IsReadOnly
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		/// <summary>
+		/// Removes the given item from the set.
+		/// </summary>
+		/// <remarks>
+		/// This operation cannot be performed on a read-only list.
+		/// </remarks>
+		/// <param name="p_tItem">The item to remove</param>
+		/// <returns><c>true</c> if the item was removed;
+		/// <c>false</c> otherwise.</returns>
+		/// <exception cref="NotSupportedException">Thrown always.</exception>
+		bool ICollection<T>.Remove(T item)
+		{
+			throw new NotSupportedException("The method or operation cannot be performed on a Read Only List.");
+		}
+
+		#endregion
+
+		#region IEnumerable<T> Members
+
+		/// <summary>
+		/// Gets an enumerator for this items in the set.
+		/// </summary>
+		/// <returns>An enumerator for this items in the set.</returns>
+		public IEnumerator<T> GetEnumerator()
+		{
+			return m_lstItems.GetEnumerator();
+		}
+
+		#endregion
+
+		#region IEnumerable Members
+
+		/// <summary>
+		/// Gets an enumerator for this items in the set.
+		/// </summary>
+		/// <returns>An enumerator for this items in the set.</returns>
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return ((IEnumerable)m_lstItems).GetEnumerator();
+		}
+
+		#endregion
+
+		#region INotifyCollectionChanged Members
+
+		/// <summary>
+		/// Raised when the collection changes.
+		/// </summary>
+		public event NotifyCollectionChangedEventHandler CollectionChanged = delegate { };
+
+		#endregion
+
+		#region INotifyPropertyChanged Members
+
+		/// <summary>
+		/// Raised when a property of the collection changes.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		#endregion
+
+		/// <summary>
+		/// Raises the <see cref="CollectionChanged"/> event.
+		/// </summary>
+		/// <param name="e">A <see cref="NotifyCollectionChangedEventArgs"/> describing the event arguments.</param>
+		protected void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+		{
+			CollectionChanged(this, e);
+		}
+
+		/// <summary>
+		/// Raises the <see cref="PropertyChanged"/> event.
+		/// </summary>
+		/// <param name="e">A <see cref="PropertyChangedEventArgs"/> describing the event arguments.</param>
+		protected void OnPropertyChanged(PropertyChangedEventArgs e)
+		{
+			PropertyChanged(this, e);
+		}
+		
+		/// <summary>
+		/// Handles the <see cref="INotifyPropertyChanged.PropertyChanged"/> event of the wrapped
+		/// list.
+		/// </summary>
+		/// <remarks>
+		/// This raises the <see cref="PropertyChanged"/>, thus bubbling the event up from the wrapped list.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">A <see cref="PropertyChangedEventArgs"/> describing the event arguments.</param>
+		private void ReadOnlyObservableList_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			OnPropertyChanged(e);
+		}
+
+		/// <summary>
+		/// Handles the <see cref="INotifyCollectionChanged.CollectionChanged"/> event of the wrapped
+		/// list.
+		/// </summary>
+		/// <remarks>
+		/// This raises the <see cref="CollectionChanged"/>, thus bubbling the event up from the wrapped list.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">A <see cref="NotifyCollectionChangedEventArgs"/> describing the event arguments.</param>
+		private void ReadOnlyObservableList_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		{
+			OnCollectionChanged(e);
+		}
+	}
+}
---
+++ XmlScript/UI/Controls/XmlScriptTreeNode.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Nexus.UI.Controls;
+
+namespace Nexus.Client.ModManagement.Scripting.XmlScript.UI.Controls
+{
+	public class XmlScriptTreeNode : TreeNode
+	{
+		public XmlScriptTreeNode(string p_strName)
+			: base(p_strName)
+		{
+		}
+
+		public virtual NodeEditor CreateEditor(IList<VirtualFileSystemItem> p_lstModFiles)
+		{
+			return null;
+		}
+
+		public XmlScriptTreeNode<T> AddNode<T>(T p_tObject, XmlScriptTreeNode<T>.EditorFactory<T> p_edfEditorFactory, XmlScriptTreeNode<T>.NodeFormatter<T> p_ftrFormatter, XmlScriptTreeNode<T>.PropertyChangedHandler<T> p_pchPropertyChangedHandler) where T : INotifyPropertyChanged
+		{
+			XmlScriptTreeNode<T> tndNode = new XmlScriptTreeNode<T>(p_tObject, p_edfEditorFactory, p_ftrFormatter, p_pchPropertyChangedHandler);
+			Nodes.Add(tndNode);
+			return tndNode;
+		}
+		
+		public XmlScriptTreeNode<T> AddNode<T>(T p_tObject, XmlScriptTreeNode<T>.EditorFactory<T> p_edfEditorFactory, XmlScriptTreeNode<T>.NodeFormatter<T> p_ftrFormatter) where T : INotifyPropertyChanged
+		{
+			XmlScriptTreeNode<T> tndNode = new XmlScriptTreeNode<T>(p_tObject, p_edfEditorFactory, p_ftrFormatter);
+			Nodes.Add(tndNode);
+			return tndNode;
+		}
+
+		public XmlScriptTreeNode<T> AddNode<T>(T p_tObject, XmlScriptTreeNode<T>.NodeFormatter<T> p_ftrFormatter) where T : INotifyPropertyChanged
+		{
+			XmlScriptTreeNode<T> tndNode = new XmlScriptTreeNode<T>(p_tObject, p_ftrFormatter);
+			Nodes.Add(tndNode);
+			return tndNode;
+		}
+
+		public XmlScriptTreeNode AddNode(string p_strName)
+		{
+			XmlScriptTreeNode tndNode = new XmlScriptTreeNode(p_strName);
+			Nodes.Add(tndNode);
+			return tndNode;
+		}
+
+		/// <summary>
+		/// Gets the object the node is representing.
+		/// </summary>
+		/// <returns>The object the node is representing, or <c>null</c> if the
+		/// node is not representing a script object.</returns>
+		public virtual object GetObject()
+		{
+			return null;
+		}
+	}
+
+	public class XmlScriptTreeNode<T> : XmlScriptTreeNode where T : INotifyPropertyChanged
+	{
+		public delegate NodeEditor EditorFactory<K>(K p_kObject, IList<VirtualFileSystemItem> p_lstModFiles);
+		public delegate void NodeFormatter<K>(XmlScriptTreeNode p_stnNode, K p_kObject);
+		public delegate void PropertyChangedHandler<K>(XmlScriptTreeNode p_stnNode, K p_kObject, string p_strPropertyName);
+
+		private NodeFormatter<T> m_ftrFormatter = null;
+		private EditorFactory<T> m_edfEditorFactory = delegate { return null; };
+		private PropertyChangedHandler<T> m_pchPropertyChangedHandler = delegate { };
+		private T m_tObject = default(T);
+
+		public T Object
+		{
+			get
+			{
+				return m_tObject;
+			}
+		}
+
+		public XmlScriptTreeNode(T p_tObject, EditorFactory<T> p_edfEditorFactory, NodeFormatter<T> p_ftrFormatter, PropertyChangedHandler<T> p_pchPropertyChangedHandler)
+			: this(p_tObject, p_edfEditorFactory, p_ftrFormatter)
+		{
+			m_pchPropertyChangedHandler += p_pchPropertyChangedHandler;
+		}
+
+		public XmlScriptTreeNode(T p_tObject, EditorFactory<T> p_edfEditorFactory, NodeFormatter<T> p_ftrFormatter)
+			: this(p_tObject, p_ftrFormatter)
+		{
+			if (p_edfEditorFactory != null)
+				m_edfEditorFactory = p_edfEditorFactory;
+		}
+
+		public XmlScriptTreeNode(T p_tObject, NodeFormatter<T> p_ftrFormatter)
+			: base(null)
+		{
+			m_tObject = p_tObject;
+			p_tObject.PropertyChanged += new PropertyChangedEventHandler(p_tObject_PropertyChanged);
+			m_ftrFormatter = p_ftrFormatter ?? DefaultFormatter;
+			m_ftrFormatter(this, m_tObject);
+		}
+
+		public override NodeEditor CreateEditor(IList<VirtualFileSystemItem> p_lstModFiles)
+		{
+			return m_edfEditorFactory(m_tObject, p_lstModFiles);
+		}
+
+		/// <summary>
+		/// Gets the object the node is representing.
+		/// </summary>
+		/// <returns>The object the node is representing, or <c>null</c> if the
+		/// node is not representing a script object.</returns>
+		public override object GetObject()
+		{
+			return Object;
+		}
+
+		private void DefaultFormatter<K>(XmlScriptTreeNode p_stnNode, K p_kObject)
+		{
+			p_stnNode.Text = p_kObject.ToString();
+		}
+
+		private void p_tObject_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			m_ftrFormatter(this, m_tObject);
+			m_pchPropertyChangedHandler(this, m_tObject, e.PropertyName);
+		}
+	}
+}
