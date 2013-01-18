48732d09137c30b72d715d4953966878296456da
duskdweller <duskdweller@localhost>
2013-01-18 15:05:05 +0100

    Prevents NMM from crashing when the user enables too many plugins, and provides feedback to the user.

---
+++ NexusClient/ModManagement/UI/ModManagerVM.cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Nexus.Client.BackgroundTasks;
+using Nexus.Client.Commands.Generic;
+using Nexus.Client.Games;
+using Nexus.Client.ModManagement;
+using Nexus.Client.ModRepositories;
+using Nexus.Client.Mods;
+using Nexus.Client.Settings;
+using Nexus.Client.Util;
+using Nexus.Client.Util.Collections;
+
+namespace Nexus.Client.ModManagement.UI
+{
+	/// <summary>
+	/// This class encapsulates the data and the operations presented by UI
+	/// elements that display mod management.
+	/// </summary>
+	public class ModManagerVM
+	{
+		#region Events
+
+		/// <summary>
+		/// Raised when mods are about to be added to the mod manager.
+		/// </summary>
+		public event EventHandler<EventArgs<IBackgroundTask>> AddingMod = delegate { };
+
+		/// <summary>
+		/// Raised when mods are about to be deleted from the mod manager.
+		/// </summary>
+		public event EventHandler<EventArgs<IBackgroundTaskSet>> DeletingMod = delegate { };
+
+		/// <summary>
+		/// Raised when the activation status of a mod is about to be changed.
+		/// </summary>
+		public event EventHandler<EventArgs<IBackgroundTaskSet>> ChangingModActivation = delegate { };
+
+		/// <summary>
+		/// Raised when a mod is being tagged.
+		/// </summary>
+		public event EventHandler<EventArgs<ModTaggerVM>> TaggingMod = delegate { };
+
+		#endregion
+
+		/// <summary>
+		/// Raised when the deletion of a mod file needs to be confirmed.
+		/// </summary>
+		public Func<IMod, bool> ConfirmModFileDeletion = delegate { return false; };
+
+		/// <summary>
+		/// Raised when the overwriting of a mod file needs to be confirmed.
+		/// </summary>
+		public Func<string, string, string> ConfirmModFileOverwrite = delegate { return null; };
+
+		/// <summary>
+		/// Raised when the overwriting of an item by another item being installed by a mod needs to be confirmed.
+		/// </summary>
+		public ConfirmItemOverwriteDelegate ConfirmItemOverwrite = delegate { return OverwriteResult.No; };
+
+		/// <summary>
+		/// Raised when the overwriting of an item by another item being installed by a mod needs to be confirmed.
+		/// </summary>
+		public ConfirmModUpgradeDelegate ConfirmModUpgrade = delegate { return ConfirmUpgradeResult.Cancel; };
+
+		#region Properties
+
+		#region Commands
+
+		/// <summary>
+		/// Gets the command to add a mod to the manager.
+		/// </summary>
+		/// <remarks>
+		/// The commands takes an argument describing the file path to the mod to be added.
+		/// </remarks>
+		/// <value>The command to add a mod to the manager.</value>
+		public Command<string> AddModCommand { get; private set; }
+
+		/// <summary>
+		/// Gets the command to delete a mod from the manager.
+		/// </summary>
+		/// <remarks>
+		/// The commands takes an argument describing the mod to be deleted.
+		/// </remarks>
+		/// <value>The command to delete a mod from the manager.</value>
+		public Command<IMod> DeleteModCommand { get; private set; }
+
+		/// <summary>
+		/// Gets the command to activate a mod.
+		/// </summary>
+		/// <remarks>
+		/// The commands takes an argument describing the mod to be activated.
+		/// </remarks>
+		/// <value>The command to activate a mod.</value>
+		public Command<IMod> ActivateModCommand { get; private set; }
+
+		/// <summary>
+		/// Gets the command to deactivate a mod.
+		/// </summary>
+		/// <remarks>
+		/// The commands takes an argument describing the mod to be deactivated.
+		/// </remarks>
+		/// <value>The command to deactivate a mod.</value>
+		public Command<IMod> DeactivateModCommand { get; private set; }
+
+		/// <summary>
+		/// Gets the command to tag a mod.
+		/// </summary>
+		/// <remarks>
+		/// The commands takes an argument describing the mod to be tagged.
+		/// </remarks>
+		/// <value>The command to tag a mod.</value>
+		public Command<IMod> TagModCommand { get; private set; }
+
+		#endregion
+
+		/// <summary>
+		/// Gets the mod manager to use to manage mods.
+		/// </summary>
+		/// <value>The mod manager to use to manage mods.</value>
+		protected ModManager ModManager { get; private set; }
+
+		/// <summary>
+		/// Gets the mod manager to use to manage mods.
+		/// </summary>
+		/// <value>The mod manager to use to manage mods.</value>
+		public CategoryManager CategoryManager { get; private set; }
+
+		/// <summary>
+		/// Gets the list of mods being managed by the mod manager.
+		/// </summary>
+		/// <value>The list of mods being managed by the mod manager.</value>
+		public ReadOnlyObservableList<IMod> ManagedMods
+		{
+			get
+			{
+				return ModManager.ManagedMods;
+			}
+		}
+
+		/// <summary>
+		/// Gets the newest available information about the managed mods.
+		/// </summary>
+		/// <value>The newest available information about the managed mods.</value>
+		public ReadOnlyObservableList<AutoUpdater.UpdateInfo> NewestModInfo
+		{
+			get
+			{
+				return ModManager.NewestModInfo;
+			}
+		}
+
+		/// <summary>
+		/// Gets the list of active mods.
+		/// </summary>
+		/// <value>The list of active mods.</value>
+		public ReadOnlyObservableList<IMod> ActiveMods
+		{
+			get
+			{
+				return ModManager.ActiveMods;
+			}
+		}
+
+		/// <summary>
+		/// Gets the application and user settings.
+		/// </summary>
+		/// <value>The application and user settings.</value>
+		public ISettings Settings { get; private set; }
+
+		/// <summary>
+		/// Gets the theme to use for the UI.
+		/// </summary>
+		/// <value>The theme to use for the UI.</value>
+		public Theme CurrentTheme { get; private set; }
+
+		/// <summary>
+		/// Gets whether the manager is in offline mode.
+		/// </summary>
+		/// <value>Whether the manager is in offline mode.</value>
+		public bool OfflineMode
+		{
+			get
+			{
+				return ModManager.RepositoryOfflineMode;
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with its dependencies.
+		/// </summary>
+		/// <param name="p_mmdModManager">The mod manager to use to manage mods.</param>
+		/// <param name="p_setSettings">The application and user settings.</param>
+		/// <param name="p_thmTheme">The current theme to use for the views.</param>
+		public ModManagerVM(ModManager p_mmdModManager, ISettings p_setSettings, Theme p_thmTheme)
+		{
+			ModManager = p_mmdModManager;
+			Settings = p_setSettings;
+			CurrentTheme = p_thmTheme;
+			CategoryManager = new CategoryManager(ModManager.CurrentGameModeModDirectory, "categories");
+			if (!this.CategoryManager.IsValidPath)
+			{
+				//messagebox load game default or just unassigned
+				string strMessage = "You currently don't have any file categories setup.";
+				strMessage += Environment.NewLine + "Would you like NMM to organise your mods based on the categories the Nexus sites use, or would you like to organise your categories yourself?";
+				strMessage += Environment.NewLine + Environment.NewLine + "Note: If you choose to use Nexus categories you can still create your own categories and move your files around them. This initial Nexus setup is just a template for you to use.";
+				
+				DialogResult Result = MessageBox.Show(strMessage, "Category setup", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+				if (Result == DialogResult.Yes)
+				{
+					this.CategoryManager.LoadCategories(ModManager.CurrentGameModeDefaultCategories);
+					if (!OfflineMode)
+						CheckForUpdates(true);
+				}
+				else
+					this.CategoryManager.LoadCategories(String.Empty);	
+			}
+			else
+				this.CategoryManager.LoadCategories(String.Empty);	
+
+			AddModCommand = new Command<string>("Add Mod", "Adds a mod to the manager.", AddMod);
+			DeleteModCommand = new Command<IMod>("Delete Mod", "Deletes the selected mod.", DeleteMod);
+			ActivateModCommand = new Command<IMod>("Activate Mod", "Activates the selected mod.", ActivateMod);
+			DeactivateModCommand = new Command<IMod>("Deactivate Mod", "Deactivates the selected mod.", DeactivateMod);
+			TagModCommand = new Command<IMod>("Tag Mod", "Gets missing mod info.", TagMod);
+		}
+
+		#endregion
+
+		#region Mod Addition/Deletion
+
+		/// <summary>
+		/// Installs the specified mod.
+		/// </summary>
+		/// <param name="p_strPath">The path to the mod to install.</param>
+		protected void AddMod(string p_strPath)
+		{
+			IBackgroundTask bgtAddingTask = ModManager.AddMod(p_strPath, ConfirmFileOverwrite);
+		}
+
+		/// <summary>
+		/// The callback that confirm a file overwrite.
+		/// </summary>
+		/// <param name="p_strOldFilePath">The path to the file that is to be overwritten.</param>
+		/// <param name="p_strNewFilePath">An out parameter specifying the file to to which to
+		/// write the file.</param>
+		/// <returns><c>true</c> if the file should be written;
+		/// <c>false</c> otherwise.</returns>
+		protected bool ConfirmFileOverwrite(string p_strOldFilePath, out string p_strNewFilePath)
+		{
+			string strNewFileName = p_strOldFilePath;
+			string strExtension = Path.GetExtension(p_strOldFilePath);
+			string strDirectory = Path.GetDirectoryName(p_strOldFilePath);
+			for (Int32 i = 2; i < Int32.MaxValue && File.Exists(strNewFileName); i++)
+				strNewFileName = Path.Combine(strDirectory, String.Format("{0} ({1}){2}", Path.GetFileNameWithoutExtension(p_strOldFilePath), i, strExtension));
+			if (File.Exists(strNewFileName))
+				throw new Exception("Cannot write file. Unable to find unused file name.");
+			p_strNewFilePath = ConfirmModFileOverwrite(p_strOldFilePath, strNewFileName);
+			return (p_strNewFilePath != null);
+		}
+
+		/// <summary>
+		/// Deletes the given mod.
+		/// </summary>
+		/// <param name="p_modMod">The mod to activate.</param>
+		protected void DeleteMod(IMod p_modMod)
+		{
+			if (ConfirmModFileDeletion(p_modMod))
+			{
+				IBackgroundTaskSet btsInstall = ModManager.DeleteMod(p_modMod);
+				DeletingMod(this, new EventArgs<IBackgroundTaskSet>(btsInstall));
+			}
+		}
+
+		#endregion
+
+		#region Mod Activation/Deactivation
+
+		/// <summary>
+		/// Activates the given mod.
+		/// </summary>
+		/// <param name="p_modMod">The mod to activate.</param>
+		protected void ActivateMod(IMod p_modMod)
+		{
+			p_modMod.InstallDate = DateTime.Now.ToString();
+			IBackgroundTaskSet btsInstall = ModManager.ActivateMod(p_modMod, ConfirmModUpgrade, ConfirmItemOverwrite);
+			if (btsInstall != null)
+				ChangingModActivation(this, new EventArgs<IBackgroundTaskSet>(btsInstall));
+		}
+
+		/// <summary>
+		/// Deactivates the given mod.
+		/// </summary>
+		/// <param name="p_modMod">The mod to deactivate.</param>
+		protected void DeactivateMod(IMod p_modMod)
+		{
+			p_modMod.InstallDate = null;
+			IBackgroundTaskSet btsUninstall = ModManager.DeactivateMod(p_modMod);
+			ChangingModActivation(this, new EventArgs<IBackgroundTaskSet>(btsUninstall));
+		}
+
+		#endregion
+
+		#region Mod Tagging
+
+		/// <summary>
+		/// Tags the given mod.
+		/// </summary>
+		/// <param name="p_modMod">The mod to tag.</param>
+		protected void TagMod(IMod p_modMod)
+		{
+			ModTaggerVM mtgTagger = new ModTaggerVM(ModManager.GetModTagger(), p_modMod, Settings, CurrentTheme);
+			TaggingMod(this, new EventArgs<ModTaggerVM>(mtgTagger));
+		}
+
+		/// <summary>
+		/// Updates the mod's name.
+		/// </summary>
+		/// <param name="p_modMod">The mod whose name is to be updated.</param>
+		/// <param name="p_strNewModName">The name to which to update the mod's name.</param>
+		public void UpdateModName(IMod p_modMod, string p_strNewModName)
+		{
+			ModInfo mifNewInfo = new ModInfo(p_modMod);
+			mifNewInfo.ModName = p_strNewModName;
+			p_modMod.UpdateInfo(mifNewInfo, true);
+		}
+
+		#endregion
+
+		#region Mod Updating
+
+		/// <summary>
+		/// Checks for mod updates.
+		/// </summary>
+		/// <returns>Message</returns>
+		/// <param name="p_booOverrideCategorySetup">Whether to just check for mods missing the Nexus Category.</param>
+		public string CheckForUpdates(bool p_booOverrideCategorySetup)
+		{
+			return ModManager.CheckForUpdates(true, p_booOverrideCategorySetup);
+		}
+
+		/// <summary>
+		/// Toggles the endorsement for the given mod.
+		/// </summary>
+		/// <param name="p_modMod">The mod to endorse/unendorse.</param>
+		public void ToggleModEndorsement(IMod p_modMod)
+		{
+			string strResult = string.Empty;
+
+			if (String.IsNullOrEmpty(p_modMod.Id))
+				throw new Exception("we couldn't find a proper Nexus ID or the file no longer exists on the Nexus sites.");
+
+			ModManager.ToggleModEndorsement(p_modMod);
+		}
+
+		#endregion
+
+		#region Category Updating
+
+		/// <summary>
+		/// Switches the mod category.
+		/// </summary>
+		/// <param name="p_modMod">The mod.</param>
+		/// <param name="p_intCategoryId">The new category id.</param>
+		public void SwitchModCategory(IMod p_modMod, Int32 p_intCategoryId)
+		{
+			ModManager.SwitchModCategory(p_modMod, p_intCategoryId);
+		}
+
+		/// <summary>
+		/// Resets to the repository default categories.
+		/// </summary>
+		public bool ResetDefaultCategories()
+		{
+			string strMessage = "Are you sure you want to reset to the Nexus site default categories?.";
+			strMessage += Environment.NewLine + Environment.NewLine + "Note: The category list will revert to the Nexus default and your downloaded mods will be automatically reassigned to the Nexus categories.";
+			DialogResult Result = MessageBox.Show(strMessage, "Category reset", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+			if (Result == DialogResult.Yes)
+			{
+				this.CategoryManager.ResetCategories(ModManager.CurrentGameModeDefaultCategories);
+				if (!OfflineMode)
+					CheckForUpdates(true);
+				SwitchModsToUnassigned(-1);
+				return true;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Resets to the repository default categories.
+		/// </summary>
+		public bool ResetToUnassigned()
+		{
+			string strMessage = "Are you sure you want to reset all mods to the Unassigned category?.";
+			strMessage += Environment.NewLine + Environment.NewLine + "Note: If you're using custom categories you won't be able to revert this operation.";
+			DialogResult Result = MessageBox.Show(strMessage, "Category reset", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+			if (Result == DialogResult.Yes)
+			{
+				SwitchModsToUnassigned(0);
+				return true;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Sets all managed mods to the Unassigned category.
+		/// </summary>
+		private void SwitchModsToUnassigned(Int32 p_intUnassignedId)
+		{
+			foreach (IMod modMod in ManagedMods)
+			{
+				SwitchModCategory(modMod, p_intUnassignedId);
+			}
+		}
+
+		/// <summary>
+		/// Sets all mods assigned to a removed category to Unassigned.
+		/// </summary>
+		/// <param name="p_imcCategory">The removed category.</param>
+		public void SwitchModsToUnassigned(IModCategory p_imcCategory)
+		{
+			var CategoryMods = from Mod in ManagedMods
+							   where ((Mod.CustomCategoryId >= 0 ? Mod.CustomCategoryId : Mod.CategoryId) == p_imcCategory.Id)
+							   select Mod;
+
+			foreach (IMod modMod in CategoryMods)
+			{
+				SwitchModCategory(modMod, 0);
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Gets the list of extensions commonly used for mod files.
+		/// </summary>
+		/// <returns>The list of extensions commonly used for mod files.</returns>
+		public IList<string> GetModFormatExtensions()
+		{
+			Set<string> setModExtensions = new Set<string>(StringComparer.OrdinalIgnoreCase);
+			foreach (IModFormat mftFormat in ModManager.ModFormats)
+				setModExtensions.Add(mftFormat.Extension);
+			setModExtensions.Add(".7z");
+			setModExtensions.Add(".zip");
+			setModExtensions.Add(".rar");
+			return setModExtensions;
+		}
+	}
+}
---
+++ UI/FontManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+
+namespace Nexus.UI
+{
+    /// <summary>
+	/// Used to manager fonts that are private.
+	/// </summary>
+	/// <remarks>
+	/// This manager handles fonts that aren't installed with the operating system, and exposed
+	/// through the runtime framework. For example, fonts that are distributed as part of an
+	/// application can be managed with this class.
+	/// </remarks>
+    public static class FontManager
+    {
+		//not clear if this is needed - there are reports that AddMemoryFont()
+		// may not make the font available for all types of controls.
+		//[DllImport("gdi32.dll")]
+		//private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont, IntPtr pdv, [In] ref uint pcFonts);
+
+        #region Properties
+
+        /// <summary>Gets or sets the families.</summary>
+        private static Dictionary<string, PrivateFontCollection> Families
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Creates the static instance of the <see cref="FontManager"/> class.</summary>
+        static FontManager()
+        {
+            FontManager.Families = new Dictionary<string, PrivateFontCollection>();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>Used to add the font family.</summary>
+        /// <param name="name">The name of the family.</param>
+        /// <param name="data">The data.</param>
+        public static void Add(string name, byte[] data)
+        {
+            //Convert the data into a pointer.
+            IntPtr ptData = IntPtr.Zero;
+
+            try
+            {
+                //Allocate the memory.
+                ptData = Marshal.AllocCoTaskMem(data.Length);
+
+                //Copy the byte data to the memory.
+                Marshal.Copy(data, 0, ptData, data.Length);
+
+                //Add font.
+                if (!FontManager.Families.ContainsKey(name))
+                    FontManager.Families.Add(name, new PrivateFontCollection());
+
+                FontManager.Families[name].AddMemoryFont(ptData, data.Length);
+				//not clear if this is needed - there are reports that AddMemoryFont()
+				// may not make the font available for all types of controls.
+				//uint dummy = 0;
+				//AddFontMemResourceEx(ptData, (uint)data.Length, IntPtr.Zero, ref dummy);
+            }
+            finally
+            {
+                Marshal.FreeCoTaskMem(ptData);
+            }
+        }
+
+        /// <summary>True if a font has been loaded with the name passed.</summary>
+        /// <param name="name">The name of the font.</param>
+        /// <returns>True if the font exists.</returns>
+        public static bool Contains(string name)
+        {
+            return FontManager.Families.ContainsKey(name);
+        }
+
+        /// <summary>Used to dispose of all the loaded fonts.</summary>
+        public static void Dispose()
+        {
+            foreach (KeyValuePair<string, PrivateFontCollection> keyValue in FontManager.Families)
+                keyValue.Value.Dispose();
+            FontManager.Families.Clear();
+        }
+
+        /// <summary>Gets the font which matching name in the size and style passed.</summary>
+        /// <param name="name">The name of the font.</param>
+        /// <param name="size">The size of the font required.</param>
+        /// <param name="style">The style of the font required.</param>
+        /// <returns>The font created, or null.</returns>
+        public static Font GetFont(string name, float size, FontStyle style)
+        {
+            if (FontManager.Families.ContainsKey(name))
+            {
+                foreach (FontFamily family in FontManager.Families[name].Families)
+                {
+                    if (family.IsStyleAvailable(style))
+                        return new Font(family, size, style);
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
+    }
+}
