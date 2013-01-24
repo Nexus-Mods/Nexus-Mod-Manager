7bd4194077a2f71fb0417ff9a76510d45d8b50d7
duskdweller <duskdweller@localhost>
2013-01-24 17:45:24 +0100

    Fixes and updates based on feedback.

--- Morrowind/MorrowindGameMode.cs
+++ Morrowind/MorrowindGameMode.cs
@@ -87,6 +87,18 @@ namespace Nexus.Client.Games.Morrowind
             }
         }
 
+		/// <summary>
+		/// Gets the default game categories.
+		/// </summary>
+		/// <value>The default game categories stored in the resource file.</value>
+		public override string GameDefaultCategories
+		{
+			get
+			{
+				return Properties.Resources.Categories;
+			}
+		}
+
         #endregion
 
         #region Constructors
