ca55c25282cb9ddc399e5eb2fac08e66a4ec6bd3
duskdweller <duskdweller@localhost>
2013-01-31 22:32:18 +0100

    Fixes NMM crashing at startup while trying to open a managed mod faulty archive.

---
+++ ModManager.Interface/ModManagement/OverwriteResult.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Nexus.Client.ModManagement
+{
+	/// <summary>
+	/// The possible results of the overwrite form.
+	/// </summary>
+	public enum OverwriteResult
+	{
+		/// <summary>
+		/// Do not overwrite the item.
+		/// </summary>
+		No = 0,
+
+		/// <summary>
+		/// Do not overwrite the item, or any subsequent items.
+		/// </summary>
+		NoToAll,
+
+		/// <summary>
+		/// Do not overwrite the item, or any subsequent items in the same group.
+		/// </summary>
+		NoToGroup,
+
+		/// <summary>
+		/// Overwrite the item.
+		/// </summary>
+		Yes,
+
+		/// <summary>
+		/// Overwrite the item, and all subsequent items.
+		/// </summary>
+		YesToAll,
+
+		/// <summary>
+		/// Overwrite the item, and all subsequent items in the same group.
+		/// </summary>
+		YesToGroup,
+
+		/// <summary>
+		/// Do not overwrite the item, or any subsequent items in the same mod.
+		/// </summary>
+		NoToMod,
+
+		/// <summary>
+		/// Overwrite the item, and all subsequent items in the same mod.
+		/// </summary>
+		YesToMod
+	}
+}
---
+++ Util/ObservableObject.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace Nexus.Client.Util
+{
+	public class ObservableObject : INotifyPropertyChanged
+	{
+		public static PropertyChangedEventArgs CreatePropertyChangedEventArgs<T>(Expression<Func<T>> p_expProperty)
+		{
+			return new PropertyChangedEventArgs(ObjectHelper.GetPropertyName(p_expProperty));
+		}
+
+		#region INotifyPropertyChanged Members
+
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		#endregion
+
+		/// <summary>
+		/// Determins if the given values differ.
+		/// </summary>
+		/// <typeparam name="T">The type of the values.</typeparam>
+		/// <param name="p_tOldValue">The old value.</param>
+		/// <param name="p_tNewValue">The new value.</param>
+		/// <returns><c>true</c> if the values are different;
+		/// <c>false</c> otherwise.</returns>
+		protected bool CheckIfChanged<T>(T p_tOldValue, T p_tNewValue)
+		{
+			if ((p_tOldValue == null) && (p_tNewValue == null))
+				return false;
+			if ((p_tOldValue == null) || !p_tOldValue.Equals((T)p_tNewValue))
+				return true;
+			return false;
+		}
+
+		protected bool SetPropertyIfChanged<T>(ref T p_tOldValue, T p_tNewValue, PropertyChangedEventArgs e)
+		{
+			if (CheckIfChanged(p_tOldValue, p_tNewValue))
+			{
+				p_tOldValue = p_tNewValue;
+				OnPropertyChanged(e);
+				return true;
+			}
+			return false;
+		}
+
+		protected bool SetPropertyIfChanged<T>(ref T p_tOldValue, T p_tNewValue, Expression<Func<T>> p_expProperty)
+		{
+			return SetPropertyIfChanged(ref p_tOldValue, p_tNewValue, CreatePropertyChangedEventArgs(p_expProperty));
+		}
+
+		protected void OnPropertyChanged<T>(Expression<Func<T>> p_expProperty)
+		{
+			OnPropertyChanged(CreatePropertyChangedEventArgs(p_expProperty));
+		}
+
+		protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+		{
+			PropertyChanged(this, e);
+		}
+	}
+}
