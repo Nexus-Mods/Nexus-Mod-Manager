f67921f914edeb44eec612ad4582f737d1663e58
duskdweller <duskdweller@localhost>
2013-01-31 21:31:37 +0100

    Fixes NMM crashing when trying to install a mod whose archive is set to readonly.

---
+++ NexusClient.Interface/UI/ViewMessage.cs
@@ -0,0 +1,122 @@
+﻿using Nexus.UI.Controls;
+using System.Windows.Forms;
+
+namespace Nexus.Client.UI
+{
+	/// <summary>
+	/// Describes a message to be displayed to the user.
+	/// </summary>
+	public class ViewMessage
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets the message to display.
+		/// </summary>
+		/// <value>The message to display.</value>
+		public string Message { get; private set; }
+
+		/// <summary>
+		/// Gets the details to display.
+		/// </summary>
+		/// <value>The details to display.</value>
+		public string Details { get; private set; }
+
+		/// <summary>
+		/// Gets the title of the message.
+		/// </summary>
+		/// <value>The title of the message.</value>
+		public string Title { get; private set; }
+
+		/// <summary>
+		/// Gets the set of choices to offer the user.
+		/// </summary>
+		/// <value>The set of choices to offer the user.</value>
+		public ExtendedMessageBoxButtons Options { get; private set; }
+
+		/// <summary>
+		/// Gets the type, or severity, of the message.
+		/// </summary>
+		/// <value>The type, or severity, of the message.</value>
+		public MessageBoxIcon MessageType { get; private set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A siple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_strMessage">The message to display.</param>
+		/// <param name="p_strTitle">The title of the message.</param>
+		public ViewMessage(string p_strMessage, string p_strTitle)
+			: this(p_strMessage, null, p_strTitle, ExtendedMessageBoxButtons.OK, MessageBoxIcon.Information)
+		{
+		}
+
+		/// <summary>
+		/// A siple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_strMessage">The message to display.</param>
+		/// <param name="p_strDetails">The details to display.</param>
+		/// <param name="p_strTitle">The title of the message.</param>
+		public ViewMessage(string p_strMessage, string p_strDetails, string p_strTitle)
+			: this(p_strMessage, p_strDetails, p_strTitle, ExtendedMessageBoxButtons.OK, MessageBoxIcon.Information)
+		{
+		}
+
+		/// <summary>
+		/// A siple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_strMessage">The message to display.</param>
+		/// <param name="p_strTitle">The title of the message.</param>
+		/// <param name="p_mbiMessageType">The type, or severity, of the message.</param>
+		public ViewMessage(string p_strMessage, string p_strTitle, MessageBoxIcon p_mbiMessageType)
+			: this(p_strMessage, null, p_strTitle, ExtendedMessageBoxButtons.OK, p_mbiMessageType)
+		{
+		}
+
+		/// <summary>
+		/// A siple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_strMessage">The message to display.</param>
+		/// <param name="p_strDetails">The details to display.</param>
+		/// <param name="p_strTitle">The title of the message.</param>
+		/// <param name="p_mbiMessageType">The type, or severity, of the message.</param>
+		public ViewMessage(string p_strMessage, string p_strDetails, string p_strTitle, MessageBoxIcon p_mbiMessageType)
+			: this(p_strMessage, p_strDetails, p_strTitle, ExtendedMessageBoxButtons.OK, p_mbiMessageType)
+		{
+		}
+
+		/// <summary>
+		/// A siple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_strMessage">The message to display.</param>
+		/// <param name="p_strTitle">The title of the message.</param>
+		/// <param name="p_ebbOptions">The set of choices to offer the user.</param>
+		/// <param name="p_mbiMessageType">The type, or severity, of the message.</param>
+		public ViewMessage(string p_strMessage, string p_strTitle, ExtendedMessageBoxButtons p_ebbOptions, MessageBoxIcon p_mbiMessageType)
+			: this(p_strMessage, null, p_strTitle, p_ebbOptions, p_mbiMessageType)
+		{
+		}
+
+		/// <summary>
+		/// A siple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_strMessage">The message to display.</param>
+		/// <param name="p_strDetails">The details to display.</param>
+		/// <param name="p_strTitle">The title of the message.</param>
+		/// <param name="p_ebbOptions">The set of choices to offer the user.</param>
+		/// <param name="p_mbiMessageType">The type, or severity, of the message.</param>
+		public ViewMessage(string p_strMessage, string p_strDetails, string p_strTitle, ExtendedMessageBoxButtons p_ebbOptions, MessageBoxIcon p_mbiMessageType)
+		{
+			Message = p_strMessage;
+			Details = p_strDetails;
+			Title = p_strTitle;
+			Options = p_ebbOptions;
+			MessageType = p_mbiMessageType;
+		}
+
+		#endregion
+	}
+}
---
+++ Transactions/PreparingEnlistment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Nexus.Transactions
+{
+	/// <summary>
+	/// This class is used to communicate with enlistments.
+	/// </summary>
+	public class PreparingEnlistment : Enlistment
+	{
+		/// <summary>
+		/// Gets or sets whether the enlistmant is voting to commit, rollback, or abstains.
+		/// </summary>
+		/// <value>Whether the enlistmant is voting to commit, rollback, or abstains.</value>
+		internal bool? VoteToCommit { get; set; }
+
+		/// <summary>
+		/// Marks the enlistmant as having finished.
+		/// </summary>
+		public override void Done()
+		{
+			base.Done();
+			if (!VoteToCommit.HasValue)
+				VoteToCommit = true;
+		}
+
+		/// <summary>
+		/// Causes the enlistment to vote to commit.
+		/// </summary>
+		public void Prepared()
+		{
+			VoteToCommit = true;
+		}
+
+		/// <summary>
+		/// Causes the enlistment to vote to rollback.
+		/// </summary>
+		public void ForceRollback()
+		{
+			VoteToCommit = false;
+		}
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public PreparingEnlistment()
+		{
+			VoteToCommit = null;
+		}
+	}
+}
