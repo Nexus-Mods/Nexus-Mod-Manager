34c8e18afb629b993891e96459da0d16ef62371b
duskdweller <duskdweller@localhost>
2013-01-31 22:13:26 +0100

    NMM will try to save the game specific Mods/Install Info folders in the registry (this should prevent users missing their mod list and install info after a complete reinstall).

---
+++ NexusClient/PluginManagement/UI/Exceptions/InvalidImportSourceException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nexus.Client.PluginManagement.UI
+{
+	/// <summary>
+	/// The exception that is thrown when a load order import source is invalid.
+	/// </summary>
+	public class InvalidImportSourceException : Exception
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InvalidImportSourceException"/> class with the given message.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public InvalidImportSourceException(string message)
+			: base(message)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InvalidImportSourceException"/> class with the given message and inner exception.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		/// <param name="innerException">The exception that is the cause of the current exception.</param>
+		public InvalidImportSourceException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+
+		#endregion
+	}
+}
