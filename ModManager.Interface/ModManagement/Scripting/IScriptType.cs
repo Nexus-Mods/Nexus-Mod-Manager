9c88a650fd9eefc0cdb4f1c65210abf05e6ac709
duskdweller <duskdweller@localhost>
2013-02-01 14:44:15 +0100

    The side panel should now be less intrusive on category view mode.

---
+++ ModManager.Interface/ModManagement/Scripting/IScriptType.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using Nexus.UI.Controls;
+using Nexus.Client.PluginManagement;
+using Nexus.Client.Mods;
+using Nexus.Client.Games;
+using System.Threading;
+
+namespace Nexus.Client.ModManagement.Scripting
+{
+	/// <summary>
+	/// Describes the properties and methods of a specifc type of mod script.
+	/// </summary>
+	public interface IScriptType
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets the name of the script type.
+		/// </summary>
+		/// <value>The name of the script type.</value>
+		string TypeName { get; }
+
+		/// <summary>
+		/// Gets the unique id of the script type.
+		/// </summary>
+		/// <value>The unique id of the script type.</value>
+		string TypeId { get; }
+
+		/// <summary>
+		/// Gets the list of file names used by scripts of the current type.
+		/// </summary>
+		/// <remarks>
+		/// The list is in order of preference, with the first item being the preferred
+		/// file name.
+		/// </remarks>
+		/// <value>The list of file names used by scripts of the current type.</value>
+		IList<string> FileNames { get; }
+
+		#endregion
+
+		/// <summary>
+		/// Creates an editor for the script type.
+		/// </summary>
+		/// <param name="p_lstModFiles">The list of files if the current mod.</param>
+		/// <returns>An editor for the script type.</returns>
+		IScriptEditor CreateEditor(IList<VirtualFileSystemItem> p_lstModFiles);
+
+		/// <summary>
+		/// Creates an executor that can run the script type.
+		/// </summary>
+		/// <param name="p_modMod">The mod being installed.</param>
+		/// <param name="p_gmdGameMode">The game mode currently bieng managed.</param>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		/// <param name="p_igpInstallers">The utility class to use to install the mod items.</param>
+		/// <param name="p_scxUIContext">The <see cref="SynchronizationContext"/> to use to marshall UI interactions to the UI thread.</param>
+		/// <returns>An executor that can run the script type.</returns>
+		IScriptExecutor CreateExecutor(IMod p_modMod, IGameMode p_gmdGameMode, IEnvironmentInfo p_eifEnvironmentInfo, InstallerGroup p_igpInstallers, SynchronizationContext p_scxUIContext);
+
+		/// <summary>
+		/// Loads the script from the given text representation.
+		/// </summary>
+		/// <param name="p_strScriptData">The text to convert into a script.</param>
+		/// <returns>The <see cref="IScript"/> represented by the given data.</returns>
+		IScript LoadScript(string p_strScriptData);
+
+		/// <summary>
+		/// Saves the given script into a text representation.
+		/// </summary>
+		/// <param name="p_scpScript">The <see cref="IScript"/> to save.</param>
+		/// <returns>The text represnetation of the given <see cref="IScript"/>.</returns>
+		string SaveScript(IScript p_scpScript);
+
+		/// <summary>
+		/// Determines if the given script is valid.
+		/// </summary>
+		/// <param name="p_scpScript">The script to validate.</param>
+		/// <returns><c>true</c> if the given script is valid;
+		/// <c>false</c> otherwise.</returns>
+		bool ValidateScript(IScript p_scpScript);
+	}
+}
---
+++ Util/Collections/ObservableSet.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace Nexus.Client.Util.Collections
+{
+	/// <summary>
+	/// A Set implementation that provides notifications when items are added or removed.
+	/// </summary>
+	/// <typeparam name="T">The type of objects in the Set.</typeparam>
+	public class ObservableSet<T> : Set<T>, INotifyCollectionChanged, INotifyPropertyChanged
+	{
+		#region Constructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public ObservableSet()
+		{
+		}
+
+		/// <summary>
+		/// A constructor that fills the set with the given items.
+		/// </summary>
+		/// <param name="p_enmItems">the items to add to the set.</param>
+		public ObservableSet(IEnumerable<T> p_enmItems)
+			: base(p_enmItems)
+		{
+		}
+
+		/// <summary>
+		/// A constructor that fills the set with the given items, and allows the specification of a custom comparer.
+		/// </summary>
+		/// <param name="p_enmItems">the items to add to the set.</param>
+		/// <param name="p_cmpComparer">The comparer to use when determining if an item is already in the set.</param>
+		public ObservableSet(IEnumerable<T> p_enmItems, IComparer<T> p_cmpComparer)
+			: base(p_enmItems, p_cmpComparer)
+		{
+		}
+
+		/// <summary>
+		/// A constructor that allows the specification of a custom comparer.
+		/// </summary>
+		/// <param name="p_cmpComparer">The comparer to use when determining if an item is already in the set.</param>
+		public ObservableSet(IComparer<T> p_cmpComparer)
+			: base(p_cmpComparer)
+		{
+		}
+
+		/// <summary>
+		/// The copy constructor.
+		/// </summary>
+		/// <param name="p_setCopy">The set to copy.</param>
+		public ObservableSet(Set<T> p_setCopy)
+			: base(p_setCopy)
+		{
+		}
+
+		#endregion
+
+		#region INotifyCollectionChanged Members
+
+		/// <summary>
+		/// Raised when the collection changes.
+		/// </summary>
+		public event NotifyCollectionChangedEventHandler CollectionChanged = delegate { };
+
+		#endregion
+
+		#region INotifyPropertyChanged Members
+
+		/// <summary>
+		/// Raised when a property of the collection changes.
+		/// </summary>
+		protected event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		/// <summary>
+		/// Raised when a property of the collection changes.
+		/// </summary>
+		event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+		{
+			add
+			{
+				PropertyChanged += value;
+			}
+			remove
+			{
+				PropertyChanged -= value;
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Raises the <see cref="CollectionChanged"/> event.
+		/// </summary>
+		/// <param name="e">A <see cref="NotifyCollectionChangedEventArgs"/> describing the event arguments.</param>
+		protected void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+		{
+			CollectionChanged(this, e);
+		}
+
+		/// <summary>
+		/// Raises the <see cref="PropertyChanged"/> event.
+		/// </summary>
+		/// <param name="e">A <see cref="PropertyChangedEventArgs"/> describing the event arguments.</param>
+		protected void OnPropertyChanged(PropertyChangedEventArgs e)
+		{
+			PropertyChanged(this, e);
+		}
+
+		/// <summary>
+		/// Adds the given item to the set.
+		/// </summary>
+		/// <param name="p_tItem">The item to add.</param>
+		public override void Add(T p_tItem)
+		{
+			if (!Contains(p_tItem))
+			{
+				base.Add(p_tItem);
+				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, p_tItem));
+				PropertyChanged(this, new PropertyChangedEventArgs(ObjectHelper.GetPropertyName(() => Count)));
+			}
+		}
+
+		/// <summary>
+		/// Empties the set.
+		/// </summary>
+		public override void Clear()
+		{
+			if (Count > 0)
+			{
+				base.Clear();
+				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+				PropertyChanged(this, new PropertyChangedEventArgs(ObjectHelper.GetPropertyName(() => Count)));
+			}
+		}
+
+		/// <summary>
+		/// Removes the item form the set at the given index.
+		/// </summary>
+		/// <param name="index">The index of the item to remove from the set.</param>
+		public override void RemoveAt(int index)
+		{
+			T tItem = this[index];
+			base.RemoveAt(index);
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, tItem, index));
+			PropertyChanged(this, new PropertyChangedEventArgs(ObjectHelper.GetPropertyName(() => Count)));
+		}
+	}
+}
