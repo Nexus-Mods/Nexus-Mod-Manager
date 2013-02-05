2c581dcf9a45226087e2d976699cef95e8825629
duskdweller <duskdweller@localhost>
2013-02-05 15:38:23 +0100

    NMM setting to handle whether the user decided to show or hide empty categories.

---
+++ Skyrim/Tools/AI/ArchiveInvalidation.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Nexus.Client.Games.Gamebryo.Tools.AI;
+using Nexus.Client.Util;
+using Nexus.Client.Games.Tools;
+using Nexus.Client.Commands;
+using Nexus.Client.Games.Gamebryo;
+
+namespace Nexus.Client.Games.Skyrim.Tools.AI
+{
+	/// <summary>
+	/// Controls ArchiveInvalidation.
+	/// </summary>
+	public class ArchiveInvalidation : ITool
+	{
+		#region Events
+
+		/// <summary>
+		/// Notifies listeners that the tool wants a view displayed.
+		/// </summary>
+		public event EventHandler<DisplayToolViewEventArgs> DisplayToolView = delegate { };
+
+		/// <summary>
+		/// Notifies listeners that the tool wants a view closed.
+		/// </summary>
+		public event EventHandler<DisplayToolViewEventArgs> CloseToolView = delegate { };
+
+		#endregion
+
+		private IToolView m_tvwToolView = null;
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the command to launch the tool.
+		/// </summary>
+		/// <value>The command to launch the tool.</value>
+		public Command LaunchCommand { get; private set; }
+
+		/// <summary>
+		/// Gets the current game mode.
+		/// </summary>
+		/// <value>The current game mode.</value>
+		protected SkyrimGameMode GameMode { get; private set; }
+
+		#endregion
+
+		/// <summary>
+		/// Confirms that AI should be reset.
+		/// </summary>
+		public Func<bool> ConfirmAiReset = delegate { return true; };
+
+		#region Constructor
+
+		/// <summary>
+		/// Initialized the object with the given values.
+		/// </summary>
+		/// <param name="p_gmdGameMode">The current game mode.</param>
+		public ArchiveInvalidation(SkyrimGameMode p_gmdGameMode)
+		{
+			GameMode = p_gmdGameMode;
+			LaunchCommand = new Command("Reset Archive Invalidation", "Resets Archive Invalidation.", ResetArchiveInvalidation);
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Sets the view to use for this tool.
+		/// </summary>
+		/// <param name="p_tvwToolView">The view to use for this tool.</param>
+		public void SetToolView(IToolView p_tvwToolView)
+		{
+			m_tvwToolView = p_tvwToolView;
+		}
+
+		/// <summary>
+		/// Resets archive invalidation.
+		/// </summary>
+		public void ResetArchiveInvalidation()
+		{
+			DisplayToolViewEventArgs teaArgs = new DisplayToolViewEventArgs(m_tvwToolView, true);
+			DisplayToolView(this, teaArgs);
+			ApplyAI();
+			CloseToolView(this, teaArgs);
+		}
+
+		/// <summary>
+		/// Enables AI.
+		/// </summary>
+		protected void ApplyAI()
+		{
+			if (ConfirmAiReset())
+			{
+				string strPluginsPath = GameMode.PluginDirectory;
+				foreach (FileInfo fi in new DirectoryInfo(strPluginsPath).GetFiles("Skyrim - *.bsa"))
+					fi.LastWriteTime = new DateTime(2008, 10, 1);
+			}
+		}
+	}
+}
---
+++ WorldOfTanks.ModScript/WoTModScriptInterpreter.cs
@@ -0,0 +1,34 @@
+using Nexus.Client.ModManagement.Scripting.ModScript;
+
+namespace Nexus.Client.Games.WorldOfTanks.Scripting.ModScript
+{
+    /// <summary>
+    /// Interpets and executes the given WorldOfTanks Mod Script script.
+    /// </summary>
+    public class WoTModScriptInterpreter : ModScriptInterpreter
+    {
+        #region Constructors
+
+        /// <summary>
+        /// A simple construtor that initializes the object with the given values.
+        /// </summary>
+        /// <param name="p_msfFunctions">The object that implements the script functions.</param>
+        /// <param name="p_strScript">The script to execute.</param>
+        public WoTModScriptInterpreter(ModScriptFunctionProxy p_msfFunctions, string p_strScript)
+            : base(p_msfFunctions, p_strScript)
+        {
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Creates the context object that tracks the state of the script being executed.
+        /// </summary>
+        /// <param name="p_msfFunctions">The object that implements the script functions.</param>
+        /// <returns>The context object to use to track the state of the script being executed.</returns>
+        protected override ModScriptInterpreterContext CreateInterpreterContext(ModScriptFunctionProxy p_msfFunctions)
+        {
+            return new WoTModScriptInterpreterContext(p_msfFunctions);
+        }
+    }
+}
