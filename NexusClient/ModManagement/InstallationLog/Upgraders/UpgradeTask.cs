f0daaee50604c56ee396c093c4ea174d4bcc75e0
duskdweller <duskdweller@localhost>
2013-02-08 16:57:06 +0100

    Adds a visual feedback to the list when a new mod is added under a category node.

---
+++ NexusClient/ModManagement/InstallationLog/Upgraders/UpgradeTask.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+using ChinhDo.Transactions;
+using Nexus.Transactions;
+using Nexus.Client.BackgroundTasks;
+
+namespace Nexus.Client.ModManagement.InstallationLog.Upgraders
+{
+	/// <summary>
+	/// Upgrades the Install Log from a specific version to the latest version.
+	/// </summary>
+	/// <remarks>
+	/// This base class handles setting up the common resources and transaction required for all
+	/// log upgrades.
+	/// </remarks>
+	public abstract class UpgradeTask : ThreadedBackgroundTask
+	{
+		private TxFileManager m_tfmFileManager = null;
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the transactional file manager to be used in the upgrade.
+		/// </summary>
+		protected TxFileManager FileManager
+		{
+			get
+			{
+				return m_tfmFileManager;
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Called to perform the upgrade.
+		/// </summary>
+		/// <remarks>
+		/// Sets up the resources required to upgrade the install log.
+		/// </remarks>
+		/// <param name="p_mdrManagedModRegistry">The <see cref="ModRegistry"/> that contains the list
+		/// of managed mods.</param>
+		/// <param name="p_strModInstallDirectory">The path of the directory where all of the mods are installed.</param>
+		/// <param name="p_strLogPath">The path from which to load the install log information.</param>
+		public void Upgrade(string p_strLogPath, string p_strModInstallDirectory, ModRegistry p_mdrManagedModRegistry)
+		{
+			Trace.WriteLine("Beginning Install Log Upgrade.");
+
+			m_tfmFileManager = new TxFileManager();
+			using (TransactionScope tsTransaction = new TransactionScope())
+			{
+				m_tfmFileManager.Snapshot(p_strLogPath);
+				Start(p_strLogPath, p_strModInstallDirectory, p_mdrManagedModRegistry);
+				tsTransaction.Complete();
+				m_tfmFileManager = null;
+			}
+		}
+
+		/// <summary>
+		/// Performs the actual upgrade.
+		/// </summary>
+		/// <param name="p_objArgs">The task arguments.</param>
+		/// <returns>A status message.</returns>
+		protected override object DoWork(object[] p_objArgs)
+		{
+			try
+			{
+				UpgradeInstallLog((string)p_objArgs[0], (string)p_objArgs[1], (ModRegistry)p_objArgs[2]);
+				return null;
+			}
+			catch (UpgradeException e)
+			{
+				Status = TaskStatus.Error;
+				return e.Message;
+			}
+		}
+
+		/// <summary>
+		/// Upgrades the install log.
+		/// </summary>
+		/// <param name="p_mrgModRegistry">The <see cref="ModRegistry"/> that contains the list
+		/// of managed mods.</param>
+		/// <param name="p_strModInstallDirectory">The path of the directory where all of the mods are installed.</param>
+		/// <param name="p_strLogPath">The path from which to load the install log information.</param>
+		protected abstract void UpgradeInstallLog(string p_strLogPath, string p_strModInstallDirectory, ModRegistry p_mrgModRegistry);
+	}
+}
---
+++ OMod/OMod.DecoderProgressWatcher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Nexus.Client.Util;
+using SevenZip.Sdk;
+
+namespace Nexus.Client.Mods.Formats.OMod
+{
+	public partial class OMod
+	{
+		/// <summary>
+		/// This class watches to progress of the decompression of 7zip
+		/// compressed file blicks in the OMod.
+		/// </summary>
+		/// <remarks>
+		/// In the current implementation of the compression library we are using
+		/// (SevenZipSharp), even though the
+		/// <see cref="SevenZip.Sdk.Compression.Lzma.Decoder.Code(System.IO.Stream, System.IO.Stream, long, long, ICodeProgress)"/>
+		/// method accepts an <see cref="ICodeProgress"/> argument, it is unused. Thus
+		/// progress is not reported. We have implemented here anyway so that
+		/// if it is ever implemented we'll be ready.
+		/// </remarks>
+		public class DecoderProgressWatcher : ICodeProgress
+		{
+			#region Events
+
+			/// <summary>
+			/// Raised when the progress of the decoder being watched has been updated.
+			/// </summary>
+			public event EventHandler<EventArgs<Int32>> ProgressUpdated = delegate { };
+
+			#endregion
+
+			private Int64 m_intTotalSize = 0;
+
+			#region Constructors
+
+			/// <summary>
+			/// A simple constructor that initializes the object with the given values.
+			/// </summary>
+			/// <param name="p_intTotalSize">The total size of the file block being decoded.</param>
+			public DecoderProgressWatcher(Int64 p_intTotalSize)
+			{
+				m_intTotalSize = p_intTotalSize;
+			}
+
+			#endregion
+
+			#region ICodeProgress Members
+
+			/// <summary>
+			/// Reports the progress of the decoder.
+			/// </summary>
+			/// <param name="inSize">The number of bytes of the compress file block that have been processed.</param>
+			/// <param name="outSize">The number of uncompressed bytes generated.</param>
+			public void SetProgress(long inSize, long outSize)
+			{
+				ProgressUpdated(this, new EventArgs<Int32>((Int32)((float)inSize / m_intTotalSize * 100.0)));
+			}
+
+			#endregion
+		}
+	}
+}
