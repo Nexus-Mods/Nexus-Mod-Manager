a53657f5d2738fa22847ad7778a8dcefa0e96a20
duskdweller <duskdweller@localhost>
2013-02-13 13:21:52 +0100

    Logic error in the update check method.

---
+++ UI/Controls/IStatusProviderAware.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Nexus.UI.Controls
+{
+	/// <summary>
+	/// An interface that allows a control to specify where to display the icon
+	/// of a <see cref="SiteStatusProvider"/>.
+	/// </summary>
+	public interface IStatusProviderAware
+	{
+		/// <summary>
+		/// Gets the child control next to which to display the icon.
+		/// </summary>
+		/// <value>The child control next to which to display the icon.</value>
+		Control StatusProviderSite
+		{
+			get;
+		}
+	}
+}
