60174fd9109c03868789ae79b935a021ebab7ff2
duskdweller <duskdweller@localhost>
2013-02-14 19:07:22 +0100

    Closing branch in preparation for merge.

--- DarkSouls/Properties/AssemblyInfo.cs
+++ DarkSouls/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4762.24361")]
+[assembly: AssemblyFileVersion("1.1.4793.32556")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
--- Skyrim.CSharpScript/Properties/AssemblyInfo.cs
+++ Skyrim.CSharpScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4763.21314")]
+[assembly: AssemblyFileVersion("1.1.4793.32557")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
