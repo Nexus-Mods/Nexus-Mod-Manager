c871f1c5163599aeac713ae5ad52e4fa706d24be
duskdweller <duskdweller@localhost>
2013-02-14 12:57:43 +0100

    Fixes the crash caused by illegal paths during the game scan.

---
+++ NexusClient.Interface/UI/Delegates.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Nexus.Client.UI
+{
+	/// <summary>
+	/// Displays a view.
+	/// </summary>
+	/// <param name="p_vewView">The view to display.</param>
+	/// <param name="p_booModal">Wheher the view should be modal.</param>
+	/// <returns>The return value of the displayed view.</returns>
+	public delegate object ShowViewDelegate(IView p_vewView, bool p_booModal);
+
+	/// <summary>
+	/// Displays a message.
+	/// </summary>
+	/// <param name="p_vmgMessage">The properties of the message to display.</param>
+	/// <returns>The return value of the displayed message.</returns>
+	public delegate object ShowMessageDelegate(ViewMessage p_vmgMessage);
+
+	/// <summary>
+	/// Confirms an action.
+	/// </summary>
+	/// <param name="p_strMessage">The message describing the action to confirm.</param>
+	/// <param name="p_strTitle">The title of the action to confirm.</param>
+	/// <returns><c>true</c> if the action has been confirmed;
+	/// <c>false</c> otherwise.</returns>
+	public delegate bool ConfirmActionMethod(string p_strMessage, string p_strTitle);
+}
---
+++ XmlScript/GameVersionCondition.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Nexus.Client.ModManagement.Scripting.XmlScript
+{
+	/// <summary>
+	/// A condition that requires a minimum version of the game to be installed.
+	/// </summary>
+	public class GameVersionCondition : VersionCondition
+	{
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_verVersion">The minimum required version of the game.</param>
+		public GameVersionCondition(Version p_verVersion)
+			: base(p_verVersion)
+		{
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Gets whether or not the condition is fulfilled.
+		/// </summary>
+		/// <remarks>
+		/// The dependency is fulfilled if the specified minimum version of
+		/// the game is installed.
+		/// </remarks>
+		/// <param name="p_csmStateManager">The manager that tracks the currect install state.</param>
+		/// <returns><c>true</c> if the condition is fulfilled;
+		/// <c>false</c> otherwise.</returns>
+		/// <seealso cref="ICondition.GetIsFulfilled(ConditionStateManager)"/>
+		public override bool GetIsFulfilled(ConditionStateManager p_csmStateManager)
+		{
+			Version verInstalledVersion = p_csmStateManager.GameMode.GameVersion;
+			return ((verInstalledVersion != null) && (verInstalledVersion >= MinimumVersion));
+		}
+
+		/// <summary>
+		/// Gets a message describing whether or not the condition is fulfilled.
+		/// </summary>
+		/// If the dependency is fulfilled the message is "Passed." If the dependency is not fulfilled the
+		/// message informs the user of the installed version.
+		/// <param name="p_csmStateManager">The manager that tracks the currect install state.</param>
+		/// <returns>A message describing whether or not the condition is fulfilled.</returns>
+		/// <seealso cref="ICondition.GetMessage(ConditionStateManager)"/>
+		public override string GetMessage(ConditionStateManager p_csmStateManager)
+		{
+			Version verInstalledVersion = p_csmStateManager.GameMode.GameVersion;
+			if (verInstalledVersion < MinimumVersion)
+				return String.Format("This mod requires v{0} or higher of the game. You have {1}. Please update your game.", MinimumVersion, verInstalledVersion);
+			return "Passed";
+		}
+	}
+}
