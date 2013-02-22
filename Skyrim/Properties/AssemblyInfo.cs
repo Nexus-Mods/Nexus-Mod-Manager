f35728b88b95bac615441ef39390869f4a532e33
duskdweller <duskdweller@localhost>
2013-02-22 20:31:28 +0100

    Closing branch in preparation for merge.

--- Skyrim/Properties/AssemblyInfo.cs
+++ Skyrim/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4793.32556")]
+[assembly: AssemblyFileVersion("1.1.4801.34742")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
