241a66783abc201f443c2700b200c68af67857b8
duskdweller <duskdweller@localhost>
2013-02-26 18:45:37 +0100

    Enables the updatecheckwarning toggle when the user clicks on a category.

---
+++ NexusClient/ModRepositories/ModFileCategory.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Nexus.Client.ModRepositories
+{
+	/// <summary>
+	/// The mod file categories.
+	/// </summary>
+	public enum ModFileCategory
+	{
+		/// <summary>
+		/// Indicates the file ia a main file in the mod.
+		/// </summary>
+		MainFiles=1,
+
+		/// <summary>
+		/// Indicates the files is an update for the mod.
+		/// </summary>
+		Updates=2,
+
+		/// <summary>
+		/// Indicates teh file is optional for the mod.
+		/// </summary>
+		OptionalFiles=3,
+
+		/// <summary>
+		/// Indicates the file is for an old version of the mod.
+		/// </summary>
+		OldVersions=4,
+
+		/// <summary>
+		/// Indicates the files is a support file for the mod.
+		/// </summary>
+		Misc=5
+	}
+}
