d62cc9990e6c4e9b2c742d9c16b61b2e41253e74
duskdweller <duskdweller@localhost>
2013-02-27 16:13:48 +0100

    Fixes a rare crash while the program tried to remove an already removed temporary install folder.

---
+++ FalloutNV.XmlScript/XmlScript40NodeAdapter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Nexus.UI.Controls;
+using Nexus.Client.Games.Fallout3.Scripting.XmlScript.CPL;
+using Nexus.Client.ModManagement.Scripting.XmlScript;
+using Nexus.Client.ModManagement.Scripting.XmlScript.UI.Controls;
+using Nexus.Client.ModManagement.Scripting.XmlScript.UI.Controls.NodeEditors;
+using Nexus.Client.Games.Fallout3.Scripting.XmlScript;
+using Nexus.Client.Games.FalloutNV.Scripting.XmlScript.CPL;
+
+namespace Nexus.Client.Games.FalloutNV.Scripting.XmlScript
+{
+	/// <summary>
+	/// Adapts a version 4.0 <see cref="XmlScript"/> to an XML script editor.
+	/// </summary>
+	public class XmlScript40NodeAdapter : Nexus.Client.Games.Fallout3.Scripting.XmlScript.XmlScript40NodeAdapter
+	{
+		#region Construtors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_xstScripType">The current xml script type.</param>
+		public XmlScript40NodeAdapter(XmlScriptType p_xstScripType)
+			: base(p_xstScripType)
+		{
+		}
+
+		#endregion
+
+		#region IXmlScriptNodeAdapter Members
+
+		/// <summary>
+		/// Gets the editor to use to edit an <see cref="InstallStep"/>.
+		/// </summary>
+		/// <param name="p_ispStep">The <see cref="InstallStep"/> to edit.</param>
+		/// <param name="p_lstModFiles">The list of files in the mod to which the <see cref="XmlScript"/>
+		/// being edited belongs.</param>
+		/// <returns>The editor to use to edit an <see cref="InstallStep"/>. <c>null</c> is returned if the
+		/// current <see cref="XmlScript"/> does not support editing <see cref="InstallStep"/>s.</returns>
+		public override NodeEditor GetInstallStepEditor(InstallStep p_ispStep, IList<VirtualFileSystemItem> p_lstModFiles)
+		{
+			FONVCplConverter cvtConverter = new FONVCplConverter(ScriptType.GetCplParserFactory());
+			ConditionEditorVM vmlConditionEditor = CreateConditionEditorVM(p_lstModFiles, cvtConverter);
+
+			InstallStepEditorVM vmlStepEditor = new InstallStepEditorVM(vmlConditionEditor, p_ispStep, InstallStepProperties.Name | InstallStepProperties.GroupSortOrder | InstallStepProperties.Visibility);
+			return new InstallStepEditor(vmlStepEditor);
+		}
+
+		#endregion
+	}
+}
---
+++ NexusClient/IMessager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Nexus.Client
+{
+	/// <summary>
+	/// The methods provided for communication bewtween multiple instances of the mod manager.
+	/// </summary>
+	public interface IMessager : IDisposable
+	{
+		#region Mod Addition
+
+		/// <summary>
+		/// Adds the specified mod to the mod manager.
+		/// </summary>
+		/// <param name="p_strFilePath">The path or URL of the mod to add to the mod manager.</param>
+		void AddMod(string p_strFilePath);
+
+		#endregion
+
+		/// <summary>
+		/// Brings the currently running client to the front.
+		/// </summary>
+		void BringToFront();
+
+		/// <summary>Used as a simple Power On Self Test method.</summary>
+		/// <remarks>
+		/// This method can be called to ensure a Messager is alive.
+		/// </remarks>
+		void Post();
+	}
+}
