3330231e70dfcfe55d359dfa19d0db3f9a2fd25a
duskdweller <duskdweller@localhost>
2013-03-01 15:19:08 +0100

    The mod list view is now loaded into the new control.

---
+++ CSharpScript/CSharpScriptFunctionProxy.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Nexus.Client.Games;
+using Nexus.Client.Mods;
+using Nexus.Client.Util;
+using Nexus.Client.Util.Collections;
+
+namespace Nexus.Client.ModManagement.Scripting.CSharpScript
+{
+	/// <summary>
+	/// Implements the functions availabe to C# scripts.
+	/// </summary>
+	public class CSharpScriptFunctionProxy : ScriptFunctionProxy
+	{
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_modMod">The mod for which the script is running.</param>
+		/// <param name="p_gmdGameMode">The game mode currently being managed.</param>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		/// <param name="p_igpInstallers">The utility class to use to install the mod items.</param>
+		/// <param name="p_uipUIProxy">The UI manager to use to interact with UI elements.</param>
+		public CSharpScriptFunctionProxy(IMod p_modMod, IGameMode p_gmdGameMode, IEnvironmentInfo p_eifEnvironmentInfo, InstallerGroup p_igpInstallers, UIUtil p_uipUIProxy)
+			: base(p_modMod, p_gmdGameMode, p_eifEnvironmentInfo, p_igpInstallers, p_uipUIProxy)
+		{
+		}
+
+		#endregion
+
+		#region File Management
+
+		/// <summary>
+		/// Installs the specified file from the mod to the specified location on the file system.
+		/// </summary>
+		/// <remarks>
+		/// This is the legacy form of <see cref="ScriptFunctionProxy.InstallFileFromMod(string, string)"/>. It now just calls
+		/// <see cref="ScriptFunctionProxy.InstallFileFromMod(string, string)"/>.
+		/// </remarks>
+		/// <param name="p_strFrom">The path of the file in the mod to install.</param>
+		/// <param name="p_strTo">The path on the file system where the file is to be created.</param>
+		/// <returns><c>true</c> if the file was written; <c>false</c> otherwise.</returns>
+		/// <seealso cref="ScriptFunctionProxy.InstallFileFromMod(string, string)"/>
+		public bool CopyDataFile(string p_strFrom, string p_strTo)
+		{
+			return InstallFileFromMod(p_strFrom, p_strTo);
+		}
+
+		#endregion
+
+		#region UI
+
+		#region Select
+
+		/// <summary>
+		/// Displays a selection form to the user.
+		/// </summary>
+		/// <param name="p_sopOptions">The options from which to select.</param>
+		/// <param name="p_strTitle">The title of the selection form.</param>
+		/// <param name="p_booSelectMany">Whether more than one item can be selected.</param>
+		/// <returns>The indices of the selected items.</returns>
+		public int[] Select(SelectOption[] p_sopOptions, string p_strTitle, bool p_booSelectMany)
+		{
+			bool booHasPreviews = false;
+			bool booHasDescriptions = false;
+			foreach (SelectOption so in p_sopOptions)
+			{
+				if (so.Preview != null)
+					booHasPreviews = true;
+				if (so.Desc != null)
+					booHasDescriptions = true;
+			}
+			string[] strItems = new string[p_sopOptions.Length];
+			Image[] imgPreviews = booHasPreviews ? new Image[p_sopOptions.Length] : null;
+			string[] strDescriptions = booHasDescriptions ? new string[p_sopOptions.Length] : null;
+			for (int i = 0; i < p_sopOptions.Length; i++)
+			{
+				strItems[i] = p_sopOptions[i].Item;
+				if (booHasPreviews)
+					imgPreviews[i] = new ExtendedImage(Mod.GetFile(p_sopOptions[i].Preview));
+				if (booHasDescriptions)
+					strDescriptions[i] = p_sopOptions[i].Desc;
+			}
+			return Select(strItems, imgPreviews, strDescriptions, p_strTitle, p_booSelectMany);
+		}
+
+		/// <summary>
+		/// Displays a selection form to the user.
+		/// </summary>
+		/// <remarks>
+		/// The items, previews, and descriptions are repectively ordered. In other words,
+		/// the i-th item in <paramref name="p_strItems"/> uses the i-th preview in
+		/// <paramref name="p_strPreviewPaths"/> and the i-th description in <paramref name="p_strDescriptions"/>.
+		/// 
+		/// Similarly, the idices return as results correspond to the indices of the items in
+		/// <paramref name="p_strItems"/>.
+		/// </remarks>
+		/// <param name="p_strItems">The items from which to select.</param>
+		/// <param name="p_strPreviewPaths">The preview image file names for the items.</param>
+		/// <param name="p_strDescriptions">The descriptions of the items.</param>
+		/// <param name="p_strTitle">The title of the selection form.</param>
+		/// <param name="p_booSelectMany">Whether more than one item can be selected.</param>
+		/// <returns>The indices of the selected items.</returns>
+		public int[] Select(string[] p_strItems, string[] p_strPreviewPaths, string[] p_strDescriptions, string p_strTitle, bool p_booSelectMany)
+		{
+			Image[] imgPreviews = null;
+			if (p_strPreviewPaths != null)
+			{
+				imgPreviews = new Image[p_strPreviewPaths.Length];
+				for (Int32 i = 0; i < p_strPreviewPaths.Length; i++)
+					if (!String.IsNullOrEmpty(p_strPreviewPaths[i]))
+						imgPreviews[i] = new ExtendedImage(Mod.GetFile(p_strPreviewPaths[i]));
+			}
+			return Select(p_strItems, imgPreviews, p_strDescriptions, p_strTitle, p_booSelectMany);
+		}
+
+		/// <summary>
+		/// Displays a selection form to the user.
+		/// </summary>
+		/// <remarks>
+		/// The items, previews, and descriptions are repectively ordered. In other words,
+		/// the i-th item in <paramref name="p_strItems"/> uses the i-th preview in
+		/// <paramref name="p_imgPreviews"/> and the i-th description in <paramref name="p_strDescriptions"/>.
+		/// 
+		/// Similarly, the idices return as results correspond to the indices of the items in
+		/// <paramref name="p_strItems"/>.
+		/// </remarks>
+		/// <param name="p_strItems">The items from which to select.</param>
+		/// <param name="p_imgPreviews">The preview images for the items.</param>
+		/// <param name="p_strDescriptions">The descriptions of the items.</param>
+		/// <param name="p_strTitle">The title of the selection form.</param>
+		/// <param name="p_booSelectMany">Whether more than one item can be selected.</param>
+		/// <returns>The indices of the selected items.</returns>
+		public int[] Select(string[] p_strItems, Image[] p_imgPreviews, string[] p_strDescriptions, string p_strTitle, bool p_booSelectMany)
+		{
+			List<Nexus.Client.ModManagement.Scripting.SelectOption> lstOptions = new List<Nexus.Client.ModManagement.Scripting.SelectOption>();
+			for (Int32 i = 0; i < p_strItems.Length; i++)
+			{
+				string strDescription = p_strDescriptions.IsNullOrEmpty() ? null : p_strDescriptions[i];
+				Image imgPreview = p_imgPreviews.IsNullOrEmpty() ? null : p_imgPreviews[i];
+				lstOptions.Add(new Nexus.Client.ModManagement.Scripting.SelectOption(p_strItems[i], false, strDescription, imgPreview));
+			}
+			string[] strSelections = UIManager.Select(lstOptions, p_strTitle, p_booSelectMany);
+			List<Int32> lstSelectionIndices = new List<Int32>();
+			foreach (string strSelection in strSelections)
+				lstSelectionIndices.Add(Array.IndexOf(p_strItems, strSelection));
+			return lstSelectionIndices.ToArray();
+		}
+
+		#endregion
+
+		#endregion
+	}
+}
---
+++ ModManager.Interface/Games/FileSet.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Nexus.Client.Games
+{
+	/// <summary>
+	/// An indexed list of file paths.
+	/// </summary>
+	public class FileSet : IEnumerable<string>
+	{
+		StringDictionary m_sdcFilePaths = new StringDictionary();
+
+		/// <summary>
+		/// Gets or sets the file path for the given key.
+		/// </summary>
+		/// <param name="p_strFileKey">The key corresponding to the desired file path.</param>
+		/// <returns>The file path for the given key.</returns>
+		public string this[string p_strFileKey]
+		{
+			get
+			{
+				if (!m_sdcFilePaths.ContainsKey(p_strFileKey))
+					return null;
+				return m_sdcFilePaths[p_strFileKey];
+			}
+			set
+			{
+				m_sdcFilePaths[p_strFileKey] = value;
+			}
+		}
+
+		#region IEnumerable Members
+
+		/// <summary>
+		/// Gets an enumerator this iterates over the file paths.
+		/// </summary>
+		/// <returns>An enumerator this iterates over the file paths.</returns>
+		public IEnumerator GetEnumerator()
+		{
+			return m_sdcFilePaths.Values.GetEnumerator();
+		}
+
+		#endregion
+
+		#region IEnumerable<string> Members
+
+		/// <summary>
+		/// Gets an enumerator this iterates over the file paths.
+		/// </summary>
+		/// <returns>An enumerator this iterates over the file paths.</returns>
+		IEnumerator<string> IEnumerable<string>.GetEnumerator()
+		{
+			foreach (string strFile in m_sdcFilePaths.Values)
+				yield return strFile;
+		}
+
+		#endregion
+	}
+}
