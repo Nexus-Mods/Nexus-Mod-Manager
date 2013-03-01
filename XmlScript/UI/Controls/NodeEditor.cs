0bddc7ab1fdf1084e7e4b40fb9708e7c3d92f5e1
duskdweller <duskdweller@localhost>
2013-03-01 13:16:14 +0100

    Adds a readme manager to the program. Fast access to the mod's readme file.

---
+++ XmlScript/UI/Controls/NodeEditor.cs
@@ -0,0 +1,20 @@
+﻿using Nexus.UI.Controls;
+using System.Windows.Forms;
+
+namespace Nexus.Client.ModManagement.Scripting.XmlScript.UI.Controls
+{
+	/// <summary>
+	/// The base class for editors of parts of an <see cref="XmlScript"/>.
+	/// </summary>
+	public class NodeEditor : UserControl
+	{
+		/// <summary>
+		/// Gets the view model of the editor.
+		/// </summary>
+		/// <returns>The view model of the editor.</returns>
+		public virtual IViewModel GetViewModel()
+		{
+			return null;
+		}
+	}
+}
