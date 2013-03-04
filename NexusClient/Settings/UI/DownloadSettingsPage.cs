60d6f983eb2c17f374013f73fcd517c1679c07b5
duskdweller <duskdweller@localhost>
2013-03-04 21:27:08 +0100

    Tweaked and fixed ReadMeManager implementation.

---
+++ NexusClient/Settings/UI/DownloadSettingsPage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using Nexus.Client.ModRepositories;
+using Nexus.Client.Util;
+
+
+namespace Nexus.Client.Settings.UI
+{
+	/// <summary>
+	/// A view allowing the editing of mod options.
+	/// </summary>
+	public partial class DownloadSettingsPage : UserControl, ISettingsGroupView
+	{
+		#region Constructors
+
+		/// <summary>
+		/// A sinmple consturctor that initializes teh object with the given values.
+		/// </summary>
+		/// <param name="p_dsgSettings">The settings group whose settings will be editable with this view.</param>
+		public DownloadSettingsPage(DownloadSettingsGroup p_dsgSettings)
+		{
+			SettingsGroup = p_dsgSettings;
+			InitializeComponent();
+
+			BindingSource bsFileServerZones = new BindingSource();
+			bsFileServerZones.DataSource = p_dsgSettings.FileServerZones;
+
+			cbxConnections.DataSource = p_dsgSettings.AllowedConnections;
+			cbxServerLocation.DataSource = bsFileServerZones.DataSource;
+			cbxServerLocation.DisplayMember = "FileServerName";
+			cbxServerLocation.ValueMember = "FileServerID";
+
+			BindingHelper.CreateFullBinding(cbxServerLocation, () => cbxServerLocation.SelectedValue, p_dsgSettings, () => p_dsgSettings.UserLocation);
+			BindingHelper.CreateFullBinding(cbxConnections, () => cbxConnections.SelectedItem, p_dsgSettings, () => p_dsgSettings.NumberOfConnections);
+			if (p_dsgSettings.PremiumEnabled)
+				BindingHelper.CreateFullBinding(ckbPremiumOnly, () => ckbPremiumOnly.Checked, p_dsgSettings, () => p_dsgSettings.PremiumOnly);
+			else
+			{
+				cbxConnections.Enabled = false;
+				ckbPremiumOnly.Enabled = false;
+			}
+		}
+
+		#endregion
+
+		#region ISettingsGroupView Members
+
+		/// <summary>
+		/// Gets the <see cref="SettingsGroup"/> whose settings will be editable with this view.
+		/// </summary>
+		/// <value>The <see cref="SettingsGroup"/> whose settings will be editable with this view.</value>
+		public SettingsGroup SettingsGroup { get; private set; }
+
+		#endregion
+	}
+}
---
+++ XmlScript/Parsers/Parser50.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace Nexus.Client.ModManagement.Scripting.XmlScript.Parsers
+{
+	/// <summary>
+	/// Parses version 5.0 xml script files.
+	/// </summary>
+	public class Parser50 : Parser40
+	{
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_xelScript">The xmlscript file.</param>
+		/// <param name="p_xstXmlScriptType">The <see cref="XmlScriptType"/> that describes
+		/// XML script type metadata.</param>
+		public Parser50(XElement p_xelScript, XmlScriptType p_xstXmlScriptType)
+			: base(p_xelScript, p_xstXmlScriptType)
+		{
+		}
+
+		#endregion
+
+		#region Parsing Methods
+
+		/// <summary>
+		/// Reads the condition from the given node.
+		/// </summary>
+		/// <param name="p_xelCondition">The node from which to load the condition.</param>
+		/// <returns>An <see cref="ICondition"/> representing the condition described in the given node.</returns>
+		protected override ICondition LoadCondition(XElement p_xelCondition)
+		{
+			if (p_xelCondition == null)
+				return null;
+			switch (p_xelCondition.GetSchemaInfo().SchemaType.Name)
+			{
+				case "compositeDependency":
+					ConditionOperator copOperator = (ConditionOperator)Enum.Parse(typeof(ConditionOperator), p_xelCondition.Attribute("operator").Value);
+					CompositeCondition cpdCondition = new CompositeCondition(copOperator);
+					IEnumerable<XElement> xeeConditions = p_xelCondition.Elements();
+					foreach (XElement xelCondition in xeeConditions)
+						cpdCondition.Conditions.Add(LoadCondition(xelCondition));
+					return cpdCondition;
+				case "fileDependency":
+					string strCondition = p_xelCondition.Attribute("file").Value.ToLower();
+					PluginState plsModState = (PluginState)Enum.Parse(typeof(PluginState), p_xelCondition.Attribute("state").Value);
+					return new PluginCondition(strCondition, plsModState);
+				case "flagDependency":
+					string strFlagName = p_xelCondition.Attribute("flag").Value;
+					string strValue = p_xelCondition.Attribute("value").Value;
+					return new FlagCondition(strFlagName, strValue);
+				case "versionDependency":
+					switch (p_xelCondition.Name.LocalName)
+					{
+						case "gameDependency":
+							Version verMinFalloutVersion = ParseVersion(p_xelCondition.Attribute("version").Value);
+							return new GameVersionCondition(verMinFalloutVersion);
+						case "fommDependency":
+							Version verMinFommVersion = ParseVersion(p_xelCondition.Attribute("version").Value);
+							return new ModManagerCondition(verMinFommVersion);
+						default:
+							throw new ParserException("Invalid plugin condition node: " + p_xelCondition.Name + ". At this point the config file has been validated against the schema, so there's something wrong with the parser.");
+					}
+				default:
+					throw new ParserException("Invalid plugin condition node: " + p_xelCondition.Name + ". At this point the config file has been validated against the schema, so there's something wrong with the parser.");
+			}
+		}
+
+		#endregion
+	}
+}
