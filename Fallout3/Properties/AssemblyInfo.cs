34ca681811823a6fc76c7e1ba5888ca69dad91ba
duskdweller <duskdweller@localhost>
2013-03-05 23:28:47 +0100

    The program won't ask the user to initialize the ReadMe Manager if there are no managed mods. Readme files will be ignored if the size is 0.

---
+++ Fallout3/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(Nexus.Client.ProgrammeMetadata.ModManagerName + " - Fallout 3 Module")]
+[assembly: AssemblyDescription("The Fallout 3 Module for the official Mod Manager the Nexus network of game mod websites.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Black Tree Gaming")]
+[assembly: AssemblyProduct("Fallout3")]
+[assembly: AssemblyCopyright("Copyright © Black Tree Gaming 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("12988191-ac6b-46dc-8803-f004b0e181b8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1")]
+// AssemblyFileVersionFormat("1.1.*")
+[assembly: AssemblyFileVersion("1.1.4812.22604")]
+[assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
