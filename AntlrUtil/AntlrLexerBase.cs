e8c565cc08ececc34aec2e4fa759f0e47cef6caa
duskdweller <duskdweller@localhost>
2013-03-13 11:39:53 +0100

    The program will now prevent readme files from being extracted (thus only installing them with the readme manager). The setting is on by default.

---
+++ AntlrUtil/AntlrLexerBase.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Antlr.Runtime;
+
+namespace Nexus.Client.Util.Antlr
+{
+	/// <summary>
+	/// The base lexer for ANTRL lexers.
+	/// </summary>
+	/// <remarks>
+	/// This extends the generated lexer in order to make error handling sane, and
+	/// to make the errors friendlier.
+	/// </remarks>
+	public abstract class AntlrLexerBase : Lexer
+	{
+		private ErrorTracker m_ertErrorTracker = null;
+
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the <see cref="ErrorTracker"/> that is being used
+		/// to keep track of any lexing errors.
+		/// </summary>
+		/// <value>The <see cref="ErrorTracker"/> that is being used
+		/// to keep track of any lexing errors.</value>
+		/// <exception cref="Exception">Thrown if the ErrorTracker has not been set.</exception>
+		public ErrorTracker ErrorTracker
+		{
+			get
+			{
+				if (m_ertErrorTracker == null)
+					m_ertErrorTracker = new ErrorTracker();
+				return m_ertErrorTracker;
+			}
+			set
+			{
+				m_ertErrorTracker = value;
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public AntlrLexerBase()
+		{
+		}
+
+		/// <summary>
+		/// A simple constructor.
+		/// </summary>
+		/// <param name="input">The code to lex.</param>
+		/// <param name="state">The <see cref="RecognizerSharedState"/> to use.</param>
+		public AntlrLexerBase(ICharStream input, RecognizerSharedState state)
+			: base(input, state)
+		{
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Logs the given error to the <see cref="ErrorTracker"/>.
+		/// </summary>
+		/// <param name="tokenNames">The names of the tokens in the current language.</param>
+		/// <param name="e">The error.</param>
+		public override void DisplayRecognitionError(String[] tokenNames, RecognitionException e)
+		{
+			LanguageError lerError = new LanguageError();
+			lerError.Line = e.Line - 1;
+			lerError.Column = e.CharPositionInLine;
+			lerError.Position = e.Index;
+			lerError.End = e.Index + 1;
+			lerError.Message = GetErrorMessage(e, tokenNames);
+			ErrorTracker.LexerErrors.Add(lerError);
+		}
+
+		/// <summary>
+		/// Reports the given error.
+		/// </summary>
+		/// <remarks>
+		/// This ensures that the error is thrown so that our <see cref="NextToken()"/>
+		/// override can handle it.
+		/// </remarks>
+		/// <param name="e">The error.</param>
+		public override void ReportError(RecognitionException e)
+		{
+			base.ReportError(e);
+			throw e;
+		}
+
+		/// <summary>
+		/// This reads the next token from the input.
+		/// </summary>
+		/// <remarks>
+		/// If there is a lexing error, this method inserts an error token
+		/// containing the bad input, and continues lexing.
+		/// </remarks>
+		/// <returns>The next token in the input.</returns>
+		public override IToken NextToken()
+		{
+			try
+			{
+				return base.NextToken();
+			}
+			catch (RecognitionException re)
+			{
+				if (re is NoViableAltException) { Recover(re); }
+				IToken tknErrorToken = new CommonToken(input, 0,
+												TokenChannels.Default,
+												state.tokenStartCharIndex,
+												input.Index - 1);
+				tknErrorToken.Line = state.tokenStartLine;
+				tknErrorToken.CharPositionInLine = state.tokenStartCharPositionInLine;
+				Emit(tknErrorToken);
+				return state.token;
+			}
+		}
+
+		/// <summary>
+		/// Generates a human-readable error message for the given error.
+		/// </summary>
+		/// <param name="e">The error.</param>
+		/// <param name="tokenNames">The names of the tokens in the current language.</param>
+		/// <returns>A human-readable error message for the given error.</returns>
+		public override string GetErrorMessage(RecognitionException e, string[] tokenNames)
+		{
+			string msg = null;
+			if (e is MismatchedTokenException)
+			{
+				MismatchedTokenException mte = (MismatchedTokenException)e;
+				msg = String.Format("Unexpected character: '{0}'. Expecting '{1}'.", GetCharErrorDisplay(e.Character), GetCharErrorDisplay(mte.Expecting));
+			}
+			else if (e is NoViableAltException)
+			{
+				NoViableAltException nvae = (NoViableAltException)e;
+				msg = String.Format("Unexpected character: '{0}'.", GetCharErrorDisplay(e.Character));
+			}
+			else if (e is EarlyExitException)
+			{
+				EarlyExitException eee = (EarlyExitException)e;
+				msg = String.Format("Unexpected character: '{0}'.", GetCharErrorDisplay(e.Character));
+				//msg = "required (...)+ loop did not match anything at character " + GetCharErrorDisplay(e.Character);
+			}
+			else if (e is MismatchedNotSetException)
+			{
+				MismatchedNotSetException mse = (MismatchedNotSetException)e;
+				msg = String.Format("Unexpected character: '{0}'. Expecting one of {1}.", GetCharErrorDisplay(e.Character), mse.Expecting);
+			}
+			else if (e is MismatchedSetException)
+			{
+				MismatchedSetException mse = (MismatchedSetException)e;
+				msg = String.Format("Unexpected character: '{0}'. Expecting one of {1}.", GetCharErrorDisplay(e.Character), mse.Expecting);
+			}
+			else if (e is MismatchedRangeException)
+			{
+				MismatchedRangeException mre = (MismatchedRangeException)e;
+				msg = String.Format("Unexpected character: '{0}'. Expecting one of '{1}..{2}'.", GetCharErrorDisplay(e.Character), GetCharErrorDisplay(mre.A), GetCharErrorDisplay(mre.B));
+			}
+			else
+			{
+				msg = base.GetErrorMessage(e, tokenNames);
+			}
+			return msg;
+		}
+	}
+}
---
+++ XmlScript/UI/Controls/NodeEditors/OptionInfoEditorVM.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Nexus.UI.Controls;
+using Nexus.Client.Util;
+using Nexus.Client.Util.Collections;
+
+namespace Nexus.Client.ModManagement.Scripting.XmlScript.UI.Controls.NodeEditors
+{
+	public class OptionInfoVM
+	{
+		protected Option Option { get; private set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string ImagePath { get; set; }
+
+		public OptionInfoVM(Option p_optOption)
+		{
+			Option = p_optOption;
+			Reset();
+		}
+
+		public Option Commit()
+		{
+			Option.Name = Name;
+			Option.Description = Description;
+			Option.ImagePath = ImagePath;
+			return Option;
+		}
+
+		public void Reset()
+		{
+			Name = Option.Name;
+			Description = Option.Description;
+			ImagePath = Option.ImagePath;
+		}
+
+		public void Reset(string p_strPropertyName)
+		{
+			if (p_strPropertyName.Equals(ObjectHelper.GetPropertyName(() => this.Name)))
+				Name = Option.Name;
+			else if (p_strPropertyName.Equals(ObjectHelper.GetPropertyName(() => this.Description)))
+				Description = Option.Description;
+			else if (p_strPropertyName.Equals(ObjectHelper.GetPropertyName(() => this.ImagePath)))
+				ImagePath = Option.ImagePath;
+		}
+	}
+
+	public class OptionInfoEditorVM:IViewModel
+	{
+		public event EventHandler OptionValidated = delegate { };
+
+		public IList<VirtualFileSystemItem> ModFiles { get; private set; }
+
+		public Option Option
+		{
+			set
+			{
+				OptionInfoVM = new OptionInfoVM(value);
+			}
+		}
+
+		public OptionInfoVM OptionInfoVM { get; private set; }
+		public ErrorContainer Errors { get; private set; }
+
+		public OptionInfoEditorVM(Option p_optOption, IList<VirtualFileSystemItem> p_lstModFiles)
+		{
+			Option = p_optOption;
+			ModFiles = p_lstModFiles;
+			Errors = new ErrorContainer();
+		}
+
+		protected void OnOptionValidated()
+		{
+			OptionValidated(this, new EventArgs());
+		}
+
+		public void SaveOptionInfo()
+		{
+			if (Validate())
+				OptionInfoVM.Commit();
+		}
+
+		/// <summary>
+		/// Ensures that the option name is valid.
+		/// </summary>
+		/// <returns><c>true</c> if the option name is valid;
+		/// <c>false</c> otherwise.</returns>
+		public bool ValidateOptionName()
+		{
+			bool booIsValid = true;
+			Errors.Clear<Option>(x => x.Name);
+			if (String.IsNullOrEmpty(OptionInfoVM.Name))
+			{
+				Errors.SetError<Option>(x => x.Name, "Name is required.");
+				booIsValid = false;
+			}
+			OnOptionValidated();
+			return booIsValid;
+		}
+
+		public Image GetImage(string p_strImagePath)
+		{
+			if (ModFiles == null)
+				return null;
+			VirtualFileSystemItem vfiItem = ModFiles.Find((f) => { return f.Path.Equals(p_strImagePath, StringComparison.OrdinalIgnoreCase); });
+			byte[] bteImage = null;
+			if (vfiItem != null)
+			{
+				if (Archive.IsArchivePath(vfiItem.Source))
+				{
+					KeyValuePair<string, string> kvpArchive = Archive.ParseArchivePath(vfiItem.Source);
+					using (Archive arcMod = new Archive(kvpArchive.Key))
+					{
+						bteImage = arcMod.GetFileContents(kvpArchive.Value);
+					}
+				}
+				else if (File.Exists(vfiItem.Source))
+				{
+					bteImage = File.ReadAllBytes(vfiItem.Source);
+				}
+			}
+			if (bteImage == null)
+				return null;
+			using (MemoryStream msmImage = new MemoryStream(bteImage))
+			{
+				try
+				{
+					Image imgTmp = Image.FromStream(msmImage);
+					return new Bitmap(imgTmp);
+				}
+				catch (ArgumentException)
+				{
+				}
+			}
+			return null;
+		}
+
+		#region IViewModel Members
+
+		public bool Validate()
+		{
+			return ValidateOptionName();
+		}
+
+		#endregion
+	}
+}
