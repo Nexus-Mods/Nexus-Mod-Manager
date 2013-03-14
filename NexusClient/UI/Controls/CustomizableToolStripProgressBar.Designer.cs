7cd15d581e1df4e06d14d6a87041bfc6be62328f
duskdweller <duskdweller@localhost>
2013-03-14 17:29:33 +0100

    Fixes the control failing to properly update and sort when renaming mods or categories.

---
+++ NexusClient/UI/Controls/CustomizableToolStripProgressBar.Designer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Nexus.Client.UI.Controls
+{
+	/// <summary>
+	/// A customizable progress bar.
+	/// </summary>
+	partial class CustomizableToolStripProgressBar
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+		}
+
+		#endregion
+	}
+}
