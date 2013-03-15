edd734a0186655415f9bdea96f4743fe457d7b3d
duskdweller <duskdweller@localhost>
2013-03-15 11:33:54 +0100

    Fixes the omod interpreter crashing when parsing comment lines.

---
+++ ModManager.Interface/ModManagement/BasicInstallTask.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Nexus.Client.BackgroundTasks;
+using Nexus.Client.Games;
+using Nexus.Client.Mods;
+using Nexus.Client.PluginManagement;
+
+namespace Nexus.Client.ModManagement
+{
+	/// <summary>
+	/// Performs a standard mod installation.
+	/// </summary>
+	/// <remarks>
+	/// A basic install installs all of the files in the mod to the installation directory,
+	/// and activates all plugin files.
+	/// </remarks>
+	public class BasicInstallTask : ThreadedBackgroundTask
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the mod being installed.
+		/// </summary>
+		/// <value>The mod being installed.</value>
+		protected IMod Mod { get; set; }
+
+		/// <summary>
+		/// Gets the current game mode.
+		/// </summary>
+		/// <value>The the current game mode.</value>
+		protected IGameMode GameMode { get; private set; }
+
+		/// <summary>
+		/// Gets or sets the installer to use to install files.
+		/// </summary>
+		/// <value>The installer to use to install files.</value>
+		protected IModFileInstaller FileInstaller { get; set; }
+
+		/// <summary>
+		/// Gets the manager to use to manage plugins.
+		/// </summary>
+		/// <value>The manager to use to manage plugins.</value>
+		protected IPluginManager PluginManager { get; private set; }
+
+		/// <summary>
+		/// Gets or sets whether the installer should skip readme files.
+		/// </summary>
+		/// <value>Whether the installer should skip readme files.</value>
+		protected bool SkipReadme { get; set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_modMod">The mod being installed.</param>
+		/// <param name="p_gmdGameMode">The the current game mode.</param>
+		/// <param name="p_mfiFileInstaller">The file installer to use.</param>
+		/// <param name="p_pmgPluginManager">The plugin manager.</param>
+		public BasicInstallTask(IMod p_modMod, IGameMode p_gmdGameMode, IModFileInstaller p_mfiFileInstaller, IPluginManager p_pmgPluginManager, bool p_booSkipReadme)
+		{
+			Mod = p_modMod;
+			GameMode = p_gmdGameMode;
+			FileInstaller = p_mfiFileInstaller;
+			PluginManager = p_pmgPluginManager;
+			SkipReadme = p_booSkipReadme;
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Runs the basic install task.
+		/// </summary>
+		/// <returns><c>true</c> if the installation succeed;
+		/// <c>false</c> otherwise.</returns>
+		public bool Execute()
+		{
+			OverallMessage = "Installing Mod...";
+			ShowItemProgress = false;
+			OverallProgressStepSize = 1;
+			return (bool)StartWait();
+		}
+
+		/// <summary>
+		/// The method that is called to start the backgound task.
+		/// </summary>
+		/// <remarks>
+		/// This method installs all of the files in the <see cref="IMod"/> being installed.
+		/// </remarks>
+		/// <param name="p_objArgs">Arguments to for the task execution.</param>
+		/// <returns>A return value.</returns>
+		protected override object DoWork(object[] p_objArgs)
+		{
+			char[] chrDirectorySeperators = new char[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar };
+			List<string> lstFiles = Mod.GetFileList();
+			OverallProgressMaximum = lstFiles.Count;
+			foreach (string strFile in lstFiles)
+			{
+				if (Status == TaskStatus.Cancelling)
+					return false;
+				string strFixedPath = GameMode.GetModFormatAdjustedPath(Mod.Format, strFile);
+				if (!(SkipReadme && Readme.IsValidExtension(Path.GetExtension(strFile).ToLower())))
+				{
+					if (FileInstaller.InstallFileFromMod(strFile, strFixedPath))
+						if (PluginManager.IsActivatiblePluginFile(strFixedPath))
+							PluginManager.ActivatePlugin(strFixedPath);
+				}
+				StepOverallProgress();
+			}
+			return true;
+		}
+	}
+}
---
+++ ModManager.Interface/Mods/FilenameModComparer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Nexus.Client.Mods
+{
+	/// <summary>
+	/// Compares <see cref="IMod"/>s based on their <see cref="IMod.Filename"/>s.
+	/// </summary>
+	/// <remarks>
+	/// In the constext of this comparer, <see cref="IMod"/>s are strictly ordered
+	/// by their ordinally case-insensitive <see cref="IMod.Filename"/>s.
+	/// </remarks>
+	public class FilenameModComparer : ModComparer
+	{
+		/// <summary>
+		/// Gets the hashcode to use for the given <see cref="IMod"/>.
+		/// </summary>
+		/// <param name="obj">The <see cref="IMod"/> whose hashcode is to be determined.</param>
+		/// <returns>The hascode to use for the given <see cref="IMod"/>.</returns>
+		public override int GetHashCode(IMod obj)
+		{
+			if ((obj == null) || (obj.Filename == null))
+				return 53;
+			return obj.Filename.GetHashCode();
+		}
+
+		/// <summary>
+		/// Compares the given <see cref="IMod"/>s.
+		/// </summary>
+		/// <param name="x">An object to compare to another object.</param>
+		/// <param name="y">An object to compare to another object.</param>
+		/// <returns>A value less than 0 if <paramref name="x"/> is less than <paramref name="y"/>.
+		/// 0 if this node is equal to the other.
+		/// A value greater than 0 if <paramref name="x"/> is greater than <paramref name="y"/>.</returns>
+		public override int Compare(IMod x, IMod y)
+		{
+			if (x == null)
+				return (y == null) ? 0 : -1;
+			if (y == null)
+				return 1;
+			return StringComparer.OrdinalIgnoreCase.Compare(x.Filename, y.Filename);
+		}
+	}
+}
