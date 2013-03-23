82a09b8e3a8acb457040e34b0c6d2b5032627179
duskdweller <duskdweller@localhost>
2013-03-23 12:01:45 +0100

    This will fix the exclusion list.

---
+++ ModScript/ModScriptType.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Nexus.UI.Controls;
+using Nexus.Client.Games;
+using Nexus.Client.Mods;
+
+namespace Nexus.Client.ModManagement.Scripting.ModScript
+{
+	/// <summary>
+	/// Describes the Mod Script script type.
+	/// </summary>
+	/// <remarks>
+	/// This is the script that allows scripting using the custom Mod Script language. It is meant
+	/// to be a siple scripting language.
+	/// </remarks>
+	public class ModScriptType : IScriptType
+	{
+		private static List<string> m_lstFileNames = new List<string>() { "script", "script.txt" };
+
+		#region IScriptType Members
+
+		/// <summary>
+		/// Gets the name of the script type.
+		/// </summary>
+		/// <value>The name of the script type.</value>
+		public string TypeName
+		{
+			get
+			{
+				return "Mod Script";
+			}
+		}
+
+		/// <summary>
+		/// Gets the unique id of the script type.
+		/// </summary>
+		/// <value>The unique id of the script type.</value>
+		public string TypeId
+		{
+			get
+			{
+				return "ModScript";
+			}
+		}
+
+		/// <summary>
+		/// Gets the list of file names used by scripts of the current type.
+		/// </summary>
+		/// <remarks>
+		/// The list is in order of preference, with the first item being the preferred
+		/// file name.
+		/// </remarks>
+		/// <value>The list of file names used by scripts of the current type.</value>
+		public IList<string> FileNames
+		{
+			get
+			{
+				return m_lstFileNames;
+			}
+		}
+
+		/// <summary>
+		/// Creates an editor for the script type.
+		/// </summary>
+		/// <param name="p_lstModFiles">The list of files if the current mod.</param>
+		/// <returns>An editor for the script type.</returns>
+		public IScriptEditor CreateEditor(IList<VirtualFileSystemItem> p_lstModFiles)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Creates an executor that can run the script type.
+		/// </summary>
+		/// <param name="p_modMod">The mod being installed.</param>
+		/// <param name="p_gmdGameMode">The game mode currently bieng managed.</param>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		/// <param name="p_igpInstallers">The utility class to use to install the mod items.</param>
+		/// <param name="p_scxUIContext">The <see cref="SynchronizationContext"/> to use to marshall UI interactions to the UI thread.</param>
+		/// <returns>An executor that can run the script type.</returns>
+		public IScriptExecutor CreateExecutor(IMod p_modMod, IGameMode p_gmdGameMode, IEnvironmentInfo p_eifEnvironmentInfo, InstallerGroup p_igpInstallers, SynchronizationContext p_scxUIContext)
+		{
+			ModScriptFunctionProxy msfFunctions = GetScriptFunctionProxy(p_modMod, p_gmdGameMode, p_eifEnvironmentInfo, p_igpInstallers, p_scxUIContext);
+			return new ModScriptExecutor(p_gmdGameMode, p_eifEnvironmentInfo, msfFunctions);
+		}
+
+		/// <summary>
+		/// Loads the script from the given text representation.
+		/// </summary>
+		/// <param name="p_strScriptData">The text to convert into a script.</param>
+		/// <returns>The <see cref="IScript"/> represented by the given data.</returns>
+		public IScript LoadScript(string p_strScriptData)
+		{
+			return new ModScript(this, p_strScriptData);
+		}
+
+		/// <summary>
+		/// Saves the given script into a text representation.
+		/// </summary>
+		/// <param name="p_scpScript">The <see cref="IScript"/> to save.</param>
+		/// <returns>The text represnetation of the given <see cref="IScript"/>.</returns>
+		public string SaveScript(IScript p_scpScript)
+		{
+			return ((ModScript)p_scpScript).Code;
+		}
+
+		/// <summary>
+		/// Determines if the given script is valid.
+		/// </summary>
+		/// <param name="p_scpScript">The script to validate.</param>
+		/// <returns><c>true</c> if the given script is valid;
+		/// <c>false</c> otherwise.</returns>
+		public bool ValidateScript(IScript p_scpScript)
+		{
+			ModScriptInterpreter msiScriptCompile = new ModScriptInterpreter(((ModScript)p_scpScript).Code);
+			return msiScriptCompile.Compile();
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Returns a proxy that implements the functions available to Mod Script scripts.
+		/// </summary>
+		/// <param name="p_modMod">The mod being installed.</param>
+		/// <param name="p_gmdGameMode">The game mode currently bieng managed.</param>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		/// <param name="p_igpInstallers">The utility class to use to install the mod items.</param>
+		/// <param name="p_scxUIContext">The <see cref="SynchronizationContext"/> to use to marshall UI interactions to the UI thread.</param>
+		/// <returns>A proxy that implements the functions available to Mod Script scripts.</returns>
+		protected virtual ModScriptFunctionProxy GetScriptFunctionProxy(IMod p_modMod, IGameMode p_gmdGameMode, IEnvironmentInfo p_eifEnvironmentInfo, InstallerGroup p_igpInstallers, SynchronizationContext p_scxUIContext)
+		{
+			return new ModScriptFunctionProxy(p_modMod, p_gmdGameMode, p_eifEnvironmentInfo, p_igpInstallers, new ModScriptUIUtil(p_gmdGameMode, p_eifEnvironmentInfo, p_scxUIContext));
+		}
+	}
+}
---
+++ NexusClient/HeaderlessTextWriterTraceListener.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Nexus.Client.Util;
+
+namespace Nexus.Client
+{
+	/// <summary>
+	/// A trace listener that doesn't print the header info.
+	/// </summary>
+	public class HeaderlessTextWriterTraceListener : TextWriterTraceListener
+	{
+		private bool m_booSaveToFile = false;
+		private Int64 m_intLastStreamPosition = 0;
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the path of the file to which the listener writes.
+		/// </summary>
+		/// <value>The path of the file to which the listener writes.</value>
+		public string FilePath { get; private set; }
+
+		/// <summary>
+		/// Gets the stream to which the tracec info has been written.
+		/// </summary>
+		/// <value>The stream to which the tracec info has been written.</value>
+		protected MemoryStream TraceStream { get; private set; }
+
+		/// <summary>
+		/// Gets whether the trace is being forced.
+		/// </summary>
+		/// <remarks>
+		/// If it's not being forced, it should only be written to file in the case of a crash.
+		/// </remarks>
+		/// <value>Whether the trace is being forced.</value>
+		public bool TraceIsForced
+		{
+			get
+			{
+				return TraceStream == null;
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the obejct with the given values.
+		/// </summary>
+		/// <param name="p_strTraceFilePath">The path of the file to which to write trace info.</param>
+		public HeaderlessTextWriterTraceListener(string p_strTraceFilePath)
+			: base(p_strTraceFilePath)
+		{
+			FilePath = p_strTraceFilePath;
+		}
+
+		/// <summary>
+		/// A simple constructor that initializes the obejct with the given values.
+		/// </summary>
+		/// <remarks>
+		/// When this constuctor is used, the trace data will only be written to the given path
+		/// when <see cref="SaveToFile()"/> is called.
+		/// </remarks>
+		/// <param name="p_msmTraceInfo">The stream to which to send trace info.</param>
+		/// <param name="p_strTraceFilePath">The path of the file to which to write trace info.</param>
+		public HeaderlessTextWriterTraceListener(MemoryStream p_msmTraceInfo, string p_strTraceFilePath)
+			: base(p_msmTraceInfo)
+		{
+			TraceStream = p_msmTraceInfo;
+			FilePath = p_strTraceFilePath;
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Writes the given messsage, if it isn't the header info.
+		/// </summary>
+		/// <param name="message">The message to write.</param>
+		public override void Write(string message)
+		{
+			if (message.StartsWith(Path.GetFileName(Environment.GetCommandLineArgs()[0])))
+				return;
+			base.Write(message);
+		}
+
+		/// <summary>
+		/// Saves the trace info to the file.
+		/// </summary>
+		public void SaveToFile()
+		{
+			if (TraceIsForced)
+				throw new InvalidOperationException("This mehtod can olny be called if the HeaderlessTextWriterTraceListener(MemoryStream, string) constructor was used.");
+			m_booSaveToFile = true;
+			Flush();
+		}
+
+		/// <summary>
+		/// Changes the file where the trace is saved.
+		/// </summary>
+		/// <param name="p_strNewFileName">The new filename.</param>
+		public void ChangeFilePath(string p_strNewFileName)
+		{
+			if (TraceIsForced || m_booSaveToFile)
+				throw new InvalidOperationException("The trace has already been written. Cannot change file path.");
+			FilePath = p_strNewFileName;
+		}
+
+		/// <summary>
+		/// Writes all remaining data to the underlying listener.
+		/// </summary>
+		public override void Flush()
+		{
+			base.Flush();
+			if (!TraceIsForced && m_booSaveToFile)
+			{
+				Int64 intPosition = TraceStream.Position;
+				byte[] bteData = new byte[intPosition - m_intLastStreamPosition];
+				Array.Copy(TraceStream.GetBuffer(), m_intLastStreamPosition, bteData, 0, bteData.Length);
+				File.AppendAllText(FilePath, TextUtil.ByteToString(bteData));
+				m_intLastStreamPosition = intPosition;
+			}
+		}
+	}
+}
