5d674a26fc2eaf1c8f44e2f0817d0e348c802f25
duskdweller <duskdweller@localhost>
2013-04-05 01:59:33 +0200

    This prevents the update check messagebox from blocking the execution when the program performs the auto check.

---
+++ UI/Controls/IViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Nexus.UI.Controls
+{
+	/// <summary>
+	/// This marks a class as a view model.
+	/// </summary>
+	public interface IViewModel
+	{
+		/// <summary>
+		/// Validates the view model.
+		/// </summary>
+		/// <returns><c>true</c> if the model is valid;
+		/// <c>false</c> otherwise.</returns>
+		bool Validate();
+	}
+}
