8e413902f9e0cb8e0c8cc6e84ceb4118d628ddce
duskdweller <duskdweller@localhost>
2013-04-12 13:35:03 +0200

    Closing branch in preparation for merge.

--- GamebryoBase/Properties/AssemblyInfo.cs
+++ GamebryoBase/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4818.22129")]
+[assembly: AssemblyFileVersion("1.1.4850.20514")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
