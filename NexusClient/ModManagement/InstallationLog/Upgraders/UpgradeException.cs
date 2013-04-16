3efa7998f46d0a569491da13285b897be481ebb7
duskdweller <duskdweller@localhost>
2013-04-16 17:26:16 +0200

    This will fix a memory leak in the ModInstaller.

---
+++ NexusClient/ModManagement/InstallationLog/Upgraders/UpgradeException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Nexus.Client.ModManagement.InstallationLog.Upgraders
+{
+	/// <summary>
+	/// The exception that is thrown if an error occurs during the upgrading of an install log.
+	/// </summary>
+	public class UpgradeException : Exception
+	{
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public UpgradeException()
+		{
+		}
+
+		/// <summary>
+		/// A simple contructor that sets the exception's message.
+		/// </summary>
+		/// <param name="message">The exception's message.</param>
+		public UpgradeException(string message)
+			: base(message)
+		{
+		}
+
+		/// <summary>
+		/// A simple constructor the sets the exception's message and inner exception.
+		/// </summary>
+		/// <param name="message">The exception's message.</param>
+		/// <param name="inner">The ineer exception.</param>
+		public UpgradeException(string message, Exception inner)
+			: base(message, inner)
+		{
+		}
+	}
+}
---
+++ NexusClient/ModRepositories/FileServerZone.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+
+namespace Nexus.Client.ModRepositories
+{
+	/// <summary>
+	/// A repository's fileserver info.
+	/// </summary>
+	public class FileServerZone
+	{
+		private string m_strFileServerName = String.Empty;
+		private string m_strFileServerID = String.Empty;
+		private Image m_imgFileServerFlag = null;
+		private Int32 m_intAffinityID = 0;
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the name of the fileserver.
+		/// </summary>
+		/// <value>The name of the fileserver.</value>
+		public string FileServerName
+		{
+			get
+			{
+				return m_strFileServerName;
+			}
+			private set
+			{
+				m_strFileServerName = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets the ID of the fileserver.
+		/// </summary>
+		/// <value>The ID of the fileserver.</value>
+		public string FileServerID
+		{
+			get
+			{
+				return m_strFileServerID;
+			}
+			private set
+			{
+				m_strFileServerID = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets the image of the fileserver.
+		/// </summary>
+		/// <value>The image of the fileserver.</value>
+		public Image FileServerFlag
+		{
+			get
+			{
+				return m_imgFileServerFlag;
+			}
+			private set
+			{
+				m_imgFileServerFlag = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets the affinity of the fileserver, affinity is used to choose the nearest alternative.
+		/// </summary>
+		/// <value>The affinity of the fileserver, affinity is used to choose the nearest alternative.</value>
+		public Int32 FileServerAffinity
+		{
+			get
+			{
+				return m_intAffinityID;
+			}
+			private set
+			{
+				m_intAffinityID = value;
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// The default constructor the initializes the object.
+		/// </summary>
+		public FileServerZone()
+		{
+			m_strFileServerID = "default";
+			m_strFileServerName = "Default";
+			m_intAffinityID = 0;
+		}
+
+		/// <summary>
+		/// A simple constructor the initializes the object with the given parameters.
+		/// </summary>
+		/// <param name="p_strFileServerID">The fileserver ID.</param>
+		/// <param name="p_strFileServerName">The fileserver name.</param>
+		/// <param name="p_intAffinityID">The fileserver affinity.</param>
+		public FileServerZone(string p_strFileServerID, string p_strFileServerName, Int32 p_intAffinityID)
+		{
+			m_strFileServerID = p_strFileServerID;
+			m_strFileServerName = p_strFileServerName;
+			m_intAffinityID = p_intAffinityID;
+		}
+
+		/// <summary>
+		/// A simple constructor the initializes the object with the given parameters.
+		/// </summary>
+		/// <param name="p_strFileServerID">The fileserver ID.</param>
+		/// <param name="p_strFileServerName">The fileserver name.</param>
+		/// <param name="p_intAffinityID">The fileserver affinity.</param>
+		/// <param name="p_imgFileServerFlag">The fileserver image.</param>
+		public FileServerZone(string p_strFileServerID, string p_strFileServerName, Int32 p_intAffinityID, Image p_imgFileServerFlag)
+		{
+			m_strFileServerID = p_strFileServerID;
+			m_strFileServerName = p_strFileServerName;
+			m_intAffinityID = p_intAffinityID;
+			m_imgFileServerFlag = p_imgFileServerFlag;
+		}
+
+		#endregion
+	}
+}
