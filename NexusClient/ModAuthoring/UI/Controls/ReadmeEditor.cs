cc16fe40055b37c3cafdb1876a6bfdbbb24d8532
duskdweller <duskdweller@localhost>
2013-04-23 18:51:31 +0200

    The user will now be able to set the Temp folder.

---
+++ NexusClient/ModAuthoring/UI/Controls/ReadmeEditor.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using Nexus.Client.Mods;
+
+namespace Nexus.Client.ModAuthoring.UI.Controls
+{
+	/// <summary>
+	/// An editor for <see cref="Readme"/> files.
+	/// </summary>
+	public partial class ReadmeEditor : UserControl
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the <see cref="Readme"/> being edited.
+		/// </summary>
+		/// <value>The <see cref="Readme"/> being edited.</value>
+		[Bindable(true)]
+		public Readme Readme
+		{
+			get
+			{
+				Readme rmeReadme = new Readme(ReadmeFormat.PlainText, null);
+				if (ddtReadme.SelectedTabPage == ddpPlainText)
+				{
+					rmeReadme.Format = ReadmeFormat.PlainText;
+					rmeReadme.Text = tbxReadme.Text;
+				}
+				else if (ddtReadme.SelectedTabPage == ddpRichText)
+				{
+					rmeReadme.Format = ReadmeFormat.RichText;
+					rmeReadme.Text = rteReadme.Rtf;
+				}
+				else if (ddtReadme.SelectedTabPage == ddpHTML)
+				{
+					rmeReadme.Format = ReadmeFormat.HTML;
+					rmeReadme.Text = xedReadme.Text;
+				}
+				return String.IsNullOrEmpty(rmeReadme.Text) ? null : rmeReadme;
+			}
+			set
+			{
+				if (value == null)
+				{
+					ddtReadme.SelectedTabPage = ddpPlainText;
+					tbxReadme.Text = null;
+				}
+				else
+				{
+					switch (value.Format)
+					{
+						case ReadmeFormat.PlainText:
+							ddtReadme.SelectedTabPage = ddpPlainText;
+							tbxReadme.Font = new Font(FontFamily.GenericMonospace, tbxReadme.Font.Size, tbxReadme.Font.Style);
+							tbxReadme.Text = value.Text;
+							break;
+						case ReadmeFormat.RichText:
+							ddtReadme.SelectedTabPage = ddpRichText;
+							try
+							{
+								rteReadme.Rtf = value.Text;
+							}
+							catch
+							{
+								rteReadme.Text = value.Text;
+							}
+							break;
+						case ReadmeFormat.HTML:
+							ddtReadme.SelectedTabPage = ddpHTML;
+							xedReadme.Text = value.Text;
+							break;
+						default:
+							throw new InvalidEnumArgumentException("Unrecognized value for ReadmeFormat enum.");
+					}
+				}
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public ReadmeEditor()
+		{
+			InitializeComponent();
+
+			xedReadme.SetHighlighting("HTML");
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Shows a preview of the HTML readme.
+		/// </summary>
+		protected void ShowHTMLPreview()
+		{
+			Form frmHTMLPreview = new Form();
+			WebBrowser wbrBrowser = new WebBrowser();
+			frmHTMLPreview.Controls.Add(wbrBrowser);
+			wbrBrowser.Dock = DockStyle.Fill;
+			wbrBrowser.DocumentCompleted += delegate(object o, WebBrowserDocumentCompletedEventArgs arg)
+			{
+				frmHTMLPreview.Text = String.IsNullOrEmpty(wbrBrowser.DocumentTitle) ? "Readme" : wbrBrowser.DocumentTitle;
+			};
+			wbrBrowser.WebBrowserShortcutsEnabled = false;
+			wbrBrowser.AllowWebBrowserDrop = false;
+			wbrBrowser.AllowNavigation = false;
+			wbrBrowser.DocumentText = xedReadme.Text;
+			frmHTMLPreview.ShowDialog(this.FindForm());
+		}
+
+		/// <summary>
+		/// Hanldes the <see cref="Control.Click"/> event of the PReview menu item.
+		/// </summary>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void tsbPreview_Click(object sender, EventArgs e)
+		{
+			ShowHTMLPreview();
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.KeyPress"/> event of the plain text readme textbox.
+		/// </summary>
+		/// <remarks>
+		/// This selects all text when Ctrl-A is pressed.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="KeyPressEventArgs"/> describing the event arguments.</param>
+		private void tbxReadme_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			//character 1 is equivalent to Ctrl-A
+			if (e.KeyChar == '\x01')
+			{
+				((TextBox)sender).SelectAll();
+				e.Handled = true;
+			}
+		}
+	}
+}
---
+++ NexusClient/ModAuthoring/UI/ModPackagingForm.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Nexus.Client.Commands;
+using Nexus.Client.Commands.Generic;
+using Nexus.UI.Controls;
+using Nexus.Client.Util;
+using Nexus.Client.BackgroundTasks.UI;
+using Nexus.Client.BackgroundTasks;
+using System.Collections.Generic;
+
+namespace Nexus.Client.ModAuthoring.UI
+{
+	/// <summary>
+	/// Encapsulates the packaging of a mod.
+	/// </summary>
+	public partial class ModPackagingForm : Form
+	{
+		private string m_strWindowTitle = null;
+		private ModPackagingFormVM m_vmlViewModel = null;
+
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the view model that provides the data and operations for this view.
+		/// </summary>
+		/// <value>The view model that provides the data and operations for this view.</value>
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		protected ModPackagingFormVM ViewModel
+		{
+			get
+			{
+				return m_vmlViewModel;
+			}
+			set
+			{
+				m_vmlViewModel = value;
+				sedScriptEditor.ViewModel = value.ScriptEditorVM;
+				mieModInfo.ViewModel = value.InfoEditorVM;
+				value.PropertyChanged += new PropertyChangedEventHandler(ViewModel_PropertyChanged);
+				value.ModPackingStarted += new EventHandler<EventArgs<Nexus.Client.BackgroundTasks.IBackgroundTask>>(ViewModel_ModPackingStarted);
+				value.ProjectValidated += new EventHandler(ViewModel_ProjectValidated);
+				BindProject(value.ModProject);
+
+				tsbMakeMod.Click += new EventHandler(ToolStripButton_Click);
+
+				new ToolStripItemCommandBinding<string>(tsbSave, value.SaveCommand, () => null);
+				new ToolStripItemCommandBinding(tsbNew, value.NewCommand);
+				new ToolStripItemCommandBinding(tsbOpen, value.OpenCommand);
+				new ToolStripItemCommandBinding<string>(tsbMakeMod, value.BuildCommand, () => null);
+				value.GetOpenPath = GetOpenPath;
+				value.GetProjectSavePath = GetProjectSavePath;
+				value.GetNewModSavePath = GetNewModSavePath;
+				value.ConfirmSaveCurrentProject = ConfirmSaveCurrentProject;
+				value.GetIgnoreWarnings = GetIgnoreWarnings;
+			}
+		}
+
+		#endregion
+
+		#region Constructor
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public ModPackagingForm(ModPackagingFormVM p_vmlModPackagingVM)
+		{
+			InitializeComponent();
+			m_strWindowTitle = this.Text;
+			this.DoubleBuffered = true;
+
+			ViewModel = p_vmlModPackagingVM;
+		}
+
+		#endregion
+
+		#region View Model Callback Delegates
+
+		/// <summary>
+		/// Gets whether the current <see cref="Project"/> should be saved.
+		/// </summary>
+		/// <returns><c>true</c> if the user wants to save the current <see cref="Project"/>, or
+		/// <c>false</c> if the user wants to discard any changes to the current <see cref="Project"/>, or
+		/// <c>null</c> if the user wants to keep the current <see cref="Project"/> as the <see cref="Project"/>
+		/// being edited.</returns>
+		private bool? ConfirmSaveCurrentProject()
+		{
+			if (ViewModel.ModProject.IsDirty)
+			{
+				switch (MessageBox.Show(this, "Would you like to save the current project?", "Save Changes", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question))
+				{
+					case DialogResult.Cancel:
+						return null;
+					case DialogResult.Yes:
+						return true;
+				}
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// Gets the path where the project should be saved.
+		/// </summary>
+		/// <returns>The path where the project should be saved.</returns>
+		protected string GetProjectSavePath()
+		{
+			if (sfdProject.ShowDialog(this) == DialogResult.OK)
+				return sfdProject.FileName;
+			return null;
+		}
+
+		/// <summary>
+		/// Gets the path where the new mod should be saved.
+		/// </summary>
+		/// <returns>The path where the new mod should be saved.</returns>
+		protected string GetNewModSavePath()
+		{
+			if (sfdNewMod.ShowDialog(this) == DialogResult.OK)
+				return sfdNewMod.FileName;
+			return null;
+		}
+
+		/// <summary>
+		/// Gets the path of the project to open.
+		/// </summary>
+		/// <returns>The path of the project to open.</returns>
+		protected string GetOpenPath()
+		{
+			if (ofdProject.ShowDialog(this) == DialogResult.OK)
+				return ofdProject.FileName;
+			return null;
+		}
+
+		/// <summary>
+		/// Determines if the validation warnings should be ignored.
+		/// </summary>
+		/// <returns><c>true</c> if the validation warnings should be ignored;
+		/// <c>false</c> otherwise.</returns>
+		protected bool GetIgnoreWarnings()
+		{
+			return (MessageBox.Show(this, "There are warnings." + Environment.NewLine + "Warnings can be ignored, but they can indicate missing information that you meant to enter." + Environment.NewLine + "Would you like to continue?", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK);
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Binds the <see cref="Project"/> to the control.
+		/// </summary>
+		/// <param name="p_prjProject">The <see cref="Project"/> to bind to the control.</param>
+		protected void BindProject(Project p_prjProject)
+		{
+			p_prjProject.PropertyChanged -= Project_PropertyChanged;
+			p_prjProject.PropertyChanged += new PropertyChangedEventHandler(Project_PropertyChanged);
+			SetWindowTitle(p_prjProject);
+			redReadme.DataBindings.Clear();
+			ftvModFilesEditor.DataBindings.Clear();
+			BindingHelper.CreateFullBinding(redReadme, () => redReadme.Readme, p_prjProject, () => p_prjProject.ModReadme);
+			BindingHelper.CreateFullBinding(ftvModFilesEditor, () => ftvModFilesEditor.Sources, p_prjProject, () => p_prjProject.ModFiles).DataSourceUpdateMode = DataSourceUpdateMode.OnPropertyChanged;
+		}
+
+		/// <summary>
+		/// Sets the window's title to reflect project name and dirty status.
+		/// </summary>
+		/// <param name="p_prjProject">The <see cref="Project"/> being edited.</param>
+		protected void SetWindowTitle(Project p_prjProject)
+		{
+			string strTitle = String.Format("{0}{1}", p_prjProject.ModName, p_prjProject.IsDirty ? "*" : "");
+			this.Text = String.Format("{0}{1}{2}", m_strWindowTitle, String.IsNullOrEmpty(strTitle) ? "" : " - ", strTitle);
+		}
+
+		#region Property Change Handlers
+
+		/// <summary>
+		/// Handles the <see cref="INotifyPropertyChanged.PropertyChanged"/> event of the view model.
+		/// </summary>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">A <see cref="PropertyChangedEventArgs"/> describing the event arguments.</param>
+		private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName.Equals(ObjectHelper.GetPropertyName(() => ViewModel.ModProject)))
+				BindProject(ViewModel.ModProject);
+		}
+
+		/// <summary>
+		/// Handles the <see cref="INotifyPropertyChanged.PropertyChanged"/> event of the <see cref="Project"/>
+		/// being edited.
+		/// </summary>
+		/// <remarks>
+		/// This method alters the form display to indicate whether the <see cref="Project"/> is dirty.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">A <see cref="PropertyChangedEventArgs"/> describing the event arguments.</param>
+		private void Project_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			SetWindowTitle((Project)sender);
+		}
+
+		#endregion
+
+
+		/// <summary>
+		/// Handles the <see cref="ModPackagingFormVM.ProjectValidated"/> event of the view model.
+		/// </summary>
+		/// <remarks>
+		/// This displays warning and error icons.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void ViewModel_ProjectValidated(object sender, EventArgs e)
+		{
+			Dictionary<string, VerticalTabPage> dicProperties = new Dictionary<string, VerticalTabPage>()
+																	{	{ ObjectHelper.GetPropertyName<Project>(x => x.ModFiles), vtpModFiles },
+																		{ ObjectHelper.GetPropertyName<Project>(x => x.InstallScript), vtpScript},
+																		{ ObjectHelper.GetPropertyName<Project>(x => x.ModName), vtpModInfo},
+																		{ ObjectHelper.GetPropertyName<Project>(x => x.ModReadme), vtpReadme}
+																	};
+			foreach (string strProperty in dicProperties.Keys)
+			{
+				if (String.IsNullOrEmpty(ViewModel.Errors[strProperty]))
+					sspWarnings.SetStatus(dicProperties[strProperty], ViewModel.Warnings[strProperty]);
+				else
+					sspErrors.SetStatus(dicProperties[strProperty], ViewModel.Errors[strProperty]);
+			}
+		}
+
+		/// <summary>
+		/// Handles the <see cref="ModPackagingFormVM.ModPackingStarted"/> event of the view model.
+		/// </summary>
+		/// <remarks>
+		/// This displays the progress dialog.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs{IBackgroundTask}"/> describing the event arguments.</param>
+		private void ViewModel_ModPackingStarted(object sender, EventArgs<IBackgroundTask> e)
+		{
+			if (InvokeRequired)
+			{
+				Invoke((Action<object, EventArgs<IBackgroundTask>>)ViewModel_ModPackingStarted, sender, e);
+				return;
+			}
+			ProgressDialog.ShowDialog(this, e.Argument);
+		}
+
+		/// <summary>
+		/// Handles the <see cref="VerticalTabControl.SelectedTabPageChanged"/> event of the navigation
+		/// tab control.
+		/// </summary>
+		/// <remarks>
+		/// This makes sure each page has the information it requires from the other pages.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">A <see cref="VerticalTabControl.TabPageEventArgs"/> describing the event arguments.</param>
+		private void verticalTabControl1_SelectedTabPageChanged(object sender, VerticalTabControl.TabPageEventArgs e)
+		{
+			if (e.TabPage == vtpScript)
+				ViewModel.ScriptEditorVM.ModFiles = ftvModFilesEditor.Sources;
+		}
+
+		/// <summary>
+		/// Handles the <see cref="ToolStripItem.Click"/> event of the build button.
+		/// </summary>
+		/// <remarks>
+		/// This makes sure all control values have been committed, so that they can be validated.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void ToolStripButton_Click(object sender, EventArgs e)
+		{
+			this.ValidateChildren();
+		}
+	}
+}
