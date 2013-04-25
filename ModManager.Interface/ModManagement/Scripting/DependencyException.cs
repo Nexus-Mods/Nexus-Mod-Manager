6aa97b5de079c9e8950f7247601e8dcf8be70429
duskdweller <duskdweller@localhost>
2013-04-25 17:06:08 +0200

    This will fix the multi uninstaller progress bar completing before the actual uninstall process was complete.

---
+++ ModManager.Interface/ModManagement/Scripting/DependencyException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Nexus.Client.ModManagement.Scripting
+{
+	/// <summary>
+	/// An exception class that represents a dependency that was not fufilled.
+	/// </summary>
+	public class DependencyException : ApplicationException
+	{
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public DependencyException()
+		{
+		}
+
+		/// <summary>
+		/// A simple contructor that sets the exception's message.
+		/// </summary>
+		/// <param name="message">The exception's message.</param>
+		public DependencyException(string message)
+			: base(message)
+		{
+		}
+
+		/// <summary>
+		/// A simple constructor the sets the exception's message and inner exception.
+		/// </summary>
+		/// <param name="message">The exception's message.</param>
+		/// <param name="inner">The ineer exception.</param>
+		public DependencyException(string message, Exception inner)
+			: base(message, inner)
+		{
+		}
+	}
+}
