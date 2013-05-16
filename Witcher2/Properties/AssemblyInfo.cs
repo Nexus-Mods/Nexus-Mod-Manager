4a792253c75ab2a805c087c764ae12ab27372699
duskdweller <duskdweller@localhost>
2013-05-16 13:57:22 +0200

    Closing branch in preparation for merge.

--- Witcher2/Properties/AssemblyInfo.cs
+++ Witcher2/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4882.28465")]
+[assembly: AssemblyFileVersion("1.1.4884.21340")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
