83f83b4d2e07ab0ca66f333479a223965b31d385
duskdweller <duskdweller@localhost>
2013-06-11 17:41:43 +0200

    This will fix the BossSorter crashing while trying to remove a non existant plugin.

---
+++ Commanding/CommandBinding.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using Nexus.Client.Commands.Generic;
+
+namespace Nexus.Client.Commands
+{
+	/// <summary>
+	/// The base class for binding a command to a command trigger.
+	/// </summary>
+	public abstract class CommandBinding : CommandBindingBase
+	{
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_objTrigger">The object that can trigger the command.</param>
+		/// <param name="p_cmdCommand">The command that can be triggered.</param>
+		protected CommandBinding(object p_objTrigger, Command p_cmdCommand)
+			: base(p_objTrigger, p_cmdCommand)
+		{
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Executes the command.
+		/// </summary>
+		public override void Execute()
+		{
+			((Command)Command).Execute();
+		}
+	}
+}
