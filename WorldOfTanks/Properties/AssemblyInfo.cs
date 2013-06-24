06e766992282486d99ed7f2347082d8cc8f1aaae
duskdweller <duskdweller@localhost>
2013-06-24 14:22:27 +0200

    Closing branch in preparation for merge.

--- WorldOfTanks/Properties/AssemblyInfo.cs
+++ WorldOfTanks/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4919.20590")]
+[assembly: AssemblyFileVersion("1.1.4923.21577")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
