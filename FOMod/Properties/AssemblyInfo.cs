ef1392a0fb85f12b6467cf959284106df1a66f4f
duskdweller <duskdweller@localhost>
2013-07-10 10:43:49 +0200

    Closing branch in preparation for merge.

--- FOMod/Properties/AssemblyInfo.cs
+++ FOMod/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4937.24221")]
+[assembly: AssemblyFileVersion("1.1.4939.15567")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
