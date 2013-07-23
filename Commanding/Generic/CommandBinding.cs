0a2880fdbc980d1e061e39550fce8ff85ad725c0
duskdweller <duskdweller@localhost>
2013-07-23 14:54:27 +0200

    This will fix a crash in the uninstall task.

---
+++ Commanding/Generic/CommandBinding.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using Nexus.Client.Commands.Generic;
+
+namespace Nexus.Client.Commands.Generic
+{
+	/// <summary>
+	/// The base class for binding a command to a command trigger.
+	/// </summary>
+	/// <typeparam name="T">The type of the command argument.</typeparam>
+	public abstract class CommandBinding<T> : CommandBindingBase
+	{
+		/// <summary>
+		/// The delegate for the method that returns the command argument.
+		/// </summary>
+		/// <returns>The command argument.</returns>
+		public delegate T GetCommandArgument();
+
+		private GetCommandArgument m_dlgGetArgument = null;
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_objTrigger">The object that can trigger the command.</param>
+		/// <param name="p_cmdCommand">The command that can be triggered.</param>
+		/// <param name="p_dlgGetArgument">The method that returns the command argument.</param>
+		protected CommandBinding(object p_objTrigger, Command<T> p_cmdCommand, GetCommandArgument p_dlgGetArgument)
+			:base(p_objTrigger,p_cmdCommand)
+		{
+			m_dlgGetArgument = p_dlgGetArgument;
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Executes the command.
+		/// </summary>
+		public override void Execute()
+		{
+			((Command<T>)Command).Execute((m_dlgGetArgument == null) ? default(T) : m_dlgGetArgument());
+		}
+	}
+}
---
+++ Transactions/TransactionInformation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Nexus.Transactions
+{
+	/// <summary>
+	/// The possible statuses of a transaction.
+	/// </summary>
+	public enum TransactionStatus
+	{
+		Active,
+		Committed,
+		Aborted,
+		InDoubt
+	}
+
+	/// <summary>
+	/// Information about a transaction.
+	/// </summary>
+	public class TransactionInformation
+	{
+		private TransactionStatus m_tstStatus = TransactionStatus.Active;
+		private string m_strLocalIdentifier = Guid.NewGuid().ToString();
+
+		/// <summary>
+		/// Gets the status of the transaction.
+		/// </summary>
+		/// <value>The status of the transaction.</value>
+		public TransactionStatus Status
+		{
+			get
+			{
+				return m_tstStatus;
+			}
+			internal set
+			{
+				m_tstStatus = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets the unique identifier of the transaction.
+		/// </summary>
+		/// <value>The unique identifier of the transaction.</value>
+		public string LocalIdentifier
+		{
+			get
+			{
+				return m_strLocalIdentifier;
+			}
+		}
+	}
+}
