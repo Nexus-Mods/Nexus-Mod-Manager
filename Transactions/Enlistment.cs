8dc33aa1a735dd6304c87a083b783302a86477d0
duskdweller <duskdweller@localhost>
2013-08-08 13:01:42 +0200

    NMM freezing or unable to proceed when a scripted installers points to an invalid image path.

---
+++ Transactions/Enlistment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Nexus.Transactions
+{
+	/// <summary>
+	/// Describes the enlistment of a resource manager in a transaction.
+	/// </summary>
+	public class Enlistment
+	{
+		/// <summary>
+		/// Gets or sets whether the enlisted resource manager has finished its processing.
+		/// </summary>
+		/// <value>Whether the enlisted resource manager has finished its processing.</value>
+		internal bool DoneProcessing { get; set; }
+
+		/// <summary>
+		/// Marks the enlistmant as having finished.
+		/// </summary>
+		public virtual void Done()
+		{
+			DoneProcessing = true;
+		}
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public Enlistment()
+		{
+			DoneProcessing = false;
+		}
+	}
+}
---
+++ XmlScript/Unparsers/UnsupportedScriptFeatureException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Nexus.Client.ModManagement.Scripting.XmlScript.Unparsers
+{
+	/// <summary>
+	/// The exception that is thrown if a feature of an XML Script cannot be unparsed to the
+	/// target script version.
+	/// </summary>
+	public class UnsupportedScriptFeatureException : Exception
+	{
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public UnsupportedScriptFeatureException()
+		{
+		}
+
+		/// <summary>
+		/// A simple contructor that sets the exception's message.
+		/// </summary>
+		/// <param name="message">The exception's message.</param>
+		public UnsupportedScriptFeatureException(string message)
+			: base(message)
+		{
+		}
+
+		/// <summary>
+		/// A simple constructor the sets the exception's message and inner exception.
+		/// </summary>
+		/// <param name="message">The exception's message.</param>
+		/// <param name="inner">The ineer exception.</param>
+		public UnsupportedScriptFeatureException(string message, Exception inner)
+			: base(message, inner)
+		{
+		}
+	}
+}
