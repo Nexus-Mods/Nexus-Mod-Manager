828f77b16967a314b399e0200e16935ac51f543e
duskdweller <duskdweller@localhost>
2013-10-31 18:58:34 +0100

    This will fix a crash while trying to run the archive invalidation.

---
+++ ModManager.Interface/ModManagement/CategoryManager/ICategoryManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nexus.Client.Mods;
+using Nexus.Client.Util.Collections;
+
+namespace Nexus.Client.ModManagement
+{
+	/// <summary>
+	/// Describes the properties and methods of a category manager.
+	/// </summary>
+	/// <remarks>
+	/// A category manager.
+	/// </remarks>
+	public interface ICategoryManager
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets the list of categories.
+		/// </summary>
+		/// <value>The list of categories.</value>
+		ThreadSafeObservableList<IModCategory> Categories { get; }
+
+		#endregion
+
+		#region Singleton
+
+		/// <summary>
+		/// Releases the manager's hold on physical resources.
+		/// </summary>
+		void Release();
+
+		#endregion
+
+		#region Category Management
+
+		/// <summary>
+		/// Adds a category to the category manager.
+		/// </summary>
+		/// <remarks>
+		/// Adding a category to the category manager assigns it a unique key.
+		/// </remarks>
+		/// <param name="p_mctCategory">The <see cref="IModCategory"/> being added.</param>
+		IModCategory AddCategory(IModCategory p_mctCategory);
+
+		/// <summary>
+		/// Updates the category file.
+		/// </summary>
+		void UpdateCategoryFile();
+
+		/// <summary>
+		/// Removes the category from the category manager.
+		/// </summary>
+		/// <param name="p_mctUninstaller">The category to remove.</param>
+		void RemoveCategory(IModCategory p_mctUninstaller);
+
+		#endregion
+
+		/// <summary>
+		/// This backsup the category manager.
+		/// </summary>
+		void Backup();
+	}
+}
---
+++ NexusClient.Interface/Settings/KeyedSettings.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using System;
+
+namespace Nexus.Client.Settings
+{
+	/// <summary>
+	/// A dictionary that stores keyed settings.
+	/// </summary>
+	public class KeyedSettings<T> : Dictionary<string, T>, IXmlSerializable
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets the name of the key name to use when serializing the dictionary.
+		/// </summary>
+		/// <value>The name of the key name to use when serializing the dictionary.</value>
+		protected virtual string XmlKeyName
+		{
+			get
+			{
+				return "key";
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public KeyedSettings()
+		{
+		}
+
+		/// <summary>
+		/// Initializes the keyed settings with the values in the given dictionary.
+		/// </summary>
+		/// <param name="p_dicValues">The values with which to initializes this object.</param>
+		public KeyedSettings(Dictionary<string, T> p_dicValues)
+			: base(p_dicValues)
+		{
+		}
+
+		#endregion
+
+		#region IXmlSerializable Members
+
+		/// <summary>
+		/// This method is reserved, and returns <lang dref="null"/> as required.
+		/// </summary>
+		/// <returns><lang dref="null"/>, as required.</returns>
+		public XmlSchema GetSchema()
+		{
+			return null;
+		}
+
+		/// <summary>
+		/// Deserializes the object from XML.
+		/// </summary>
+		/// <param name="reader">The xml reader from which to deserialize the object.</param>
+		public void ReadXml(XmlReader reader)
+		{
+			bool booIsEmpty = reader.IsEmptyElement;
+			reader.ReadStartElement();
+			if (booIsEmpty)
+				return;
+			while (reader.MoveToContent() == XmlNodeType.Element && reader.LocalName == "item")
+			{
+				string strKey = reader[XmlKeyName];
+				booIsEmpty = reader.IsEmptyElement;
+				reader.ReadStartElement("item");
+				T tValue = default(T);
+				if (!booIsEmpty)
+				{
+					XmlSerializer xsrSerializer = new XmlSerializer(typeof(T));
+					tValue = (T)xsrSerializer.Deserialize(reader);
+					reader.ReadEndElement();
+				}
+				this[strKey] = tValue;
+			}
+			reader.ReadEndElement();
+		}
+
+		/// <summary>
+		/// Serializes the object to XML.
+		/// </summary>
+		/// <param name="writer">The xml writer to which to serialize the object.</param>
+		public void WriteXml(XmlWriter writer)
+		{
+			foreach (KeyValuePair<string, T> kvpItem in this)
+			{
+				writer.WriteStartElement("item");
+				writer.WriteAttributeString(XmlKeyName, (string)kvpItem.Key);
+
+				try
+				{
+					XmlSerializer xsrSerializer = new XmlSerializer(typeof(T));
+					xsrSerializer.Serialize(writer, kvpItem.Value);
+				}
+				catch (Exception e)
+				{
+					string g = e.ToString();
+				}
+
+				writer.WriteEndElement();
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Gets or sets the value for the given key.
+		/// </summary>
+		/// <remarks>
+		/// This implementation returns the default value of <typeparamref name="T"/> instead
+		/// of throwing an exceiption of the given key is not in the dictionary.
+		/// </remarks>
+		/// <param name="p_strKey">The key of the value to retrieve.</param>
+		/// <returns>The value for the given key.</returns>
+		public new T this[string p_strKey]
+		{
+			get
+			{
+				if (!ContainsKey(p_strKey))
+					return default(T);
+				return base[p_strKey];
+			}
+			set
+			{
+				base[p_strKey] = value;
+			}
+		}
+	}
+}
