61c7663f366493406624371c50dc544a2badf5e4
duskdweller <duskdweller@localhost>
2013-10-31 18:26:06 +0100

    The tracelog window will now ask the user whether he wants to open the folder containing the tracelog files.

---
+++ UI/Controls/DoubleBufferedListView.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+
+namespace Nexus.UI.Controls
+{
+	/// <summary>
+	/// A normal <see cref="ListView"/>, but with double buffering enabled.
+	/// </summary>
+	public class DoubleBufferedListView : ListView
+	{
+		#region Constructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public DoubleBufferedListView()
+			: base()
+		{
+			this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+		}
+
+		#endregion
+	}
+}
---
+++ UI/Controls/ImageViewer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Nexus.UI.Controls
+{
+	/// <summary>
+	/// Displays an image.
+	/// </summary>
+	public partial class ImageViewer : Form
+	{
+		#region Properties
+
+		/// <summary>
+		/// Sets the image being viewed.
+		/// </summary>
+		/// <value>The image being viewed.</value>
+		public Image Image
+		{
+			set
+			{
+				pbxImage.Image = value;
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public ImageViewer()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Handles the <see cref="ToolStripItem.Click"/> event of the zoom buttons.
+		/// </summary>
+		/// <remarks>
+		/// This changes the size mode of the picture box to match the selected zoom mode.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void Zoom_Click(object sender, EventArgs e)
+		{
+			tsbZoomOriginal.Checked = !tsbZoomOriginal.Checked;
+			tsbZoomFit.Checked = !tsbZoomOriginal.Checked;
+			pbxImage.SizeMode = tsbZoomFit.Checked ? PictureBoxSizeMode.Zoom : PictureBoxSizeMode.AutoSize;
+			pbxImage.Dock = tsbZoomFit.Checked ? DockStyle.Fill : DockStyle.None;
+		}
+		
+		/// <summary>
+		/// Handles the <see cref="Control.Click"/> event of the picture box.
+		/// </summary>
+		/// <remarks>
+		/// This passes focus to the scroll panel so that it can recieve mouse wheel
+		/// events (and other events that indicated a scrolling action).
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void pbxImage_Click(object sender, EventArgs e)
+		{
+			panel1.Focus();
+		}
+	}
+}
