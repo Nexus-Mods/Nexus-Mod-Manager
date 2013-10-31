495cbe88356495ca0fa39e8f96ec7e8d1f44f177
duskdweller <duskdweller@localhost>
2013-10-31 18:21:43 +0100

    This will provide compatibility with every Dragon Age versions.

---
+++ NexusClient.Interface/PluginManagement/OrderLog/IPluginOrderLog.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Nexus.Client.Plugins;
+using Nexus.Client.Util.Collections;
+
+namespace Nexus.Client.PluginManagement.OrderLog
+{
+	/// <summary>
+	/// The log that tracks the order of plugins.
+	/// </summary>
+	public interface IPluginOrderLog
+	{
+		#region Singleton
+
+		/// <summary>
+		/// This disposes of the singleton object, allowing it to be re-initialized.
+		/// </summary>
+		void Release();
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the list of ordered plugins.
+		/// </summary>
+		/// <value>The list of ordered plugins.</value>
+		ReadOnlyObservableList<Plugin> OrderedPlugins { get; }
+
+		#endregion
+
+		#region Plugin Order Management
+
+		/// <summary>
+		/// Sets the order of the plugins to the given order.
+		/// </summary>
+		/// <remarks>
+		/// If the given list does not include all registered plugins, then the plugins are ordered in a manner
+		/// so as to not displace the positions of the plugins whose order was not specified.
+		/// </remarks>
+		/// <param name="p_lstOrderedPlugins">The list indicating the desired order of the plugins.</param>
+		void SetPluginOrder(IList<Plugin> p_lstOrderedPlugins);
+
+		/// <summary>
+		/// Sets the load order of the specifid plugin.
+		/// </summary>
+		/// <param name="p_plgPlugin">The full path to the plugin file whose load order is to be set.</param>
+		/// <param name="p_intNewIndex">The new load order index of the plugin.</param>
+		void SetPluginOrderIndex(Plugin p_plgPlugin, int p_intNewIndex);
+
+		/// <summary>
+		/// Removes the given plugin from the order list.
+		/// </summary>
+		/// <param name="p_plgPlugin">The plugin to remove from the order list.</param>
+		void RemovePlugin(Plugin p_plgPlugin);
+
+		#endregion
+	}
+}
---
+++ XmlScript/UI/Controls/NodeEditors/InstallStepEditor.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Nexus.Client.Util;
+using Nexus.UI.Controls;
+
+namespace Nexus.Client.ModManagement.Scripting.XmlScript.UI.Controls.NodeEditors
+{
+	public partial class InstallStepEditor : NodeEditor
+	{
+		private InstallStepEditorVM m_vmlViewModel = null;
+		private InstallStepVM m_ispInstallStepVM = null;
+
+		#region Properties
+
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public InstallStepEditorVM ViewModel
+		{
+			get
+			{
+				return m_vmlViewModel;
+			}
+			set
+			{
+				m_vmlViewModel = value;
+				if (value.VisibilityVisible)
+					cedVisibilityCondition.ViewModel = value.ConditionEditorVM;
+				InstallStepVM = value.InstallStepVM;
+				cbxSortOrder.DataSource = value.SortOrders;
+
+				pnlName.Visible = value.NameVisible;
+				pnlSortOrder.Visible = value.GroupSortOrderVisible;
+				if (!value.VisibilityVisible)
+					tclInstallStep.TabPages.Remove(tpgVisibility);
+				else if (!tclInstallStep.TabPages.Contains(tpgVisibility))
+					tclInstallStep.TabPages.Add(tpgVisibility);
+
+				value.InstallStepValidated += new EventHandler(InstallStepValidated);
+			}
+		}
+
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		protected InstallStepVM InstallStepVM
+		{
+			get
+			{
+				return m_ispInstallStepVM;
+			}
+			set
+			{
+				m_ispInstallStepVM = value;
+				erpErrors.SetError(tbxName, null);
+
+				tbxName.DataBindings.Clear();
+				cbxSortOrder.DataBindings.Clear();
+
+				BindingHelper.CreateFullBinding(tbxName, () => tbxName.Text, value, () => value.Name);
+				BindingHelper.CreateFullBinding(cbxSortOrder, () => cbxSortOrder.SelectedItem, value, () => value.GroupSortOrder);
+
+				rlvGroups.Items.Clear();
+				foreach (OptionGroup grpGroup in value.OptionGroups)
+				{
+					ListViewItem lviItem = new ListViewItem(grpGroup.Name);
+					lviItem.Tag = grpGroup;
+					rlvGroups.Items.Add(lviItem);
+				}
+			}
+		}
+
+		#endregion
+
+		public InstallStepEditor(InstallStepEditorVM p_vmlViewModel)
+		{
+			InitializeComponent();
+			ViewModel = p_vmlViewModel;
+		}
+
+		private void tbxName_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape)
+			{
+				Binding bndBinding = ((Control)sender).DataBindings[0];
+				ViewModel.InstallStepVM.Reset(bndBinding.BindingMemberInfo.BindingField);
+				bndBinding.ReadValue();
+				e.Handled = true;
+				e.SuppressKeyPress = true;
+			}
+		}
+
+		private void InstallStepValidated(object sender, EventArgs e)
+		{
+			erpErrors.SetError(tbxName, ViewModel.Errors.GetError<OptionGroupVM>(x => x.Name));
+		}
+
+		private void Control_Validated(object sender, EventArgs e)
+		{
+			Binding bndBinding = ((Control)sender).DataBindings[0];
+			ViewModel.SaveInstallStep(bndBinding.BindingMemberInfo.BindingField);
+		}
+
+		/// <summary>
+		/// Gets the view model of the editor.
+		/// </summary>
+		/// <returns>The view model of the editor.</returns>
+		public override IViewModel GetViewModel()
+		{
+			return ViewModel;
+		}
+
+		private void rlvGroups_Resize(object sender, EventArgs e)
+		{
+			clmGroupName.Width = rlvGroups.ClientSize.Width;
+		}
+
+		private void cbxSortOrder_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			rlvGroups.Enabled = ((SortOrder)cbxSortOrder.SelectedItem == SortOrder.Explicit);
+			//we have to explicitly write the value to the bound datasource, as this handler may be
+			// called before the binding updates itself, even if the update mode is set
+			// to OnPropertyChange (presumably this is because the datasource uses the same event
+			// to write to the datasource, and our handler can be called first).
+			Binding bndBinding = ((Control)sender).DataBindings[0];
+			bndBinding.WriteValue();
+			ViewModel.SaveInstallStep(bndBinding.BindingMemberInfo.BindingField);
+		}
+
+		private void rlvGroups_ItemsReordered(object sender, ReorderedItemsEventArgs e)
+		{
+			foreach (ReorderedItemsEventArgs.ReorderedListViewItem rliItem in e.ReorderedListViewItems)
+			{
+				InstallStepVM.OptionGroups.RemoveAt(rliItem.OldIndex);
+				InstallStepVM.OptionGroups.Insert(rliItem.NewIndex, (OptionGroup)rliItem.Item.Tag);
+			}
+			ViewModel.SaveInstallStep(ObjectHelper.GetPropertyName<InstallStep>(x => x.OptionGroups));
+		}
+
+		private void tclInstallStep_Deselecting(object sender, TabControlCancelEventArgs e)
+		{
+			if (e.TabPage == tpgVisibility)
+				e.Cancel = !ViewModel.Validate();
+		}
+	}
+}
