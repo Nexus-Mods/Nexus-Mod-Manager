f75e2f12ae2c5409fe4fbdc1c0deed28b1fd403c
duskdweller <duskdweller@localhost>
2014-01-27 11:41:48 +0100

    Closing branch in preparation for merge.

--- Morrowind.ModScript/Properties/AssemblyInfo.cs
+++ Morrowind.ModScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.4937.24584")]
+[assembly: AssemblyFileVersion("1.1.5137.32930")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
