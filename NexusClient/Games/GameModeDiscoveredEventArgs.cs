730f1cfcd41307377f8d4babb86d8e35730c5a26
duskdweller <duskdweller@localhost>
2014-02-27 10:28:44 +0100

    This will fix a crash and a disappearing folders bug while using the searchbox in the category view.

---
+++ NexusClient/Games/GameModeDiscoveredEventArgs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Nexus.Client.Games
+{
+	/// <summary>
+	/// An event arguments class that indicates a possible installation path of
+	/// a game mode has been found.
+	/// </summary>
+	public class GameModeDiscoveredEventArgs : EventArgs
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets the game mode info for the game for which an installation path has been found.
+		/// </summary>
+		/// <value>The game mode info for the game for which an installation path has been found.</value>
+		public IGameModeDescriptor GameMode { get; private set; }
+
+		/// <summary>
+		/// Gets the installation path that was found.
+		/// </summary>
+		/// <value>The installation path that was found.</value>
+		public string InstallationPath { get; private set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given values.
+		/// </summary>
+		/// <param name="p_gmdGameModeInfo">The game mode info for the game for which an installation path has been found.</param>
+		/// <param name="p_strInstallationPath">The installation path that was found.</param>
+		public GameModeDiscoveredEventArgs(IGameModeDescriptor p_gmdGameModeInfo, string p_strInstallationPath)
+		{
+			GameMode = p_gmdGameModeInfo;
+			InstallationPath = p_strInstallationPath;
+		}
+
+		#endregion
+	}
+}
