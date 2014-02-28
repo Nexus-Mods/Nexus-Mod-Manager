8195041fde3b48e4c0adc6d7cbe7df8558f96707
duskdweller <duskdweller@localhost>
2014-02-28 16:30:28 +0100

    Closing branch in preparation for merge.

--- FalloutNV.XmlScript/Properties/AssemblyInfo.cs
+++ FalloutNV.XmlScript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.5172.17049")]
+[assembly: AssemblyFileVersion("1.1.5172.27519")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
--- WorldOfTanks/Properties/AssemblyInfo.cs
+++ WorldOfTanks/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.5172.17049")]
+[assembly: AssemblyFileVersion("1.1.5172.27518")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
