3f7460673ca6faa0e90557b2bd18bf2f03e3ffa4
duskdweller <duskdweller@localhost>
2014-02-28 16:30:28 +0100

    Closing branch in preparation for merge.

--- DragonAge2/Properties/AssemblyInfo.cs
+++ DragonAge2/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.5172.17047")]
+[assembly: AssemblyFileVersion("1.1.5172.27517")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
