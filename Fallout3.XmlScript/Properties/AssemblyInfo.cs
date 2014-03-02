02dddb6ef04210e69def3fca337f1c286e5d1431
duskdweller <duskdweller@localhost>
2014-03-02 12:33:20 +0100

    Closing branch in preparation for merge.

--- Fallout3.XmlScript/Properties/AssemblyInfo.cs
+++ Fallout3.XmlScript/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.5172.27954")]
+[assembly: AssemblyFileVersion("1.1.5174.20716")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
