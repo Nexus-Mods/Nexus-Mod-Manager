1c390df0f78662493dc836a11ad3b42ccb57dad5
duskdweller <duskdweller@localhost>
2014-03-03 18:42:34 +0100

    This will fix the ability to force a global check on the mod update that went missing somewhere along the last year's updates.

---
+++ NexusClient/Settings/ModOptionsSettingsGroup.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Nexus.Client.Games;
+using SevenZip;
+
+namespace Nexus.Client.Settings
+{
+	/// <summary>
+	/// The group of mod option settings.
+	/// </summary>
+	public class ModOptionsSettingsGroup : SettingsGroup
+	{
+		private CompressionLevel m_cplCompressionLevel = CompressionLevel.Ultra;
+		private OutArchiveFormat m_oafModArchiveFormat = OutArchiveFormat.SevenZip;
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the title of the settings group.
+		/// </summary>
+		/// <value>The title of the settings group.</value>
+		public override string Title
+		{
+			get
+			{
+				return "Mod Options";
+			}
+		}
+
+		/// <summary>
+		/// Gets the list of available compression levels.
+		/// </summary>
+		/// <value>The list of available compression levels.</value>
+		public CompressionLevel[] ModCompressionLevels { get; private set; }
+
+		/// <summary>
+		/// Gets or sets the preferred compression level to use for mods.
+		/// </summary>
+		/// <remarks>
+		/// Note that not all mod formats support confirgurable compression levels.
+		/// </remarks>
+		/// <value>The preferred compression level to use for mods.</value>
+		public CompressionLevel ModCompressionLevel
+		{
+			get
+			{
+				return m_cplCompressionLevel;
+			}
+			set
+			{
+				SetPropertyIfChanged(ref m_cplCompressionLevel, value, () => ModCompressionLevel);
+			}
+		}
+
+		/// <summary>
+		/// Gets the list of available compression formats.
+		/// </summary>
+		/// <value>The list of available compression formats.</value>
+		public OutArchiveFormat[] ModCompressionFormats { get; private set; }
+
+		/// <summary>
+		/// Gets or sets the preferred compression format to use for mods.
+		/// </summary>
+		/// <remarks>
+		/// Note that not all mod formats support confirgurable compression formats.
+		/// </remarks>
+		/// <value>The preferred compression format to use for mods.</value>
+		public OutArchiveFormat ModCompressionFormat
+		{
+			get
+			{
+				return m_oafModArchiveFormat;
+			}
+			set
+			{
+				SetPropertyIfChanged(ref m_oafModArchiveFormat, value, () => ModCompressionFormat);
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// A simple constructor that initializes the object with the given dependencies.
+		/// </summary>
+		/// <param name="p_eifEnvironmentInfo">The application's envrionment info.</param>
+		public ModOptionsSettingsGroup(IEnvironmentInfo p_eifEnvironmentInfo)
+			: base(p_eifEnvironmentInfo)
+		{
+			ModCompressionLevels = (CompressionLevel[])Enum.GetValues(typeof(CompressionLevel));
+			ModCompressionFormats = (OutArchiveFormat[])Enum.GetValues(typeof(OutArchiveFormat));
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Loads the grouped setting values from the persistent store.
+		/// </summary>
+		public override void Load()
+		{
+			ModCompressionLevel = EnvironmentInfo.Settings.ModCompressionLevel;
+			ModCompressionFormat = EnvironmentInfo.Settings.ModCompressionFormat;
+		}
+
+		/// <summary>
+		/// Persists the grouped setting values to the persistent store.
+		/// </summary>
+		/// <returns><c>true</c> if the settings were persisted;
+		/// <c>false</c> otherwise.</returns>
+		public override bool Save()
+		{
+			EnvironmentInfo.Settings.ModCompressionLevel = ModCompressionLevel;
+			EnvironmentInfo.Settings.ModCompressionFormat = ModCompressionFormat;
+			EnvironmentInfo.Settings.Save();
+			return true;
+		}
+	}
+}
---
+++ XmlScript/UI/Controls/NodeEditors/InstallableFileEditorVM.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using Nexus.UI.Controls;
+using Nexus.Client.Util;
+
+namespace Nexus.Client.ModManagement.Scripting.XmlScript.UI.Controls.NodeEditors
+{
+	public class InstallableFileVM : ObservableObject, IEquatable<InstallableFileVM>
+	{
+		private bool m_booAlwaysInstall = false;
+		private bool m_booInstallIfUsable = false;
+		private string m_strDestination = null;
+		private string m_strSource = null;
+		private Int32 m_intPriority = 0;
+
+		#region Properties
+
+		protected InstallableFile InstallableFile { get; private set; }
+
+		public bool AlwaysInstall
+		{
+			get
+			{
+				return m_booAlwaysInstall;
+			}
+			set
+			{
+				SetPropertyIfChanged(ref m_booAlwaysInstall, value, () => AlwaysInstall);
+			}
+		}
+
+		public bool InstallIfUsable
+		{
+			get
+			{
+				return m_booInstallIfUsable;
+			}
+			set
+			{
+				SetPropertyIfChanged(ref m_booInstallIfUsable, value, () => InstallIfUsable);
+			}
+		}
+
+		public string Destination
+		{
+			get
+			{
+				return m_strDestination;
+			}
+			set
+			{
+				SetPropertyIfChanged(ref m_strDestination, value, () => Destination);
+			}
+		}
+
+		public string Source
+		{
+			get
+			{
+				return m_strSource;
+			}
+			set
+			{
+				SetPropertyIfChanged(ref m_strSource, value, () => Source);
+			}
+		}
+
+		public Int32 Priority
+		{
+			get
+			{
+				return m_intPriority;
+			}
+			set
+			{
+				SetPropertyIfChanged(ref m_intPriority, value, () => Priority);
+			}
+		}
+
+		#endregion
+
+		public InstallableFileVM(InstallableFile p_iflInstallableFile)
+		{
+			InstallableFile = p_iflInstallableFile ?? new InstallableFile(null, null, false, 0, false, false);
+			Reset();
+		}
+
+		public InstallableFile Commit()
+		{
+			InstallableFile.AlwaysInstall = AlwaysInstall;
+			InstallableFile.Destination = Destination;
+			InstallableFile.InstallIfUsable = InstallIfUsable;
+			InstallableFile.Priority = Priority;
+			InstallableFile.Source = Source;
+			return InstallableFile;
+		}
+
+		public void Reset()
+		{
+			AlwaysInstall = InstallableFile.AlwaysInstall;
+			Destination = InstallableFile.Destination;
+			InstallIfUsable = InstallableFile.InstallIfUsable;
+			Priority = InstallableFile.Priority;
+			Source = InstallableFile.Source;
+		}
+
+		#region IEquatable<InstallableFileVM> Members
+
+		public bool Equals(InstallableFileVM other)
+		{
+			return InstallableFile == other.InstallableFile;
+		}
+
+		#endregion
+	}
+
+	public class InstallableFileEditorVM : ObservableObject, IViewModel
+	{
+		public event EventHandler InstallableFileValidated = delegate { };
+		public event EventHandler<EventArgs<InstallableFile>> InstallableFileSaved = delegate { };
+
+		private InstallableFileVM m_ifmInstallableFileVM = null;
+
+		public IList<VirtualFileSystemItem> FileSystemItems { get; private set; }
+
+		public InstallableFile InstallableFile
+		{
+			set
+			{
+				InstallableFileVM = new InstallableFileVM(value);
+			}
+		}
+
+		public InstallableFileVM InstallableFileVM
+		{
+			get
+			{
+				return m_ifmInstallableFileVM;
+			}
+			private set
+			{
+				SetPropertyIfChanged(ref m_ifmInstallableFileVM, value, () => InstallableFileVM);
+			}
+		}
+
+		public ErrorContainer Errors { get; private set; }
+
+		public InstallableFileEditorVM(InstallableFile p_iflInstallableFile, IList<VirtualFileSystemItem> p_lstFileSystemItems)
+		{
+			InstallableFile = p_iflInstallableFile;
+			FileSystemItems = p_lstFileSystemItems;
+			Errors = new ErrorContainer();
+		}
+
+		public void SaveInstallableFile()
+		{
+			if (Validate())
+			{
+				InstallableFileSaved(this, new EventArgs<InstallableFile>(InstallableFileVM.Commit()));
+				InstallableFileVM = new InstallableFileVM(null);
+			}
+		}
+
+		/// <summary>
+		/// Ensures that the source is valid.
+		/// </summary>
+		/// <returns><c>true</c> if the source is valid;
+		/// <c>false</c> otherwise.</returns>
+		public bool ValidateSource()
+		{
+			bool booIsValid = true;
+			Errors.Clear();
+			if (String.IsNullOrEmpty(InstallableFileVM.Source))
+			{
+				Errors.SetError(() => InstallableFileVM.Source, "You must select a source.");
+				booIsValid = false;
+			}
+			InstallableFileValidated(this, new EventArgs());
+			return booIsValid;
+		}
+
+		#region IViewModel Members
+
+		public bool Validate()
+		{
+			return ValidateSource();
+		}
+
+		#endregion
+	}
+}
