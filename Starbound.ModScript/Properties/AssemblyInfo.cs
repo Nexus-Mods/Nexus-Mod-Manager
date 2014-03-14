c1d1da0bf91189a5b4a00e63139a7635f78416a3
duskdweller <duskdweller@localhost>
2014-03-14 11:09:42 +0100

    Indentation.

--- Starbound.ModScript/Properties/AssemblyInfo.cs
+++ Starbound.ModScript/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.5184.1003")]
+[assembly: AssemblyFileVersion("1.1.5186.18231")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
