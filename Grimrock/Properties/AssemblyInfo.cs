895b66ec1fd8c22093e64dbed056e810b3e43139
duskdweller <duskdweller@localhost>
2014-04-30 18:09:59 +0200

    Closing branch in preparation for merge.

--- Grimrock/Properties/AssemblyInfo.cs
+++ Grimrock/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.5233.25876")]
+[assembly: AssemblyFileVersion("1.1.5233.26079")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
