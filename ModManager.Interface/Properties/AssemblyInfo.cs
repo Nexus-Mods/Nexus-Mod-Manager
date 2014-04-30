fb9e0505b64cd8d45c373b6cdfaf4ad5ce289d1e
duskdweller <duskdweller@localhost>
2014-04-30 18:09:59 +0200

    Closing branch in preparation for merge.

--- ModManager.Interface/Properties/AssemblyInfo.cs
+++ ModManager.Interface/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1")]
 // AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.5233.25875")]
+[assembly: AssemblyFileVersion("1.1.5233.26077")]
 [assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
