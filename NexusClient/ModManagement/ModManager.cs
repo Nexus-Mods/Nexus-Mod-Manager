2e56bc24cd90462155738ea0cbb2b0ac9c06ce82
duskdweller <duskdweller@localhost>
2014-05-08 14:19:11 +0200

    Upgraded to the .Net Framework 4.0

--- NexusClient/ModManagement/ModManager.cs
+++ NexusClient/ModManagement/ModManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using Nexus.Client.BackgroundTasks;
 using Nexus.Client.DownloadMonitoring;
