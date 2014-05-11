6b3d7fd104cdadc8fa5db164ef9e85c091f02c44
duskdweller <duskdweller@localhost>
2014-05-11 11:50:40 +0200

    Fixed a crash while trying to create a folder that has not yet been set.

---
+++ NexusClient/ModAuthoring/UI/Controls/ModFilesTreeView.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Nexus.UI.Controls;
+using Nexus.Client.Util;
+
+namespace Nexus.Client.ModAuthoring.UI.Controls
+{
+	/// <summary>
+	/// The file structure of a mod.
+	/// </summary>
+	public partial class ModFilesTreeView : UserControl, INotifyPropertyChanged
+	{
+		#region INotifyPropertyChanged Members
+
+		/// <summary>
+		/// Raised whenever a property of the class changes.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		#endregion
+
+		private FilteredFolderBrowserDialog m_ffbFileChooser = new FilteredFolderBrowserDialog();
+
+		/// <summary>
+		/// Gets or sets the sources listed in the control.
+		/// </summary>
+		/// <value>The sources listed in the control.</value>
+		[Bindable(true)]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public IList<VirtualFileSystemItem> Sources
+		{
+			get
+			{
+				return ftvSource.FileSystemItems;
+			}
+			set
+			{
+				ftvSource.FileSystemItems = value;
+			}
+		}
+
+		/// <summary>
+		/// The default constructor.
+		/// </summary>
+		public ModFilesTreeView()
+		{
+			InitializeComponent();
+			m_ffbFileChooser.ShowNewFolderButton = false;
+			m_ffbFileChooser.Description = "Select the folder containing the files you would like to select. Then, specify a filter that will match the files you wish to select." + Environment.NewLine +
+											"For example, a  filter of mymod*.dds will select alltexture files whose name start with mymod.";
+			m_ffbFileChooser.RootFolder = Environment.SpecialFolder.MyComputer;
+			m_ffbFileChooser.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+			ftvSource.NodeAdded += new NodesChangedEventHandler(ftvSource_NodesChanged);
+			ftvSource.NodeRemoved += new NodesChangedEventHandler(ftvSource_NodesChanged);
+		}
+
+		/// <summary>
+		/// Handles the <see cref="FileSystemTreeViewBase.NodeAdded"/> and 
+		/// <see cref="FileSystemTreeViewBase.NodeRemoved"/> events of the sources tree view.
+		/// </summary>
+		/// <remarks>
+		/// This raised the <see cref="PropertyChanged"/> event on the <see cref="Sources"/> property.
+		/// </remarks>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">A <see cref="NodesChangedEventArgs"/> describing the event arguments.</param>
+		private void ftvSource_NodesChanged(object sender, NodesChangedEventArgs e)
+		{
+			PropertyChanged(this, new PropertyChangedEventArgs(ObjectHelper.GetPropertyName(() => Sources)));
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.Click"/> event of the add files button.
+		/// </summary>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void tspAddFiles_Click(object sender, EventArgs e)
+		{
+			TreeNode tndRoot = ftvSource.SelectedNode;
+			if (ofdFileChooser.ShowDialog(this) == DialogResult.OK)
+				ftvSource.AddPaths((FileSystemTreeNode)tndRoot, ofdFileChooser.FileNames);
+			if (tndRoot != null)
+				tndRoot.Expand();
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.Click"/> event of the add filtered files button.
+		/// </summary>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void tspAddFilteredFiles_Click(object sender, EventArgs e)
+		{
+			TreeNode tndRoot = ftvSource.SelectedNode;
+			if (m_ffbFileChooser.ShowDialog(this) == DialogResult.OK)
+			{
+				string strPathPrefix = m_ffbFileChooser.SelectedPath;
+				List<VirtualFileSystemItem> lstVFSItems = new List<VirtualFileSystemItem>();
+				foreach (string strPath in m_ffbFileChooser.MatchedFiles)
+					lstVFSItems.Add(new VirtualFileSystemItem(strPath, strPath.Substring(strPathPrefix.Length), false));
+				ftvSource.AddVirtualPaths((FileSystemTreeNode)tndRoot, lstVFSItems.ToArray());
+			}
+			if (tndRoot != null)
+				tndRoot.Expand();
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.Click"/> event of the add folder button.
+		/// </summary>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void tsbAddFolder_Click(object sender, EventArgs e)
+		{
+			TreeNode tndRoot = ftvSource.SelectedNode;
+			if (fbdFolderChooser.ShowDialog(this) == DialogResult.OK)
+				ftvSource.AddPath((FileSystemTreeNode)tndRoot, fbdFolderChooser.SelectedPath);
+			if (tndRoot != null)
+				tndRoot.Expand();
+		}
+
+		/// <summary>
+		/// Handles the <see cref="Control.Click"/> event of the delete button.
+		/// </summary>
+		/// <param name="sender">The object that raised the event.</param>
+		/// <param name="e">An <see cref="EventArgs"/> describing the event arguments.</param>
+		private void tsbDelete_Click(object sender, EventArgs e)
+		{
+			ftvSource.RemoveNodes(ftvSource.SelectedNodes);
+		}
+	}
+}
---
+++ XmlScript/UI/Controls/NodeEditors/HeaderEditor.cs
@@ -0,0 +1,113 @@
+﻿using System.Windows.Forms;
+using Nexus.Client.Util;
+using System.ComponentModel;
+using System;
+using Nexus.UI.Controls;
+
+namespace Nexus.Client.ModManagement.Scripting.XmlScript.UI.Controls.NodeEditors
+{
+	public partial class HeaderEditor : NodeEditor
+	{
+		private HeaderEditorVM m_vmlViewModel = null;
+		private FileSelectionDialog m_fsdFileChooser = new FileSelectionDialog();
+
+		#region Properties
+
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public HeaderEditorVM ViewModel
+		{
+			get
+			{
+				return m_vmlViewModel;
+			}
+			set
+			{
+				m_vmlViewModel = value;
+				HeaderInfoVM = value.HeaderInfoVM;
+				cbxAlignment.DataSource = value.TextPositions;
+				m_fsdFileChooser.FileSystemItems = value.ModFiles;
+
+				pnlTitle.Visible = value.TitleVisible;
+				pnlColour.Visible = value.TextColourVisible;
+				pnlAlignment.Visible = value.TextPositionVisible;
+				pnlImage.Visible = value.ImageVisible;
+				pnlHeight.Visible = value.HeightVisible;
+
+				value.HeaderInfoValidated += new EventHandler(HeaderInfoValidated);
+			}
+		}
+
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		protected HeaderInfoVM HeaderInfoVM
+		{
+			set
+			{
+				tbxModName.DataBindings.Clear();
+				cpkColour.DataBindings.Clear();
+				cbxAlignment.DataBindings.Clear();
+				tbxImagePath.DataBindings.Clear();
+				ckbShowImage.DataBindings.Clear();
+				ckbShowFade.DataBindings.Clear();
+				nudHeight.DataBindings.Clear();
+
+				BindingHelper.CreateFullBinding(tbxModName, () => tbxModName.Text, value, () => value.Title);
+				BindingHelper.CreateFullBinding(cpkColour, () => cpkColour.Colour, value, () => value.TextColour);
+				BindingHelper.CreateFullBinding(cbxAlignment, () => cbxAlignment.SelectedItem, value, () => value.TextPosition);
+				BindingHelper.CreateFullBinding(tbxImagePath, () => tbxImagePath.Text, value, () => value.ImagePath);
+				BindingHelper.CreateFullBinding(ckbShowImage, () => ckbShowImage.Checked, value, () => value.ShowImage);
+				BindingHelper.CreateFullBinding(ckbShowFade, () => ckbShowFade.Checked, value, () => value.ShowFade);
+				BindingHelper.CreateFullBinding(nudHeight, () => nudHeight.Value, value, () => value.Height);
+
+				m_fsdFileChooser.SelectedPath = value.ImagePath;
+			}
+		}
+
+		#endregion
+
+		public HeaderEditor(HeaderEditorVM p_vmlViewModel)
+		{
+			InitializeComponent();
+			ViewModel = p_vmlViewModel;
+		}
+
+		private void tbxModName_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape)
+			{
+				Binding bndBinding = ((Control)sender).DataBindings[0];
+				ViewModel.HeaderInfoVM.Reset(bndBinding.BindingMemberInfo.BindingField);
+				bndBinding.ReadValue();
+				e.Handled = true;
+				e.SuppressKeyPress = true;
+			}
+		}
+
+		private void HeaderInfoValidated(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void Control_Validated(object sender, EventArgs e)
+		{
+			ViewModel.SaveHeaderInfo();
+		}
+
+		private void butSelectImage_Click(object sender, EventArgs e)
+		{
+			if (m_fsdFileChooser.ShowDialog(this) == DialogResult.OK)
+			{
+				butSelectImage.Text = m_fsdFileChooser.SelectedPath;
+				butSelectImage.Focus();
+			}
+		}
+
+		/// <summary>
+		/// Gets the view model of the editor.
+		/// </summary>
+		/// <returns>The view model of the editor.</returns>
+		public override IViewModel GetViewModel()
+		{
+			return (IViewModel)ViewModel;
+		}
+	}
+}
