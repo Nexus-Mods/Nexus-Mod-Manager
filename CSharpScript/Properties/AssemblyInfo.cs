7ea3d2ef1b4ff17b7af2781452d0c6b3aa066734
russkie <russkie@localhost>
2016-01-05 12:34:27 +1100

    fix: auto-updated AssemblyInfo.cs

--- CSharpScript/Properties/AssemblyInfo.cs
+++ CSharpScript/Properties/AssemblyInfo.cs
@@ -21,18 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a5a07e92-5d99-4767-ae2d-79ccca90cd99")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1")]
-// AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.5774.31144")]
-[assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
--- Oblivion.ModScript/Properties/AssemblyInfo.cs
+++ Oblivion.ModScript/Properties/AssemblyInfo.cs
@@ -21,18 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d29d8bdf-cc4a-481a-8c72-8825f2fbdf78")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1")]
-// AssemblyFileVersionFormat("1.1.*")
-[assembly: AssemblyFileVersion("1.1.5774.31146")]
-[assembly: AssemblyInformationalVersion(Nexus.Client.ProgrammeMetadata.VersionString)]
