dbee695e17ddfd5ffb6b8f0464c4dfc21523d222
delMar43 <martingu1979@gmail.com>
2016-11-11 21:02:23 +0100

    iGames now Implementing SteamInstallationPathDetector

--- WarThunder/WarThunderGameModeFactory.cs
+++ WarThunder/WarThunderGameModeFactory.cs
@@ -63,75 +63,7 @@ namespace Nexus.Client.Games.WarThunder
 		/// <c>null</c> if the path could not be determined.</returns>
 		public string GetInstallationPath()
 		{
-			var registryKey = @"HKEY_CURRENT_USER\Software\Valve\Steam\Apps\236390";
-			Trace.TraceInformation(@"Checking for steam install: {0}\Installed", registryKey);
-			Trace.Indent();
-
-			string strValue = null;
-			try
-			{
-				var steamKey = Registry.GetValue(registryKey, "Installed", 0);
-				if (steamKey != null)
-				{
-					var isSteamInstall = steamKey.ToString() == "1";
-					if (isSteamInstall)
-					{
-						Trace.TraceInformation("Getting Steam install folder.");
-
-						var steamPath = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Valve\Steam", "SteamPath", null).ToString();
-
-						// convert path to windows path. (steam uses C:/x/y we want C:\\x\\y
-						steamPath = Path.GetFullPath(steamPath);
-						var appPath = Path.Combine(steamPath, @"steamapps\common\War Thunder");
-
-						// check if game is installed in the default directory
-						if (!Directory.Exists(appPath))
-						{
-							Trace.TraceInformation(
-								"WarThunder is not installed in standard directory. Checking steam config.vdf...");
-
-							// second try, check steam config.vdf
-							// if any of this fails, no problem... just drop through the catch
-							var steamConfig = Path.Combine(Path.Combine(steamPath, "config"), "config.vdf");
-							var kv = KeyValue.LoadAsText(steamConfig);
-							var node =
-								kv.Children[0].Children[0].Children[0].Children.Single(x => x.Name == "apps")
-									.Children.Single(x => x.Name == "236390");
-							if (node != null)
-							{
-								appPath = node.Children.Single(x => x.Name == "installdir").Value;
-								if (Directory.Exists(appPath) && File.Exists(Path.Combine(appPath, "aces.exe")))
-									strValue = appPath;
-							}
-						}
-						else
-							strValue = appPath;
-					}
-				}
-			}
-			catch
-			{
-				//if we can't read the registry or config.vdf, just return null
-			}
-
-			try
-			{
-				if (string.IsNullOrWhiteSpace(strValue))
-				{
-					Trace.TraceInformation("Getting install folder from Uninstall.");
-
-					var uniPath = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 236390", "InstallLocation", null).ToString();
-
-					if (Directory.Exists(uniPath))
-						strValue = uniPath;
-				}
-			}
-			catch
-			{
-			}
-
-			Trace.TraceInformation("Found {0}", strValue);
-			Trace.Unindent();
+            string strValue = SteamInstallationPathDetector.Instance.GetSteamInstallationPath("236930", "War Thunder", "aces.exe");
 
 			return strValue;
 		}
