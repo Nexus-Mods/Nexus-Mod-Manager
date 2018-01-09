40bbbb75166c1dff9786539f36cdabe08ac678b4
Joel Ahlgren <joel@ahlgren.io>
2018-01-09 23:08:11 +0100

    Removed unnecessarily cloned projects, works better now.

--- Fallout4VR/Fallout4VRLauncher.cs
+++ Fallout4VR/Fallout4VRLauncher.cs
@@ -41,114 +41,22 @@ namespace Nexus.Client.Games.Fallout4VR
 			string strCommand = GetPlainLaunchCommand();
 			Trace.TraceInformation("Plain Command: {0} (IsNull={1})", strCommand, (strCommand == null));
 			Image imgIcon = File.Exists(strCommand) ? Icon.ExtractAssociatedIcon(strCommand).ToBitmap() : null;
-			AddLaunchCommand(new Command("PlainLaunch", "Launch Fallout4", "Launches plain Fallout4.", imgIcon, LaunchFallout4Plain, true));
-
-			strCommand = GetSkseLaunchCommand();
-			Trace.TraceInformation("SKSE Command: {0} (IsNull={1})", strCommand, (strCommand == null));
-			if (File.Exists(strCommand))
-			{
-				imgIcon = Icon.ExtractAssociatedIcon(strCommand).ToBitmap();
-				AddLaunchCommand(new Command("F4seLaunch", "Launch F4SE", "Launches Fallout4 with F4SE.", imgIcon, LaunchFallout4SKSE, true));
-			}
-
-			strCommand = GetCustomLaunchCommand();
-			Trace.TraceInformation("Custom Command: {0} (IsNull={1})", strCommand, (strCommand == null));
-			imgIcon = File.Exists(strCommand) ? Icon.ExtractAssociatedIcon(strCommand).ToBitmap() : null;
-			AddLaunchCommand(new Command("CustomLaunch", "Launch Custom Fallout4", "Launches Fallout4 with custom command.", imgIcon, LaunchFallout4Custom, true));
-
-			DefaultLaunchCommand = new Command("Launch Fallout4", "Launches Fallout4.", LaunchGame);
+			AddLaunchCommand(new Command("PlainLaunch", "Launch Fallout4VR", "Launches Fallout 4 VR.", imgIcon, LaunchFallout4VR, true));
 
 			Trace.Unindent();
 		}
 
 		#region Launch Commands
-
-		#region Custom Command
-
-		/// <summary>
-		/// Launches the game with a custom command.
-		/// </summary>
-		private void LaunchFallout4Custom()
-		{
-			ForceReadOnlyPluginsFile();
-            Trace.TraceInformation("Launching Fallout4 (Custom)...");
-			Trace.Indent();
-
-			string strCommand = GetCustomLaunchCommand();
-			string strCommandArgs = EnvironmentInfo.Settings.CustomLaunchCommandArguments[GameMode.ModeId];
-			if (String.IsNullOrEmpty(strCommand))
-			{
-				Trace.TraceError("No custom launch command has been set.");
-				Trace.Unindent();
-				OnGameLaunched(false, "No custom launch command has been set.");
-				return;
-			}
-			Launch(strCommand, strCommandArgs);
-		}
-
-		/// <summary>
-		/// Gets the custom launch command.
-		/// </summary>
-		/// <returns>The custom launch command.</returns>
-		private string GetCustomLaunchCommand()
-		{
-			string strCommand = EnvironmentInfo.Settings.CustomLaunchCommands[GameMode.ModeId];
-			if (!String.IsNullOrEmpty(strCommand))
-			{
-				strCommand = Environment.ExpandEnvironmentVariables(strCommand);
-				strCommand = FileUtil.StripInvalidPathChars(strCommand);
-				if (!Path.IsPathRooted(strCommand))
-					strCommand = Path.Combine(GameMode.GameModeEnvironmentInfo.InstallationPath, strCommand);
-			}
-			return strCommand;
-		}
-
-		#endregion
-
-		#region SKSE
-
-		/// <summary>
-		/// Launches the game, with SKSE.
-		/// </summary>
-		private void LaunchFallout4SKSE()
-		{
-			ForceReadOnlyPluginsFile();
-			Trace.TraceInformation("Launching Fallout4 (F4SE)...");
-			Trace.Indent();
-
-			string strCommand = GetSkseLaunchCommand();
-			Trace.TraceInformation("Command: " + strCommand);
-
-			if (!File.Exists(strCommand))
-			{
-				Trace.TraceError("F4SE does not appear to be installed.");
-				Trace.Unindent();
-				OnGameLaunched(false, "F4SE does not appear to be installed.");
-				return;
-			}
-			Launch(strCommand, null);
-		}
-
-		/// <summary>
-		/// Gets the SKSE launch command.
-		/// </summary>
-		/// <returns>The SKSE launch command.</returns>
-		private string GetSkseLaunchCommand()
-		{
-			return Path.Combine(GameMode.GameModeEnvironmentInfo.InstallationPath, "f4se_loader.exe");
-		}
-
-		#endregion
-
+        				
 		#region Vanilla Launch
 
 		/// <summary>
 		/// Launches the game, without OBSE.
 		/// </summary>
-		private void LaunchFallout4Plain()
+		private void LaunchFallout4VR()
 		{
 			ForceReadOnlyPluginsFile();
-            Trace.TraceInformation("Launching Fallout4 (Plain)...");
+            Trace.TraceInformation("Launching Fallout 4 VR...");
 			Trace.Indent();
 			string strCommand = GetPlainLaunchCommand();
 			Trace.TraceInformation("Command: " + strCommand);
@@ -172,14 +80,8 @@ namespace Nexus.Client.Games.Fallout4VR
 		/// </summary>
 		private void LaunchGame()
 		{
-			ForceReadOnlyPluginsFile();
-
-			if (!string.IsNullOrEmpty(EnvironmentInfo.Settings.CustomLaunchCommands[GameMode.ModeId]))
-				LaunchFallout4Custom();
-			else if (File.Exists(Path.Combine(GameMode.GameModeEnvironmentInfo.InstallationPath, "f4se_loader.exe")))
-				LaunchFallout4SKSE();
-			else
-				LaunchFallout4Plain();
+			ForceReadOnlyPluginsFile();		
+            LaunchFallout4VR();
 		}
 
 		#endregion
