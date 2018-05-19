dc48646b0305230abe11af02d3a042bc92172460
Joel Ahlgren <joel@ahlgren.io>
2018-05-19 23:36:15 +0200

    When scanning for games, we now take the underlying file system into consideration. If it's incompatible we show an error and block the user from selecting that game.

--- NexusClient/Games/GameModeDiscoveredEventArgs.cs
+++ NexusClient/Games/GameModeDiscoveredEventArgs.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Nexus.Client.Games
 {
@@ -22,6 +24,11 @@ namespace Nexus.Client.Games
 		/// <value>The installation path that was found.</value>
 		public string InstallationPath { get; private set; }
 
+        /// <summary>
+        /// Gets a value determining whether or not the game is installed on a file system that supports symbolic linking.
+        /// </summary>
+        public bool InstalledOnSuitableFileSystem { get; private set; }
+
 		#endregion
 
 		#region Constructors
@@ -35,7 +42,36 @@ namespace Nexus.Client.Games
 		{
 			GameMode = p_gmdGameModeInfo;
 			InstallationPath = p_strInstallationPath;
-		}
+            InstalledOnSuitableFileSystem = DoesFileSystemSupportSymbolicLinks(p_strInstallationPath);            
+        }
+
+        /// <summary>
+        /// Determines if the file system of the drive is suitable for NMM to use.
+        /// </summary>
+        /// <param name="p_strPath">Path to folder on drive we want to check.</param>
+        /// <returns>True if we expect NMM to be able to use the drive in question, otherwise false.</returns>
+        private static bool DoesFileSystemSupportSymbolicLinks(string p_strPath)
+        {
+            if (string.IsNullOrEmpty(p_strPath))
+            {
+                // Won't matter if there's no path.
+                return true;
+            }
+
+            // This list can be extended as needed, and is not case sensitive.
+            var knownBadFileSystems = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+            {
+                "FAT",
+                "FAT32",
+                "ReFS",
+                "exFAT"
+            };
+
+            var file = new FileInfo(p_strPath);
+            var drive = new DriveInfo(file.Directory.Root.FullName);
+
+            return !knownBadFileSystems.Contains(drive.DriveFormat);
+        }
 
 		#endregion
 	}
