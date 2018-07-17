7e7ddf27a16973de03e3b55f8f14584fa20ef3bd
Joel Ahlgren <joel@ahlgren.io>
2018-07-17 22:16:25 +0200

    Initial stab at adding unit tests to solution. Added trivial tests for existing code.

---
+++ Tests/UtilTests/TextUtilTests.cs
@@ -0,0 +1,33 @@
+﻿namespace UtilTests
+{
+    using Nexus.Client.Util;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TextUtilTests
+    {
+        private readonly byte[] _thisIsATest = { 0x74, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x61, 0x20, 0x74, 0x65, 0x73, 0x74 };
+
+        [Test]
+        public void ByteToStringLinesTest()
+        {
+            var result = TextUtil.ByteToStringLines(new byte[0]);
+            Assert.AreEqual("", result);
+
+            Assert.Ignore("Can't write working test input/results.");
+        }
+
+        [Test]
+        public void ByteToStringTest()
+        {
+            var result = TextUtil.ByteToString(new byte[0]);
+            Assert.AreEqual("", result);
+
+            const string thisIsATestString = "this is a test";
+
+            result = TextUtil.ByteToString(_thisIsATest);
+            Assert.AreEqual(thisIsATestString, result);
+        }
+    }
+}
---
+++ Tests/UtilTests/UriUtilTests.cs
@@ -0,0 +1,36 @@
+﻿namespace UtilTests
+{
+    using System;
+    using Nexus.Client.Util;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UriUtilTests
+    {
+        [Test]
+        public void ValidUriTest()
+        {
+            Assert.IsTrue(UriUtil.TryBuildUri("http://www.nexusmods.com", out var result));
+            Assert.AreEqual(new Uri("http://www.nexusmods.com"), result);
+
+            Assert.IsTrue(UriUtil.TryBuildUri("https://www.nexusmods.com", out result));
+            Assert.AreEqual(new Uri("https://www.nexusmods.com"), result);
+
+            Assert.IsTrue(UriUtil.TryBuildUri("www.nexusmods.com", out result));
+            Assert.AreEqual(new Uri("http://www.nexusmods.com"), result);
+
+            Assert.IsTrue(UriUtil.TryBuildUri("nexusmods.com", out result));
+            Assert.AreEqual(new Uri("http://nexusmods.com"), result);
+        }
+
+        [Test]
+        public void InvalidUriTest()
+        {
+            Assert.IsFalse(UriUtil.TryBuildUri("this is not a URL", out var result));
+            Assert.IsNull(result);
+
+            Assert.IsFalse(UriUtil.TryBuildUri(string.Empty, out result));
+            Assert.IsNull(result);
+        }
+    }
+}
