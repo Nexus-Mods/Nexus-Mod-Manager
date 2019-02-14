4d64326c31aaae4ca46c5a935d50fddb3954b5de
Joel Ahlgren <joel@ahlgren.io>
2019-02-15 00:31:20 +0100

    Getting started connecting to the new API, using RestSharp.

---
+++ NexusClient/ModRepositories/NexusModsApi/ApiCallManager.cs
@@ -0,0 +1,59 @@
+﻿namespace Nexus.Client.ModRepositories.NexusModsApi
+{
+    using System;
+    using System.IO;
+    using RestSharp;
+    using Util;
+
+    /// <summary>
+    /// Central point for managing calls to the API.
+    /// </summary>
+    public class ApiCallManager
+    {
+        private const string BaseUrl = "https://api.nexusmods.com";
+
+        private string _apiKey;
+        private readonly RestClient _restClient;
+
+        public ApiCallManager()
+        {
+            _apiKey = @"GetYourOwn";
+            _restClient = new RestClient(BaseUrl);
+            RateLimit = new RateLimit();
+        }
+
+        public RateLimit RateLimit { get; }
+
+        /// <summary>
+        /// Application reference to be used with SSO integration.
+        /// </summary>
+        public static string ApplicationReference => "HaveNotRequestedOneYet";
+
+        /// <summary>
+        /// Gets the user agent string to send to the Nexus Mods API.
+        /// </summary>
+        public static string UserAgent => $"NexusModManager/{CommonData.VersionString} ({Environment.OSVersion})";
+
+        private IRestResponse ExecuteRequest(IRestRequest request)
+        {
+            request.AddHeader("apikey", _apiKey);
+            request.AddHeader("User-Agent", UserAgent);
+
+            var result = _restClient.Execute(request);
+
+            RateLimit.Update(result.Headers);
+
+            return result;
+        }
+
+        public IRestResponse TestApiCall()
+        {
+            var request = new RestRequest("v1/games.json", Method.GET);
+            request.AddParameter("include_unapproved", "false");
+
+            var result = ExecuteRequest(request);
+
+            return result;
+        }
+    }
+}
---
+++ NexusClient/ModRepositories/NexusModsApi/RateLimit.cs
@@ -0,0 +1,100 @@
+﻿namespace Nexus.Client.ModRepositories.NexusModsApi
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using RestSharp;
+
+    /// <summary>
+    /// Keeps track of current rate limit against the API.
+    /// </summary>
+    public class RateLimit
+    {
+        public RateLimit()
+        {
+            HourlyLimit = -1;
+            HourlyLimitRemaining = -1;
+            HourlyLimitReset = DateTime.MinValue;
+
+            DailyLimit = -1;
+            DailyLimitRemaining = -1;
+            DailyLimitReset = DateTime.MinValue;
+        }
+
+        /// <summary>
+        /// The maximum API calls allowed per hour.
+        /// </summary>
+        public int HourlyLimit { get; private set; }
+
+        /// <summary>
+        /// Number of API calls remaining of the hourly limit.
+        /// </summary>
+        public int HourlyLimitRemaining { get; private set; }
+
+        /// <summary>
+        /// Time when the hourly limit is reset.
+        /// </summary>
+        public DateTime HourlyLimitReset { get; private set; }
+
+        /// <summary>
+        /// The maximum API calls allowed per day.
+        /// </summary>
+        public int DailyLimit { get; private set; }
+
+        /// <summary>
+        /// Number of API calls remaining of the daily limit.
+        /// </summary>
+        public int DailyLimitRemaining { get; private set; }
+
+        /// <summary>
+        /// Time when the daily limit is reset.
+        /// </summary>
+        public DateTime DailyLimitReset { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="headers"></param>
+        public void Update(IList<Parameter> headers)
+        {
+            if (headers == null)
+            {
+                return;
+            }
+
+            foreach (var header in headers)
+            {
+                if (header.Name.Equals("X-RL-Hourly-Limit"))
+                {
+                    HourlyLimit = Convert.ToInt32(header.Value);
+                }
+                else if (header.Name.Equals("X-RL-Hourly-Remaining"))
+                {
+                    HourlyLimitRemaining = Convert.ToInt32(header.Value);
+                }
+                else if (header.Name.Equals("X-RL-Hourly-Reset"))
+                {
+                    HourlyLimitReset = DateTime.Parse(header.Value.ToString());
+                }
+                else if (header.Name.Equals("X-RL-Daily-Limit"))
+                {
+                    DailyLimit = Convert.ToInt32(header.Value);
+                }
+                else if (header.Name.Equals("X-RL-Daily-Remaining"))
+                {
+                    DailyLimitRemaining = Convert.ToInt32(header.Value);
+                }
+                else if (header.Name.Equals("X-RL-Daily-Reset"))
+                {
+                    DailyLimitReset = DateTime.Parse(header.Value.ToString());
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Hourly - Limit: {HourlyLimit}, Remaining: {HourlyLimitRemaining}, Reset: {HourlyLimitReset}\n" +
+                   $"Daily - Limit: {DailyLimit}, Remaining: {DailyLimitRemaining}, Rest: {DailyLimitReset}";
+        }
+    }
+}
