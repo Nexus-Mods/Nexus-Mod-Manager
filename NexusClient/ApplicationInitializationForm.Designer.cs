a841eff2b786090c3d421fed709c08a58da93abf
duskdweller <fabio.auletta@gmail.com>
2019-05-12 19:53:05 +0200

    Logo redesign

--- NexusClient/ApplicationInitializationForm.Designer.cs
+++ NexusClient/ApplicationInitializationForm.Designer.cs
@@ -28,55 +28,56 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.pbxLogo = new System.Windows.Forms.PictureBox();
-            this.lblVersion = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pbxLogo
-            // 
-            this.pbxLogo.Image = global::Nexus.Client.Properties.Resources.tes_logo_full;
-            this.pbxLogo.Location = new System.Drawing.Point(0, 0);
-            this.pbxLogo.Margin = new System.Windows.Forms.Padding(0);
-            this.pbxLogo.Name = "pbxLogo";
-            this.pbxLogo.Size = new System.Drawing.Size(520, 178);
-            this.pbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pbxLogo.TabIndex = 0;
-            this.pbxLogo.TabStop = false;
-            // 
-            // lblVersion
-            // 
-            this.lblVersion.AutoSize = true;
-            this.lblVersion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(69)))), ((int)(((byte)(69)))));
-            this.lblVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblVersion.ForeColor = System.Drawing.Color.White;
-            this.lblVersion.Location = new System.Drawing.Point(373, 87);
-            this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(77, 24);
-            this.lblVersion.TabIndex = 1;
-            this.lblVersion.Text = "0.10.11";
-            // 
-            // ApplicationInitializationForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(522, 295);
-            this.ControlBox = false;
-            this.Controls.Add(this.lblVersion);
-            this.Controls.Add(this.pbxLogo);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "ApplicationInitializationForm";
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Nexus Mod Manager";
-            this.TransparencyKey = System.Drawing.SystemColors.Control;
-            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.pbxLogo = new System.Windows.Forms.PictureBox();
+			this.lblVersion = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pbxLogo
+			// 
+			this.pbxLogo.BackColor = System.Drawing.Color.Transparent;
+			this.pbxLogo.Image = global::Nexus.Client.Properties.Resources.Nmm_ce_800;
+			this.pbxLogo.Location = new System.Drawing.Point(0, 0);
+			this.pbxLogo.Margin = new System.Windows.Forms.Padding(0);
+			this.pbxLogo.Name = "pbxLogo";
+			this.pbxLogo.Size = new System.Drawing.Size(800, 360);
+			this.pbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pbxLogo.TabIndex = 0;
+			this.pbxLogo.TabStop = false;
+			// 
+			// lblVersion
+			// 
+			this.lblVersion.AutoSize = true;
+			this.lblVersion.BackColor = System.Drawing.Color.Transparent;
+			this.lblVersion.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblVersion.ForeColor = System.Drawing.Color.White;
+			this.lblVersion.Location = new System.Drawing.Point(148, 328);
+			this.lblVersion.Name = "lblVersion";
+			this.lblVersion.Size = new System.Drawing.Size(70, 23);
+			this.lblVersion.TabIndex = 1;
+			this.lblVersion.Text = "0.10.11";
+			// 
+			// ApplicationInitializationForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(800, 360);
+			this.ControlBox = false;
+			this.Controls.Add(this.lblVersion);
+			this.Controls.Add(this.pbxLogo);
+			this.DoubleBuffered = true;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "ApplicationInitializationForm";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Nexus Mod Manager";
+			this.TransparencyKey = System.Drawing.Color.Red;
+			((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
--- NexusClient/Properties/Resources.Designer.cs
+++ NexusClient/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Nexus.Client.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -736,6 +736,26 @@ namespace Nexus.Client.Properties {
         internal static System.Drawing.Bitmap nl {
             get {
                 object obj = ResourceManager.GetObject("nl", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Nmm_ce_800 {
+            get {
+                object obj = ResourceManager.GetObject("Nmm-ce_800", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Nmm_community_edition {
+            get {
+                object obj = ResourceManager.GetObject("Nmm-community-edition", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
